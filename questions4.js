1db02ce4a8efaf7e5e277d77cf5c406176dd8e9c
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-05-18 20:34:39 +0500

    Create questions4.js

---
+++ questions4.js
@@ -0,0 +1,222 @@
+let questions4 = [
+    {
+        numb: 1,
+        question: "appropriate",
+        answer: "B. angemessen",
+        options: [
+            "A. relevant",
+            "B. angemessen",
+            "C. stark",
+            "D. schnell"
+        ]
+    },
+    {
+        numb: 2,
+        question: "to avoid",
+        answer: "D. vermeiden",
+        options: [
+            "A. annehmen",
+            "B. erlauben",
+            "C. rennen",
+            "D. vermeiden"
+        ]
+    },
+    {
+        numb: 3,
+        question: "to behave",
+        answer: "A. sich benehmen",
+        options: [
+            "A. sich benehmen",
+            "B. sich bewegen",
+            "C. sich erholen",
+            "D. sich verstecken"
+        ]
+    },
+    {
+        numb: 4,
+        question: "calm",
+        answer: "C. ruhig",
+        options: [
+            "A. kalt",
+            "B. warm",
+            "C. ruhig",
+            "D. heiß"
+        ]
+    },
+    {
+        numb: 5,
+        question: "concern",
+        answer: "A. Sorge",
+        options: [
+            "A. Sorge",
+            "B. Freude",
+            "C. Interesse",
+            "D. Wut"
+        ]
+    },
+    {
+        numb: 6,
+        question: "content",
+        answer: "C. zufrieden",
+        options: [
+            "A. erfreut",
+            "B. begeistert",
+            "C. zufrieden",
+            "D. wütend"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to expect",
+        answer: "B. erwarten",
+        options: [
+            "A. annehmen",
+            "B. erwarten",
+            "C. hoffen",
+            "D. wünschen"
+        ]
+    },
+    {
+        numb: 8,
+        question: "frequently",
+        answer: "D. häufig",
+        options: [
+            "A. selten",
+            "B. manchmal",
+            "C. nie",
+            "D. häufig"
+        ]
+    },
+    {
+        numb: 9,
+        question: "habit",
+        answer: "A. Gewohnheit",
+        options: [
+            "A. Gewohnheit",
+            "B. Regel",
+            "C. Plan",
+            "D. Ziel"
+        ]
+    },
+    {
+        numb: 10,
+        question: "to instruct",
+        answer: "C. unterrichten",
+        options: [
+            "A. leiten",
+            "B. führen",
+            "C. unterrichten",
+            "D. erziehen"
+        ]
+    },
+    {
+        numb: 11,
+        question: "issue",
+        answer: "D. Thema",
+        options: [
+            "A. Problem",
+            "B. Frage",
+            "C. Aufgabe",
+            "D. Thema"
+        ]
+    },
+    {
+        numb: 12,
+        question: "none",
+        answer: "B. keiner",
+        options: [
+            "A. alle",
+            "B. keiner",
+            "C. jeder",
+            "D. manche"
+        ]
+    },
+    {
+        numb: 13,
+        question: "patient",
+        answer: "A. geduldig",
+        options: [
+            "A. geduldig",
+            "B. krank",
+            "C. müde",
+            "D. schwach"
+        ]
+    },
+    {
+        numb: 14,
+        question: "positive",
+        answer: "C. positiv",
+        options: [
+            "A. negativ",
+            "B. neutral",
+            "C. positiv",
+            "D. pessimistisch"
+        ]
+    },
+    {
+        numb: 15,
+        question: "to punish",
+        answer: "B. bestrafen",
+        options: [
+            "A. belohnen",
+            "B. bestrafen",
+            "C. loben",
+            "D. schlagen"
+        ]
+    },
+    {
+        numb: 16,
+        question: "to represent",
+        answer: "A. vertreten",
+        options: [
+            "A. vertreten",
+            "B. präsentieren",
+            "C. darstellen",
+            "D. zeigen"
+        ]
+    },
+    {
+        numb: 17,
+        question: "to shake",
+        answer: "D. schütteln",
+        options: [
+            "A. schlagen",
+            "B. werfen",
+            "C. fangen",
+            "D. schütteln"
+        ]
+    },
+    {
+        numb: 18,
+        question: "to spread",
+        answer: "C. verbreiten",
+        options: [
+            "A. sammeln",
+            "B. teilen",
+            "C. verbreiten",
+            "D. verstecken"
+        ]
+    },
+    {
+        numb: 19,
+        question: "to stroll",
+        answer: "A. bummeln",
+        options: [
+            "A. bummeln",
+            "B. rennen",
+            "C. springen",
+            "D. laufen"
+        ]
+    },
+    {
+        numb: 20,
+        question: "village",
+        answer: "B. Dorf",
+        options: [
+            "A. Stadt",
+            "B. Dorf",
+            "C. Land",
+            "D. Gebäude"
+        ]
+    }
+];
