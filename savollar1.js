afe59b966a68cb31c6ee525a048f52e18d670db4
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-05-18 11:51:13 +0500

    Create savollar1.js

---
+++ savollar1.js
@@ -0,0 +1,57 @@
+let savollar1 = [
+    {
+        numb: 1,
+        question: "afraid",
+        answer: "C. qo'rqqan",
+        options: [
+            "A. jaxldor",
+            "B. dovyurak",
+            "C. qo'rqqan",
+            "D. uyalgan"
+        ]
+    },
+    {
+        numb: 2,
+        question: "agree",
+        answer: "A. qo'shilmoq",
+        options: [
+            "A. qo'shilmoq",
+            "B. qarshi chiqmoq",
+            "C. qo'shmoq",
+            "D. anglamoq"
+        ]
+    },
+    {
+        numb: 3,
+        question: "angry",
+        answer: "B. jaxldor",
+        options: [
+            "A. g'amgin",
+            "B. jaxldor",
+            "C. baxtli",
+            "D. hayratlangan"
+        ]
+    },
+    {
+        numb: 4,
+        question: "arrive",
+        answer: "D. yetib kelmoq",
+        options: [
+            "A. jo'nab ketmoq",
+            "B. uchib ketmoq",
+            "C. tark etmoq",
+            "D. yetib kelmoq"
+        ]
+    },
+    {
+        numb: 5,
+        question: "attack",
+        answer: "C. hujum qilmoq",
+        options: [
+            "A. himoya qilmoq",
+            "B. baqirmoq",
+            "C. hujum qilmoq",
+            "D. asabiylashmoq"
+        ]
+    }
+];
