b29c8236632ba2ad103b4ca74c0ab59afec5ba95
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-05-18 11:54:59 +0500

    Create questions3.js

---
+++ questions3.js
@@ -0,0 +1,226 @@
+// document.addEventListener('DOMContentLoaded', () => {
+
+let questions3 = [
+    {
+        numb: 1,
+        question: "alien",
+        answer: "A. Außerirdischer",
+        options: [
+            "A. Außerirdischer",
+            "B. Fremder",
+            "C. Ausländer",
+            "D. Unbekannter"
+        ]
+    },
+    {
+        numb: 2,
+        question: "among",
+        answer: "B. unter",
+        options: [
+            "A. zwischen",
+            "B. unter",
+            "C. neben",
+            "D. hinter"
+        ]
+    },
+    {
+        numb: 3,
+        question: "chart",
+        answer: "D. Diagramm",
+        options: [
+            "A. Karte",
+            "B. Tabelle",
+            "C. Plan",
+            "D. Diagramm"
+        ]
+    },
+    {
+        numb: 4,
+        question: "cloud",
+        answer: "C. Wolke",
+        options: [
+            "A. Himmel",
+            "B. Regen",
+            "C. Wolke",
+            "D. Wind"
+        ]
+    },
+    {
+        numb: 5,
+        question: "comprehend",
+        answer: "B. verstehen",
+        options: [
+            "A. laufen",
+            "B. verstehen",
+            "C. erkennen",
+            "D. wissen"
+        ]
+    },
+    {
+        numb: 6,
+        question: "describe",
+        answer: "A. beschreiben",
+        options: [
+            "A. beschreiben",
+            "B. erklären",
+            "C. erzählen",
+            "D. zeigen"
+        ]
+    },
+    {
+        numb: 7,
+        question: "ever",
+        answer: "C. jemals",
+        options: [
+            "A. immer",
+            "B. nie",
+            "C. jemals",
+            "D. manchmal"
+        ]
+    },
+    {
+        numb: 8,
+        question: "fail",
+        answer: "D. scheitern",
+        options: [
+            "A. fallen",
+            "B. fehlen",
+            "C. verlieren",
+            "D. scheitern"
+        ]
+    },
+    {
+        numb: 9,
+        question: "friendly",
+        answer: "A. freundlich",
+        options: [
+            "A. freundlich",
+            "B. nett",
+            "C. lieb",
+            "D. gut"
+        ]
+    },
+    {
+        numb: 10,
+        question: "grade",
+        answer: "C. Note",
+        options: [
+            "A. Markierung",
+            "B. Schule",
+            "C. Note",
+            "D. Schüler"
+        ]
+    },
+    {
+        numb: 11,
+        question: "instead",
+        answer: "B. stattdessen",
+        options: [
+            "A. außerdem",
+            "B. stattdessen",
+            "C. trotzdem",
+            "D. deshalb"
+        ]
+    },
+    {
+        numb: 12,
+        question: "library",
+        answer: "D. Bibliothek",
+        options: [
+            "A. Buch",
+            "B. Buchladen",
+            "C. Buchhandlung",
+            "D. Bibliothek"
+        ]
+    },
+    {
+        numb: 13,
+        question: "planet",
+        answer: "A. Planet",
+        options: [
+            "A. Planet",
+            "B. Welt",
+            "C. Land",
+            "D. Erde"
+        ]
+    },
+    {
+        numb: 14,
+        question: "report",
+        answer: "B. Bericht",
+        options: [
+            "A. Nachricht",
+            "B. Bericht",
+            "C. Reportage",
+            "D. Information"
+        ]
+    },
+    {
+        numb: 15,
+        question: "several",
+        answer: "D. mehrere",
+        options: [
+            "A. viele",
+            "B. einige",
+            "C. wenige",
+            "D. mehrere"
+        ]
+    },
+    {
+        numb: 16,
+        question: "solve",
+        answer: "B. lösen",
+        options: [
+            "A. finden",
+            "B. lösen",
+            "C. machen",
+            "D. schaffen"
+        ]
+    },
+    {
+        numb: 17,
+        question: "suddenly",
+        answer: "C. plötzlich",
+        options: [
+            "A. schnell",
+            "B. sofort",
+            "C. plötzlich",
+            "D. langsam"
+        ]
+    },
+    {
+        numb: 18,
+        question: "suppose",
+        answer: "D. annehmen",
+        options: [
+            "A. glauben",
+            "B. meinen",
+            "C. denken",
+            "D. annehmen"
+        ]
+    },
+    {
+        numb: 19,
+        question: "universe",
+        answer: "A. Universum",
+        options: [
+            "A. Universum",
+            "B. Welt",
+            "C. Erde",
+            "D. Himmel"
+        ]
+    },
+    {
+        numb: 20,
+        question: "view",
+        answer: "C. Aussicht",
+        options: [
+            "A. Überprüfung",
+            "B. Rückblick",
+            "C. Aussicht",
+            "D. Perspektive"
+        ]
+    }
+];
+
+// });
