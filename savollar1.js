84ec8aab454821d597bcb67fd2cfdb0b77363b63
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-05-18 13:47:37 +0500

    Update savollar1.js

--- savollar1.js
+++ savollar1.js
@@ -12,7 +12,7 @@ let savollar1 = [
     },
     {
         numb: 2,
-        question: "agree",
+        question: "to agree",
         answer: "A. qo'shilmoq",
         options: [
             "A. qo'shilmoq",
@@ -34,7 +34,7 @@ let savollar1 = [
     },
     {
         numb: 4,
-        question: "arrive",
+        question: "to arrive",
         answer: "D. yetib kelmoq",
         options: [
             "A. jo'nab ketmoq",
@@ -45,13 +45,178 @@ let savollar1 = [
     },
     {
         numb: 5,
-        question: "attack",
+        question: "to attack",
         answer: "C. hujum qilmoq",
         options: [
             "A. himoya qilmoq",
             "B. baqirmoq",
             "C. hujum qilmoq",
             "D. asabiylashmoq"
+        ]
+    },
+    {
+        numb: 6,
+        question: "bottom",
+        answer: "A. past",
+        options: [
+            "A. past",
+            "B. yuqori",
+            "C. o'rta",
+            "D. yon"
+        ]
+    },
+    {
+        numb: 7,
+        question: "clever",
+        answer: "D. aqlli",
+        options: [
+            "A. ahmoq",
+            "B. quvnoq",
+            "C. qulay",
+            "D. aqlli"
+        ]
+    },
+    {
+        numb: 8,
+        question: "cruel",
+        answer: "C. zolim",
+        options: [
+            "A. yaxshi",
+            "B. yomon",
+            "C. zolim",
+            "D. qiziq"
+        ]
+    },
+    {
+        numb: 9,
+        question: "finally",
+        answer: "B. vanihoyat",
+        options: [
+            "A. baxtimga",
+            "B. vanihoyat",
+            "C. keyin",
+            "D. har doim"
+        ]
+    },
+    {
+        numb: 10,
+        question: "to hide",
+        answer: "D. yashirmoq",
+        options: [
+            "A. ko'rsatmoq",
+            "B. topmoq",
+            "C. qo'ymoq",
+            "D. yashirmoq"
+        ]
+    },
+    {
+        numb: 11,
+        question: "to hunt",
+        answer: "C. ov qilmoq",
+        options: [
+            "A. quvmoq",
+            "B. ushlamoq",
+            "C. ov qilmoq",
+            "D. tutmoq"
+        ]
+    },
+    {
+        numb: 12,
+        question: "lot",
+        answer: "A. ko'p",
+        options: [
+            "A. ko'p",
+            "B. yetarli",
+            "C. bir nechta",
+            "D. bir qancha"
+        ]
+    },
+    {
+        numb: 13,
+        question: "middle",
+        answer: "B. o'rta",
+        options: [
+            "A. past",
+            "B. o'rta",
+            "C. yuqori",
+            "D. yon"
+        ]
+    },
+    {
+        numb: 14,
+        question: "moment",
+        answer: "C. on",
+        options: [
+            "A. soat",
+            "B. kun",
+            "C. on",
+            "D. hafta"
+        ]
+    },
+    {
+        numb: 15,
+        question: "pleased",
+        answer: "D. baxtli",
+        options: [
+            "A. sokin",
+            "B. asabiy",
+            "C. omadli",
+            "D. baxtli"
+        ]
+    },
+    {
+        numb: 16,
+        question: "to promise",
+        answer: "B. va'da bermoq",
+        options: [
+            "A. qo'llab quvvatlamoq",
+            "B. va'da bermoq",
+            "C. buzmoq",
+            "D. unutmoq"
+        ]
+    },
+    {
+        numb: 17,
+        question: "to reply",
+        answer: "C. javob bermoq",
+        options: [
+            "A. so'ramoq",
+            "B. eslatmoq",
+            "C. javob bermoq",
+            "D. qaytarmoq"
+        ]
+    },
+    {
+        numb: 18,
+        question: "safe",
+        answer: "A. xavfsiz",
+        options: [
+            "A. xavfsiz",
+            "B. yaroqli",
+            "C. doimiy",
+            "D. foydali"
+        ]
+    },
+    {
+        numb: 19,
+        question: "trick",
+        answer: "D. hiyla",
+        options: [
+            "A. o'yin",
+            "B. zavq",
+            "C. hazil",
+            "D. hiyla"
+        ]
+    },
+    {
+        numb: 20,
+        question: "well",
+        answer: "C. yaxshi",
+        options: [
+            "A. yetarli",
+            "B. qoniqarli",
+            "C. yaxshi",
+            "D. qiziq"
         ]
     }
 ];
