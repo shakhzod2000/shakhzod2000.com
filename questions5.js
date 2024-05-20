86c4888304182533313b8ba73ce16142a0100a78
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-05-20 12:04:19 +0500

    Create questions5.js

---
+++ questions5.js
@@ -0,0 +1,222 @@
+let questions5 = [
+    {
+        numb: 1,
+        question: "aware",
+        answer: "B. sich bewusst",
+        options: [
+            "A. sich erinnern",
+            "B. sich bewusst",
+            "C. sich entschuldigen",
+            "D. sich beschweren"
+        ]
+    },
+    {
+        numb: 2,
+        question: "badly",
+        answer: "A. schlecht",
+        options: [
+            "A. schlecht",
+            "B. unangenehm",
+            "C. unglücklich",
+            "D. ungünstig"
+        ]
+    },
+    {
+        numb: 3,
+        question: "to belong",
+        answer: "C. gehören",
+        options: [
+            "A. gehorchen",
+            "B. gelten",
+            "C. gehören",
+            "D. gelingen"
+        ]
+    },
+    {
+        numb: 4,
+        question: "to continue",
+        answer: "A. fortsetzen",
+        options: [
+            "A. fortsetzen",
+            "B. erweitern",
+            "C. verlängern",
+            "D. wiederholen"
+        ]
+    },
+    {
+        numb: 5,
+        question: "error",
+        answer: "D. Fehler",
+        options: [
+            "A. Illusion",
+            "B. Unglück",
+            "C. Missverständnis",
+            "D. Fehler"
+        ]
+    },
+    {
+        numb: 6,
+        question: "experience",
+        answer: "B. Erfahrung",
+        options: [
+            "A. Begegnung",
+            "B. Erfahrung",
+            "C. Kenntnis",
+            "D. Abenteuer"
+        ]
+    },
+    {
+        numb: 7,
+        question: "field",
+        answer: "C. Feld",
+        options: [
+            "A. Platz",
+            "B. Gebiet",
+            "C. Feld",
+            "D. Wiese"
+        ]
+    },
+    {
+        numb: 8,
+        question: "to hurt",
+        answer: "A. verletzen",
+        options: [
+            "A. verletzen",
+            "B. zerstören",
+            "C. ruinieren",
+            "D. schrecken"
+        ]
+    },
+    {
+        numb: 9,
+        question: "judgment",
+        answer: "D. Urteil",
+        options: [
+            "A. Anklage",
+            "B. Gericht",
+            "C. Strafe",
+            "D. Urteil"
+        ]
+    },
+    {
+        numb: 10,
+        question: "likely",
+        answer: "B. wahrscheinlich",
+        options: [
+            "A. erlaubt",
+            "B. wahrscheinlich",
+            "C. sicherlich",
+            "D. ungewiss"
+        ]
+    },
+    {
+        numb: 11,
+        question: "normal",
+        answer: "C. normal",
+        options: [
+            "A. natürlich",
+            "B. gewöhnlich",
+            "C. normal",
+            "D. standard"
+        ]
+    },
+    {
+        numb: 12,
+        question: "rare",
+        answer: "A. selten",
+        options: [
+            "A. selten",
+            "B. außergewöhnlich",
+            "C. unerwartet",
+            "D. sporadisch"
+        ]
+    },
+    {
+        numb: 13,
+        question: "to relax",
+        answer: "B. entspannen",
+        options: [
+            "A. beruhigen",
+            "B. entspannen",
+            "C. loslassen",
+            "D. nachlassen"
+        ]
+    },
+    {
+        numb: 14,
+        question: "to request",
+        answer: "C. anfordern",
+        options: [
+            "A. freuen",
+            "B. fordern",
+            "C. anfordern",
+            "D. suchen"
+        ]
+    },
+    {
+        numb: 15,
+        question: "to reside",
+        answer: "B. wohnen",
+        options: [
+            "A. bleiben",
+            "B. wohnen",
+            "C. verweilen",
+            "D. übernachten"
+        ]
+    },
+    {
+        numb: 16,
+        question: "result",
+        answer: "D. Ergebnis",
+        options: [
+            "A. Ausgang",
+            "B. Folge",
+            "C. Konsequenz",
+            "D. Ergebnis"
+        ]
+    },
+    {
+        numb: 17,
+        question: "to roll",
+        answer: "A. rollen",
+        options: [
+            "A. rollen",
+            "B. kippen",
+            "C. wenden",
+            "D. schleudern"
+        ]
+    },
+    {
+        numb: 18,
+        question: "since",
+        answer: "B. seit",
+        options: [
+            "A. bevor",
+            "B. seit",
+            "C. während",
+            "D. weil"
+        ]
+    },
+    {
+        numb: 19,
+        question: "visible",
+        answer: "C. sichtbar",
+        options: [
+            "A. erkennbar",
+            "B. deutlich",
+            "C. sichtbar",
+            "D. wahrnehmbar"
+        ]
+    },
+    {
+        numb: 20,
+        question: "wild",
+        answer: "A. wild",
+        options: [
+            "A. wild",
+            "B. böse",
+            "C. gefährlich",
+            "D. unkontrolliert"
+        ]
+    }
+];
