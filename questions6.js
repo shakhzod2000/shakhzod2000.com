e3799750069d6ad01ca80373cd228f2cf429576b
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-05-26 15:49:12 +0500

    Create questions6.js

---
+++ questions6.js
@@ -0,0 +1,222 @@
+let questions6 = [
+    {
+        numb: 1,
+        question: "advantage",
+        answer: "B. Vorteil",
+        options: [
+            "A. Nutzen",
+            "B. Vorteil",
+            "C. Gewinn",
+            "D. Profit"
+        ]
+    },
+    {
+        numb: 2,
+        question: "to cause",
+        answer: "A. verursachen",
+        options: [
+            "A. verursachen",
+            "B. anregen",
+            "C. ermutigen",
+            "D. motivieren"
+        ]
+    },
+    {
+        numb: 3,
+        question: "choice",
+        answer: "C. Wahl",
+        options: [
+            "A. Entscheidung",
+            "B. Option",
+            "C. Wahl",
+            "D. Abstimmung"
+        ]
+    },
+    {
+        numb: 4,
+        question: "community",
+        answer: "D. Gemeinschaft",
+        options: [
+            "A. Verein",
+            "B. Gruppe",
+            "C. Team",
+            "D. Gemeinschaft"
+        ]
+    },
+    {
+        numb: 5,
+        question: "dead",
+        answer: "A. tot",
+        options: [
+            "A. tot",
+            "B. lebendig",
+            "C. bewusst",
+            "D. wachsam"
+        ]
+    },
+    {
+        numb: 6,
+        question: "distance",
+        answer: "C. Entfernung",
+        options: [
+            "A. Raum",
+            "B. Länge",
+            "C. Entfernung",
+            "D. Weite"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to escape",
+        answer: "D. entkommen",
+        options: [
+            "A. angreifen",
+            "B. ergreifen",
+            "C. verfolgen",
+            "D. entkommen"
+        ]
+    },
+    {
+        numb: 8,
+        question: "to face",
+        answer: "B. gegenüberstehen",
+        options: [
+            "A. treffen",
+            "B. gegenüberstehen",
+            "C. umdrehen",
+            "D. vermeiden"
+        ]
+    },
+    {
+        numb: 9,
+        question: "to follow",
+        answer: "A. folgen",
+        options: [
+            "A. folgen",
+            "B. führen",
+            "C. bleiben",
+            "D. rennen"
+        ]
+    },
+    {
+        numb: 10,
+        question: "fright",
+        answer: "B. Schreck",
+        options: [
+            "A. Freude",
+            "B. Schreck",
+            "C. Glück",
+            "D. Liebe"
+        ]
+    },
+    {
+        numb: 11,
+        question: "ghost",
+        answer: "D. Geist",
+        options: [
+            "A. Mensch",
+            "B. Tier",
+            "C. Pflanze",
+            "D. Geist"
+        ]
+    },
+    {
+        numb: 12,
+        question: "individual",
+        answer: "C. Einzelperson",
+        options: [
+            "A. Gruppe",
+            "B. Familie",
+            "C. Einzelperson",
+            "D. Team"
+        ]
+    },
+    {
+        numb: 13,
+        question: "pet",
+        answer: "A. Haustier",
+        options: [
+            "A. Haustier",
+            "B. Wildtier",
+            "C. Vogel",
+            "D. Fisch"
+        ]
+    },
+    {
+        numb: 14,
+        question: "to reach",
+        answer: "B. erreichen",
+        options: [
+            "A. verlieren",
+            "B. erreichen",
+            "C. ignorieren",
+            "D. vermeiden"
+        ]
+    },
+    {
+        numb: 15,
+        question: "to return",
+        answer: "A. zurückkehren",
+        options: [
+            "A. zurückkehren",
+            "B. bleiben",
+            "C. verlieren",
+            "D. verstecken"
+        ]
+    },
+    {
+        numb: 16,
+        question: "to survive",
+        answer: "C. überleben",
+        options: [
+            "A. aufgeben",
+            "B. verlieren",
+            "C. überleben",
+            "D. scheitern"
+        ]
+    },
+    {
+        numb: 17,
+        question: "upset",
+        answer: "A. traurig",
+        options: [
+            "A. traurig",
+            "B. glücklich",
+            "C. überrascht",
+            "D. zufrieden"
+        ]
+    },
+    {
+        numb: 18,
+        question: "voice",
+        answer: "B. Stimme",
+        options: [
+            "A. Werkzeug",
+            "B. Stimme",
+            "C. Musik",
+            "D. Klang"
+        ]
+    },
+    {
+        numb: 19,
+        question: "weather",
+        answer: "D. Wetter",
+        options: [
+            "A. Klima",
+            "B. Temperatur",
+            "C. Jahreszeit",
+            "D. Wetter"
+        ]
+    },
+    {
+        numb: 20,
+        question: "wise",
+        answer: "C. weise",
+        options: [
+            "A. dumm",
+            "B. klug",
+            "C. weise",
+            "D. jung"
+        ]
+    }
+];
