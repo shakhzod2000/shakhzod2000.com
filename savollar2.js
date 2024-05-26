558d1ae47d67f59de026d178c272bcb7929fe34e
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-05-26 15:48:42 +0500

    Create savollar2.js

---
+++ savollar2.js
@@ -0,0 +1,222 @@
+let savollar2 = [
+    {
+        numb: 1,
+        question: "adventure",
+        answer: "B. sarguzasht",
+        options: [
+            "A. hordiq",
+            "B. sarguzasht",
+            "C. maroq",
+            "D. sayir"
+        ]
+    },
+    {
+        numb: 2,
+        question: "to approach",
+        answer: "A. yaqinlashmoq",
+        options: [
+            "A. yaqinlashmoq",
+            "B. harakat qilmoq",
+            "C. jo'namoq",
+            "D. bormoq"
+        ]
+    },
+    {
+        numb: 3,
+        question: "carefully",
+        answer: "D. ehtiyotkorlik bilan",
+        options: [
+            "A. tez",
+            "B. sekin",
+            "C. katta",
+            "D. ehtiyotkorlik bilan"
+        ]
+    },
+    {
+        numb: 4,
+        question: "a chemical",
+        answer: "C. kimyoviy modda",
+        options: [
+            "A. kimyogar",
+            "B. kimyo kitobi",
+            "C. kimyoviy modda",
+            "D. kimyo"
+        ]
+    },
+    {
+        numb: 5,
+        question: "to create",
+        answer: "B. yaratmoq",
+        options: [
+            "A. buzmoq",
+            "B. yaratmoq",
+            "C. o'zgartirmoq",
+            "D. yaxshilamoq"
+        ]
+    },
+    {
+        numb: 6,
+        question: "evil",
+        answer: "A. yomon",
+        options: [
+            "A. yomon",
+            "B. yaxshi",
+            "C. qo'rqinchli",
+            "D. xayrli"
+        ]
+    },
+    {
+        numb: 7,
+        question: "experiment",
+        answer: "B. tajriba",
+        options: [
+            "A. baholash",
+            "B. tajriba",
+            "C. o'lchash",
+            "D. o'rganish"
+        ]
+    },
+    {
+        numb: 8,
+        question: "to kill",
+        answer: "C. o'ldirmoq",
+        options: [
+            "A. yashirmoq",
+            "B. tashlanmoq",
+            "C. o'ldirmoq",
+            "D. qo'rqitmoq"
+        ]
+    },
+    {
+        numb: 9,
+        question: "laboratory",
+        answer: "D. laboratoriya",
+        options: [
+            "A. laborant",
+            "B. ilm-fan",
+            "C. natija",
+            "D. laboratoriya"
+        ]
+    },
+    {
+        numb: 10,
+        question: "laugh",
+        answer: "B. kulgi",
+        options: [
+            "A. ohang",
+            "B. kulgi",
+            "C. shovqin",
+            "D. ovoz"
+        ]
+    },
+    {
+        numb: 11,
+        question: "loud",
+        answer: "C. baland",
+        options: [
+            "A. past",
+            "B. yoqimsiz",
+            "C. baland",
+            "D. qo'rqinchli"
+        ]
+    },
+    {
+        numb: 12,
+        question: "nervous",
+        answer: "A. xavotirlangan",
+        options: [
+            "A. xavotirlangan",
+            "B. qo'rqinchli",
+            "C. ayanchli",
+            "D. shovqinli"
+        ]
+    },
+    {
+        numb: 13,
+        question: "noise",
+        answer: "C. shovqin",
+        options: [
+            "A. kulgi",
+            "B. ovoz",
+            "C. shovqin",
+            "D. ohang"
+        ]
+    },
+    {
+        numb: 14,
+        question: "project",
+        answer: "B. yumush",
+        options: [
+            "A. ish",
+            "B. yumush",
+            "C. harakat",
+            "D. natija"
+        ]
+    },
+    {
+        numb: 15,
+        question: "to scare",
+        answer: "D. qo'rqitmoq",
+        options: [
+            "A. urushmoq",
+            "B. baqirmoq",
+            "C. hujum qilmoq",
+            "D. qo'rqitmoq"
+        ]
+    },
+    {
+        numb: 16,
+        question: "secret",
+        answer: "C. sir",
+        options: [
+            "A. sehr",
+            "B. masala",
+            "C. sir",
+            "D. javob"
+        ]
+    },
+    {
+        numb: 17,
+        question: "to shout",
+        answer: "A. baqirmoq",
+        options: [
+            "A. baqirmoq",
+            "B. urushmoq",
+            "C. qo'rqitmoq",
+            "D. hujum qilmoq"
+        ]
+    },
+    {
+        numb: 18,
+        question: "to smell",
+        answer: "D. hidlamoq",
+        options: [
+            "A. sezmoq",
+            "B. his qilmoq",
+            "C. ko'rmoq",
+            "D. hidlamoq"
+        ]
+    },
+    {
+        numb: 19,
+        question: "terrible",
+        answer: "B. juda yomon",
+        options: [
+            "A. hunuk",
+            "B. juda yomon",
+            "C. qo'rqinchli",
+            "D. hayratlanarli"
+        ]
+    },
+    {
+        numb: 20,
+        question: "worse",
+        answer: "C. yomonroq",
+        options: [
+            "A. baxil",
+            "B. yomon",
+            "C. yomonroq",
+            "D. juda yomon"
+        ]
+    }
+]
