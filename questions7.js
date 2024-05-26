10be47de70483418c38735b28ffc722151c05406
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-05-26 15:50:14 +0500

    Create questions7.js

---
+++ questions7.js
@@ -0,0 +1,222 @@
+let questions7 = [
+    {
+        numb: 1,
+        question: "to allow",
+        answer: "A. erlauben",
+        options: [
+            "A. erlauben",
+            "B. verbieten",
+            "C. zusagen",
+            "D. einschränken"
+        ]
+    },
+    {
+        numb: 2,
+        question: "to announce",
+        answer: "B. ankündigen",
+        options: [
+            "A. erklären",
+            "B. ankündigen",
+            "C. verbergen",
+            "D. verschleiern"
+        ]
+    },
+    {
+        numb: 3,
+        question: "beside",
+        answer: "C. neben",
+        options: [
+            "A. über",
+            "B. unter",
+            "C. neben",
+            "D. vor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "challenge",
+        answer: "D. Herausforderung",
+        options: [
+            "A. Möglichkeit",
+            "B. Gelegenheit",
+            "C. Chance",
+            "D. Herausforderung"
+        ]
+    },
+    {
+        numb: 5,
+        question: "to claim",
+        answer: "A. behaupten",
+        options: [
+            "A. behaupten",
+            "B. ablehnen",
+            "C. akzeptieren",
+            "D. bestätigen"
+        ]
+    },
+    {
+        numb: 6,
+        question: "condition",
+        answer: "C. Zustand",
+        options: [
+            "A. Situation",
+            "B. Umstand",
+            "C. Zustand",
+            "D. Lage"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to contribute",
+        answer: "B. beitragen",
+        options: [
+            "A. spenden",
+            "B. beitragen",
+            "C. verzichten",
+            "D. ausgeben"
+        ]
+    },
+    {
+        numb: 8,
+        question: "difference",
+        answer: "C. Unterschied",
+        options: [
+            "A. Ähnlichkeit",
+            "B. Gemeinsamkeit",
+            "C. Unterschied",
+            "D. Verbindung"
+        ]
+    },
+    {
+        numb: 9,
+        question: "to divide",
+        answer: "A. teilen",
+        options: [
+            "A. teilen",
+            "B. verbinden",
+            "C. vervielfältigen",
+            "D. vereinen"
+        ]
+    },
+    {
+        numb: 10,
+        question: "expert",
+        answer: "D. Experte",
+        options: [
+            "A. Anfänger",
+            "B. Amateur",
+            "C. Meister",
+            "D. Experte"
+        ]
+    },
+    {
+        numb: 11,
+        question: "famous",
+        answer: "B. berühmt",
+        options: [
+            "A. unbekannt",
+            "B. berühmt",
+            "C. bedeutend",
+            "D. beliebt"
+        ]
+    },
+    {
+        numb: 12,
+        question: "force",
+        answer: "A. Kraft",
+        options: [
+            "A. Kraft",
+            "B. Energie",
+            "C. Widerstand",
+            "D. Druck"
+        ]
+    },
+    {
+        numb: 13,
+        question: "harm",
+        answer: "C. Schaden",
+        options: [
+            "A. Nutzen",
+            "B. Vorteil",
+            "C. Schaden",
+            "D. Gewinn"
+        ]
+    },
+    {
+        numb: 14,
+        question: "to lay",
+        answer: "B. legen",
+        options: [
+            "A. heben",
+            "B. legen",
+            "C. werfen",
+            "D. schlagen"
+        ]
+    },
+    {
+        numb: 15,
+        question: "peace",
+        answer: "D. Frieden",
+        options: [
+            "A. Krieg",
+            "B. Kampf",
+            "C. Unruhe",
+            "D. Frieden"
+        ]
+    },
+    {
+        numb: 16,
+        question: "prince",
+        answer: "C. Prinz",
+        options: [
+            "A. König",
+            "B. Ritter",
+            "C. Prinz",
+            "D. Herzog"
+        ]
+    },
+    {
+        numb: 17,
+        question: "to protect",
+        answer: "A. schützen",
+        options: [
+            "A. schützen",
+            "B. angreifen",
+            "C. verletzen",
+            "D. zerstören"
+        ]
+    },
+    {
+        numb: 18,
+        question: "to sense",
+        answer: "B. spüren",
+        options: [
+            "A. riechen",
+            "B. spüren",
+            "C. schmecken",
+            "D. hören"
+        ]
+    },
+    {
+        numb: 19,
+        question: "sudden",
+        answer: "A. plötzlich",
+        options: [
+            "A. plötzlich",
+            "B. langsam",
+            "C. allmählich",
+            "D. stetig"
+        ]
+    },
+    {
+        numb: 20,
+        question: "therefore",
+        answer: "D. daher",
+        options: [
+            "A. trotzdem",
+            "B. dennoch",
+            "C. aber",
+            "D. daher"
+        ]
+    }
+];
