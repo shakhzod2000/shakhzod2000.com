743351b3547359c161afc122cb902f738533d635
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-05-26 15:50:53 +0500

    Create questions11.js

---
+++ questions11.js
@@ -0,0 +1,222 @@
+let questions11 = [
+    {
+        numb: 1,
+        question: "anymore",
+        answer: "B. nicht mehr",
+        options: [
+            "A. irgendwann",
+            "B. nicht mehr",
+            "C. immer noch",
+            "D. schon wieder"
+        ]
+    },
+    {
+        numb: 2,
+        question: "asleep",
+        answer: "A. schlafend",
+        options: [
+            "A. schlafend",
+            "B. wach",
+            "C. müde",
+            "D. traumhaft"
+        ]
+    },
+    {
+        numb: 3,
+        question: "berry",
+        answer: "C. Beere",
+        options: [
+            "A. Apfel",
+            "B. Orange",
+            "C. Beere",
+            "D. Banane"
+        ]
+    },
+    {
+        numb: 4,
+        question: "collect",
+        answer: "D. sammeln",
+        options: [
+            "A. zerstören",
+            "B. teilen",
+            "C. verschwenden",
+            "D. sammeln"
+        ]
+    },
+    {
+        numb: 5,
+        question: "compete",
+        answer: "B. konkurrieren",
+        options: [
+            "A. kooperieren",
+            "B. konkurrieren",
+            "C. kollidieren",
+            "D. koordinieren"
+        ]
+    },
+    {
+        numb: 6,
+        question: "conversation",
+        answer: "A. Unterhaltung",
+        options: [
+            "A. Unterhaltung",
+            "B. Präsentation",
+            "C. Diskussion",
+            "D. Vorstellung"
+        ]
+    },
+    {
+        numb: 7,
+        question: "creature",
+        answer: "C. Kreatur",
+        options: [
+            "A. Pflanze",
+            "B. Fisch",
+            "C. Kreatur",
+            "D. Mineral"
+        ]
+    },
+    {
+        numb: 8,
+        question: "decision",
+        answer: "B. Entscheidung",
+        options: [
+            "A. Absicht",
+            "B. Entscheidung",
+            "C. Kompromiss",
+            "D. Lösung"
+        ]
+    },
+    {
+        numb: 9,
+        question: "either",
+        answer: "D. entweder",
+        options: [
+            "A. sowohl",
+            "B. auch",
+            "C. stattdessen",
+            "D. entweder"
+        ]
+    },
+    {
+        numb: 10,
+        question: "forest",
+        answer: "A. Wald",
+        options: [
+            "A. Wald",
+            "B. Wüste",
+            "C. Savanne",
+            "D. Steppe"
+        ]
+    },
+    {
+        numb: 11,
+        question: "ground",
+        answer: "C. Boden",
+        options: [
+            "A. Himmel",
+            "B. Wasser",
+            "C. Boden",
+            "D. Luft"
+        ]
+    },
+    {
+        numb: 12,
+        question: "introduce",
+        answer: "A. vorstellen",
+        options: [
+            "A. vorstellen",
+            "B. begrüßen",
+            "C. herausfinden",
+            "D. erklären"
+        ]
+    },
+    {
+        numb: 13,
+        question: "marry",
+        answer: "B. heiraten",
+        options: [
+            "A. treffen",
+            "B. heiraten",
+            "C. trennen",
+            "D. verloben"
+        ]
+    },
+    {
+        numb: 14,
+        question: "prepare",
+        answer: "C. vorbereiten",
+        options: [
+            "A. beenden",
+            "B. beginnen",
+            "C. vorbereiten",
+            "D. abschließen"
+        ]
+    },
+    {
+        numb: 15,
+        question: "sail",
+        answer: "B. segeln",
+        options: [
+            "A. schwimmen",
+            "B. segeln",
+            "C. fliegen",
+            "D. fahren"
+        ]
+    },
+    {
+        numb: 16,
+        question: "serious",
+        answer: "A. ernst",
+        options: [
+            "A. ernst",
+            "B. lustig",
+            "C. traurig",
+            "D. wichtig"
+        ]
+    },
+    {
+        numb: 17,
+        question: "spend",
+        answer: "D. verbringen",
+        options: [
+            "A. sparen",
+            "B. investieren",
+            "C. ausgeben",
+            "D. verbringen"
+        ]
+    },
+    {
+        numb: 18,
+        question: "strange",
+        answer: "B. seltsam",
+        options: [
+            "A. bekannt",
+            "B. seltsam",
+            "C. vertraut",
+            "D. normal"
+        ]
+    },
+    {
+        numb: 19,
+        question: "truth",
+        answer: "A. Wahrheit",
+        options: [
+            "A. Wahrheit",
+            "B. Geheimnis",
+            "C. Lüge",
+            "D. Tatsache"
+        ]
+    },
+    {
+        numb: 20,
+        question: "wake",
+        answer: "C. aufwachen",
+        options: [
+            "A. schlafen",
+            "B. träumen",
+            "C. aufwachen",
+            "D. einschlafen"
+        ]
+    },
+];
