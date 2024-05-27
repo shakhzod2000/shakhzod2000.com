8d4264f0338f1812b63232da76742cfc6d34a85a
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-05-27 14:14:55 +0500

    Create savollar4.js

---
+++ savollar4.js
@@ -0,0 +1,222 @@
+let savollar4 = [
+    {
+        numb: 1,
+        question: "appropriate",
+        answer: "A. to'g'ri",
+        options: [
+            "A. to'g'ri",
+            "B. qulay",
+            "C. katta",
+            "D. kichik"
+        ]
+    },
+    {
+        numb: 2,
+        question: "to avoid",
+        answer: "B. olib qochmoq",
+        options: [
+            "A. ketmoq",
+            "B. olib qochmoq",
+            "C. xohlamoq",
+            "D. o'ylanmoq"
+        ]
+    },
+    {
+        numb: 3,
+        question: "to behave",
+        answer: "D. o'zini tutmoq",
+        options: [
+            "A. baqirmoq",
+            "B. yig'lamoq",
+            "C. kulmoq",
+            "D. o'zini tutmoq"
+        ]
+    },
+    {
+        numb: 4,
+        question: "calm",
+        answer: "C. hotirjam",
+        options: [
+            "A. shovqinli",
+            "B. qizg'in",
+            "C. hotirjam",
+            "D. xavotirli"
+        ]
+    },
+    {
+        numb: 5,
+        question: "concern",
+        answer: "A. tashvish",
+        options: [
+            "A. tashvish",
+            "B. baxt",
+            "C. qiziqish",
+            "D. orzu"
+        ]
+    },
+    {
+        numb: 6,
+        question: "content",
+        answer: "B. hursand",
+        options: [
+            "A. g'amgin",
+            "B. hursand",
+            "C. xafa",
+            "D. quvnoq"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to expect",
+        answer: "C. kutmoq",
+        options: [
+            "A. unutmoq",
+            "B. sevmoq",
+            "C. kutmoq",
+            "D. yoqtirmoq"
+        ]
+    },
+    {
+        numb: 8,
+        question: "frequently",
+        answer: "A. tez-tez",
+        options: [
+            "A. tez-tez",
+            "B. kamdan-kam",
+            "C. ba'zan",
+            "D. hech qachon"
+        ]
+    },
+    {
+        numb: 9,
+        question: "habit",
+        answer: "B. odat",
+        options: [
+            "A. harakat",
+            "B. odat",
+            "C. qoida",
+            "D. maqsad"
+        ]
+    },
+    {
+        numb: 10,
+        question: "to instruct",
+        answer: "C. o'rgatmoq",
+        options: [
+            "A. o'rganmoq",
+            "B. sinab ko'rmoq",
+            "C. o'rgatmoq",
+            "D. kuzatmoq"
+        ]
+    },
+    {
+        numb: 11,
+        question: "issue",
+        answer: "A. masala",
+        options: [
+            "A. masala",
+            "B. muvaffaqiyat",
+            "C. reja",
+            "D. orzu"
+        ]
+    },
+    {
+        numb: 12,
+        question: "none",
+        answer: "D. hech qancha",
+        options: [
+            "A. hamma",
+            "B. ba'zi",
+            "C. ko'p",
+            "D. hech qancha"
+        ]
+    },
+    {
+        numb: 13,
+        question: "patient",
+        answer: "B. sabrli",
+        options: [
+            "A. shoshqaloq",
+            "B. sabrli",
+            "C. jahldor",
+            "D. dangasa"
+        ]
+    },
+    {
+        numb: 14,
+        question: "positive",
+        answer: "A. ijobiy",
+        options: [
+            "A. ijobiy",
+            "B. salbiy",
+            "C. neytral",
+            "D. shubhali"
+        ]
+    },
+    {
+        numb: 15,
+        question: "to punish",
+        answer: "C. jazolamoq",
+        options: [
+            "A. mukofotlamoq",
+            "B. yordam bermoq",
+            "C. jazolamoq",
+            "D. kechirmoq"
+        ]
+    },
+    {
+        numb: 16,
+        question: "to represent",
+        answer: "D. vakil bo'lmoq",
+        options: [
+            "A. yashirmoq",
+            "B. unutmoq",
+            "C. sotmoq",
+            "D. vakil bo'lmoq"
+        ]
+    },
+    {
+        numb: 17,
+        question: "to shake",
+        answer: "B. silkitmoq",
+        options: [
+            "A. ko'tarmoq",
+            "B. silkitmoq",
+            "C. tushirmoq",
+            "D. ko'rmoq"
+        ]
+    },
+    {
+        numb: 18,
+        question: "to spread",
+        answer: "A. yoymoq",
+        options: [
+            "A. yoymoq",
+            "B. yig'moq",
+            "C. to'plamoq",
+            "D. yopmoq"
+        ]
+    },
+    {
+        numb: 19,
+        question: "to stroll",
+        answer: "C. sayr qilmoq",
+        options: [
+            "A. yugurmoq",
+            "B. sakramoq",
+            "C. sayr qilmoq",
+            "D. uxlamoq"
+        ]
+    },
+    {
+        numb: 20,
+        question: "village",
+        answer: "B. qishloq",
+        options: [
+            "A. shahar",
+            "B. qishloq",
+            "C. davlat",
+            "D. mamlakat"
+        ]
+    }
+];
