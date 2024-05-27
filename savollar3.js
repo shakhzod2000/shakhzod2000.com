86785b6fc42b994f229c9146b478190c45d68e98
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-05-27 13:55:03 +0500

    Create savollar3.js

---
+++ savollar3.js
@@ -0,0 +1,222 @@
+let savollar3 = [
+    {
+        numb: 1,
+        question: "alien",
+        answer: "C. o'zga sayyoralik",
+        options: [
+            "A. do'st",
+            "B. dushman",
+            "C. o'zga sayyoralik",
+            "D. hamkor"
+        ]
+    },
+    {
+        numb: 2,
+        question: "among",
+        answer: "A. orasida",
+        options: [
+            "A. orasida",
+            "B. ustida",
+            "C. yonida",
+            "D. qarshisida"
+        ]
+    },
+    {
+        numb: 3,
+        question: "chart",
+        answer: "D. diagramma",
+        options: [
+            "A. kitob",
+            "B. xat",
+            "C. rasm",
+            "D. jadval"
+        ]
+    },
+    {
+        numb: 4,
+        question: "cloud",
+        answer: "B. bulut",
+        options: [
+            "A. quyosh",
+            "B. bulut",
+            "C. yomg'ir",
+            "D. havo"
+        ]
+    },
+    {
+        numb: 5,
+        question: "to comprehend",
+        answer: "C. tushunmoq",
+        options: [
+            "A. eshitmoq",
+            "B. ko'rmoq",
+            "C. tushunmoq",
+            "D. yozmoq"
+        ]
+    },
+    {
+        numb: 6,
+        question: "to describe",
+        answer: "A. tasvirlamoq",
+        options: [
+            "A. tasvirlamoq",
+            "B. eshitmoq",
+            "C. topmoq",
+            "D. unutmoq"
+        ]
+    },
+    {
+        numb: 7,
+        question: "ever",
+        answer: "D. qachon bo'lmasin",
+        options: [
+            "A. hech qachon",
+            "B. ba'zida",
+            "C. kamdan-kam",
+            "D. qachon bo'lmasin"
+        ]
+    },
+    {
+        numb: 8,
+        question: "fail",
+        answer: "B. muvaffaqiyatsizlikka uchramoq",
+        options: [
+            "A. yutmoq",
+            "B. muvaffaqiyatsizlikka uchramoq",
+            "C. intilmoq",
+            "D. harakat qilmoq"
+        ]
+    },
+    {
+        numb: 9,
+        question: "friendly",
+        answer: "A. do'stona",
+        options: [
+            "A. do'stona",
+            "B. foydali",
+            "C. do'stlik",
+            "D. yoqimli"
+        ]
+    },
+    {
+        numb: 10,
+        question: "grade",
+        answer: "D. baho",
+        options: [
+            "A. kitob",
+            "B. daftar",
+            "C. sinf",
+            "D. baho"
+        ]
+    },
+    {
+        numb: 11,
+        question: "instead",
+        answer: "C. o'rniga",
+        options: [
+            "A. keyin",
+            "B. oldin",
+            "C. o'rniga",
+            "D. yonida"
+        ]
+    },
+    {
+        numb: 12,
+        question: "library",
+        answer: "A. kutubxona",
+        options: [
+            "A. kutubxona",
+            "B. sinf",
+            "C. rasmxona",
+            "D. do'kon"
+        ]
+    },
+    {
+        numb: 13,
+        question: "planet",
+        answer: "D. sayyora",
+        options: [
+            "A. yulduz",
+            "B. quyosh",
+            "C. oy",
+            "D. sayyora"
+        ]
+    },
+    {
+        numb: 14,
+        question: "report",
+        answer: "B. hisobot",
+        options: [
+            "A. xat",
+            "B. hisobot",
+            "C. rasm",
+            "D. jurnal"
+        ]
+    },
+    {
+        numb: 15,
+        question: "several",
+        answer: "C. bir necha",
+        options: [
+            "A. kam",
+            "B. oz miqdorda",
+            "C. bir necha",
+            "D. ko'p"
+        ]
+    },
+    {
+        numb: 16,
+        question: "solve",
+        answer: "A. yechmoq",
+        options: [
+            "A. yechmoq",
+            "B. o'ylamoq",
+            "C. yozmoq",
+            "D. o'qimoq"
+        ]
+    },
+    {
+        numb: 17,
+        question: "suddenly",
+        answer: "D. to'satdan",
+        options: [
+            "A. sekin",
+            "B. doimiy ravishda",
+            "C. muntazam",
+            "D. to'satdan"
+        ]
+    },
+    {
+        numb: 18,
+        question: "suppose",
+        answer: "B. taxmin qilmoq",
+        options: [
+            "A. bilmoq",
+            "B. taxmin qilmoq",
+            "C. ko'rmoq",
+            "D. yozmoq"
+        ]
+    },
+    {
+        numb: 19,
+        question: "universe",
+        answer: "A. koinot",
+        options: [
+            "A. koinot",
+            "B. yer",
+            "C. quyosh",
+            "D. osmon"
+        ]
+    },
+    {
+        numb: 20,
+        question: "to view",
+        answer: "C. qaramoq",
+        options: [
+            "A. eshitmoq",
+            "B. yozmoq",
+            "C. qaramoq",
+            "D. gapirmoq"
+        ]
+    }
+];
