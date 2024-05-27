384bac7aa406ba438d3835add640ec54c68852f4
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-05-27 16:57:08 +0500

    Create savollar6.js

---
+++ savollar6.js
@@ -0,0 +1,222 @@
+let savollar6 = [
+    {
+        numb: 1,
+        question: "advantage",
+        answer: "B. ustunlik",
+        options: [
+            "A. doimiylik",
+            "B. ustunlik",
+            "C. daromad",
+            "D. foydali"
+        ]
+    },
+    {
+        numb: 2,
+        question: "to cause",
+        answer: "A. sabab bo'lmoq",
+        options: [
+            "A. sabab bo'lmoq",
+            "B. ilgari surmoq",
+            "C. rag'batlantirmoq",
+            "D. to'sqinlik qilmoq"
+        ]
+    },
+    {
+        numb: 3,
+        question: "choice",
+        answer: "C. tanlov",
+        options: [
+            "A. qaror",
+            "B. saralash",
+            "C. tanlov",
+            "D. ovoz berish"
+        ]
+    },
+    {
+        numb: 4,
+        question: "community",
+        answer: "D. jamiyat",
+        options: [
+            "A. jamiyat",
+            "B. guruh",
+            "C. jamoa",
+            "D. aholi"
+        ]
+    },
+    {
+        numb: 5,
+        question: "dead",
+        answer: "A. o'lgan",
+        options: [
+            "A. o'lgan",
+            "B. ko'milgan",
+            "C. kasal",
+            "D. jarohatli"
+        ]
+    },
+    {
+        numb: 6,
+        question: "distance",
+        answer: "C. masofa",
+        options: [
+            "A. yo'nalish",
+            "B. uzunlik",
+            "C. masofa",
+            "D. yo'l"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to escape",
+        answer: "D. qochib qutulmoq",
+        options: [
+            "A. himoyalanmoq",
+            "B. tark etmoq",
+            "C. uzoqqa bormoq",
+            "D. qochib qutulmoq"
+        ]
+    },
+    {
+        numb: 8,
+        question: "to face",
+        answer: "B. duch kelmoq",
+        options: [
+            "A. uchratmoq",
+            "B. duch kelmoq",
+            "C. o'girilmoq",
+            "D. e'tiborsiz qolmoq"
+        ]
+    },
+    {
+        numb: 9,
+        question: "to follow",
+        answer: "A. ortidan bormoq",
+        options: [
+            "A. ortidan bormoq",
+            "B. birga yurmoq",
+            "C. ortga qaytmoq",
+            "D. yugurmoq"
+        ]
+    },
+    {
+        numb: 10,
+        question: "fright",
+        answer: "B. qo'rquv",
+        options: [
+            "A. qiziquvchanlik",
+            "B. qo'rquv",
+            "C. iztirob",
+            "D. g'azab"
+        ]
+    },
+    {
+        numb: 11,
+        question: "ghost",
+        answer: "D. rux",
+        options: [
+            "A. maxluq",
+            "B. jodugar",
+            "C. o'lik",
+            "D. rux"
+        ]
+    },
+    {
+        numb: 12,
+        question: "individual",
+        answer: "C. kishi",
+        options: [
+            "A. guruh",
+            "B. oila",
+            "C. kishi",
+            "D. jamoa"
+        ]
+    },
+    {
+        numb: 13,
+        question: "pet",
+        answer: "A. uy hayvoni",
+        options: [
+            "A. uy hayvoni",
+            "B. parranda",
+            "C. qush",
+            "D. chorva"
+        ]
+    },
+    {
+        numb: 14,
+        question: "to reach",
+        answer: "B. yetib bormoq",
+        options: [
+            "A. uddalamoq",
+            "B. yetib bormoq",
+            "C. payqamoq",
+            "D. yutmoq"
+        ]
+    },
+    {
+        numb: 15,
+        question: "to return",
+        answer: "A. qaytmoq",
+        options: [
+            "A. qaytmoq",
+            "B. yetib kelmoq",
+            "C. jo'namoq",
+            "D. bormoq"
+        ]
+    },
+    {
+        numb: 16,
+        question: "to survive",
+        answer: "C. tirik qolmoq",
+        options: [
+            "A. zabt etmoq",
+            "B. yashamoq",
+            "C. tirik qolmoq",
+            "D. hursand bo'lmoq"
+        ]
+    },
+    {
+        numb: 17,
+        question: "upset",
+        answer: "A. xafa",
+        options: [
+            "A. xafa",
+            "B. havotirlangan",
+            "C. qo'rquvga tushgan",
+            "D. g'azablangan"
+        ]
+    },
+    {
+        numb: 18,
+        question: "voice",
+        answer: "B. ovoz",
+        options: [
+            "A. vosita",
+            "B. ovoz",
+            "C. musiqa",
+            "D. shovqin"
+        ]
+    },
+    {
+        numb: 19,
+        question: "weather",
+        answer: "D. ob-havo",
+        options: [
+            "A. iqlim",
+            "B. harorat",
+            "C. fasl",
+            "D. ob-havo"
+        ]
+    },
+    {
+        numb: 20,
+        question: "wise",
+        answer: "C. dono",
+        options: [
+            "A. sergak",
+            "B. odobli",
+            "C. dono",
+            "D. hushyor"
+        ]
+    }
+];
