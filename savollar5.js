b390222691d23ea78cb2e761da823060727949eb
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-05-27 16:11:39 +0500

    Create savollar5.js

---
+++ savollar5.js
@@ -0,0 +1,222 @@
+let savollar5 = [
+    {
+        numb: 1,
+        question: "aware",
+        answer: "C. xabardor",
+        options: [
+            "A. ko'rgan",
+            "B. eshitgan",
+            "C. xabardor",
+            "D. unutilgan"
+        ]
+    },
+    {
+        numb: 2,
+        question: "badly",
+        answer: "B. yomon",
+        options: [
+            "A. qo'shiq",
+            "B. yomon",
+            "C. o'rtacha",
+            "D. tez"
+        ]
+    },
+    {
+        numb: 3,
+        question: "to belong",
+        answer: "A. tegishli bo'lmoq",
+        options: [
+            "A. tegishli bo'lmoq",
+            "B. yo'qotmoq",
+            "C. topmoq",
+            "D. qo'ymoq"
+        ]
+    },
+    {
+        numb: 4,
+        question: "to continue",
+        answer: "D. davom etmoq",
+        options: [
+            "A. to'xtatmoq",
+            "B. boshlamoq",
+            "C. tugatmoq",
+            "D. davom etmoq"
+        ]
+    },
+    {
+        numb: 5,
+        question: "error",
+        answer: "B. xato",
+        options: [
+            "A. omadsizlik",
+            "B. xato",
+            "C. muvaffaqiyatsizlik",
+            "D. yiqilish"
+        ]
+    },
+    {
+        numb: 6,
+        question: "experience",
+        answer: "C. taasurot",
+        options: [
+            "A. bilim",
+            "B. o'rganish",
+            "C. taasurot",
+            "D. dars"
+        ]
+    },
+    {
+        numb: 7,
+        question: "field",
+        answer: "A. keng maydon",
+        options: [
+            "A. keng maydon",
+            "B. daryo",
+            "C. o'rmon",
+            "D. tog'"
+        ]
+    },
+    {
+        numb: 8,
+        question: "to hurt",
+        answer: "D. jarohatlamoq",
+        options: [
+            "A. davolamoq",
+            "B. qutqarmoq",
+            "C. yiqilmoq",
+            "D. jarohatlamoq"
+        ]
+    },
+    {
+        numb: 9,
+        question: "judgment",
+        answer: "B. fikr",
+        options: [
+            "A. suhbat",
+            "B. fikr",
+            "C. savol",
+            "D. javob"
+        ]
+    },
+    {
+        numb: 10,
+        question: "likely",
+        answer: "C. ehtimol",
+        options: [
+            "A. aniq",
+            "B. hech qachon",
+            "C. ehtimol",
+            "D. shubhali"
+        ]
+    },
+    {
+        numb: 11,
+        question: "normal",
+        answer: "A. normal",
+        options: [
+            "A. normal",
+            "B. o'ziga xos",
+            "C. g'ayrioddiy",
+            "D. qiziqarli"
+        ]
+    },
+    {
+        numb: 12,
+        question: "rare",
+        answer: "D. noyob",
+        options: [
+            "A. keng tarqalgan",
+            "B. ko'p",
+            "C. odatiy",
+            "D. noyob"
+        ]
+    },
+    {
+        numb: 13,
+        question: "to relax",
+        answer: "C. dam olmoq",
+        options: [
+            "A. o'ynamoq",
+            "B. maza qilmoq",
+            "C. dam olmoq",
+            "D. yotmoq"
+        ]
+    },
+    {
+        numb: 14,
+        question: "to request",
+        answer: "B. so'ramoq",
+        options: [
+            "A. buyurmoq",
+            "B. so'ramoq",
+            "C. to'xtatmoq",
+            "D. olmoq"
+        ]
+    },
+    {
+        numb: 15,
+        question: "to reside",
+        answer: "A. istiqomat qilmoq",
+        options: [
+            "A. istiqomat qilmoq",
+            "B. sayohat qilmoq",
+            "C. o'qimoq",
+            "D. ishlamoq"
+        ]
+    },
+    {
+        numb: 16,
+        question: "result",
+        answer: "C. natija",
+        options: [
+            "A. savol",
+            "B. qaror",
+            "C. natija",
+            "D. maqsad"
+        ]
+    },
+    {
+        numb: 17,
+        question: "to roll",
+        answer: "B. yumalatmoq",
+        options: [
+            "A. aylantirmoq",
+            "B. yumalatmoq",
+            "C. itarmoq",
+            "D. ko'chirmoq"
+        ]
+    },
+    {
+        numb: 18,
+        question: "since",
+        answer: "D. dan beri",
+        options: [
+            "A. keyin",
+            "B. ilgari",
+            "C. hozir",
+            "D. dan beri"
+        ]
+    },
+    {
+        numb: 19,
+        question: "visible",
+        answer: "A. ko'rinadigan",
+        options: [
+            "A. ko'rinadigan",
+            "B. ochiq",
+            "C. tiniq",
+            "D. katta"
+        ]
+    },
+    {
+        numb: 20,
+        question: "wild",
+        answer: "C. yovvoyi",
+        options: [
+            "A. badjaxl",
+            "B. yirtqich",
+            "C. yovvoyi",
+            "D. qo'rqinchli"
+        ]
+    }
+];
