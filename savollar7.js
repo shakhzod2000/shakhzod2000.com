659eeaa6f0617af531d94aad72c70683090e909e
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-06-24 21:06:44 +0500

    Create savollar7.js

---
+++ savollar7.js
@@ -0,0 +1,222 @@
+let savollar7 = [
+    {
+        numb: 1,
+        question: "to allow",
+        answer: "A. ruxsat bermoq",
+        options: [
+            "A. ruxsat bermoq",
+            "B. taqiqlamoq",
+            "C. va'da bermoq",
+            "D. cheklamoq"
+        ]
+    },
+    {
+        numb: 2,
+        question: "to announce",
+        answer: "B. e'lon qilmoq",
+        options: [
+            "A. bildirmoq",
+            "B. e'lon qilmoq",
+            "C. ko'rsatmoq",
+            "D. aytmoq"
+        ]
+    },
+    {
+        numb: 3,
+        question: "beside",
+        answer: "C. yonma-yon",
+        options: [
+            "A. ustida",
+            "B. ostida",
+            "C. yonma-yon",
+            "D. oldida"
+        ]
+    },
+    {
+        numb: 4,
+        question: "challenge",
+        answer: "D. qiyinchilik",
+        options: [
+            "A. imkoniyat",
+            "B. fursat",
+            "C. muammo",
+            "D. qiyinchilik"
+        ]
+    },
+    {
+        numb: 5,
+        question: "to claim",
+        answer: "A. tasdiqlamoq",
+        options: [
+            "A. tasdiqlamoq",
+            "B. rad etmoq",
+            "C. qabul qilmoq",
+            "D. rozi bo'lmoq"
+        ]
+    },
+    {
+        numb: 6,
+        question: "condition",
+        answer: "C. holat",
+        options: [
+            "A. imkoniyat",
+            "B. kattalik",
+            "C. holat",
+            "D. daraja"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to contribute",
+        answer: "B. hissa qo'shmoq",
+        options: [
+            "A. maslahat bermoq",
+            "B. hissa qo'shmoq",
+            "C. kelishmoq",
+            "D. sarflamoq"
+        ]
+    },
+    {
+        numb: 8,
+        question: "difference",
+        answer: "C. farq",
+        options: [
+            "A. o'xshashlik",
+            "B. umumiylik",
+            "C. farq",
+            "D. bog'liqlik"
+        ]
+    },
+    {
+        numb: 9,
+        question: "to divide",
+        answer: "A. bo'lmoq",
+        options: [
+            "A. bo'lmoq",
+            "B. birlashtirmoq",
+            "C. ko'paytirmoq",
+            "D. kamaytirmoq"
+        ]
+    },
+    {
+        numb: 10,
+        question: "expert",
+        answer: "D. ekspert",
+        options: [
+            "A. bilimdon",
+            "B. havaskor",
+            "C. shogird",
+            "D. ekspert"
+        ]
+    },
+    {
+        numb: 11,
+        question: "famous",
+        answer: "B. mashhur",
+        options: [
+            "A. ma'lum",
+            "B. mashhur",
+            "C. muhim",
+            "D. sevimli"
+        ]
+    },
+    {
+        numb: 12,
+        question: "force",
+        answer: "A. kuch",
+        options: [
+            "A. kuch",
+            "B. energiya",
+            "C. qarshilik",
+            "D. bosim"
+        ]
+    },
+    {
+        numb: 13,
+        question: "harm",
+        answer: "C. zarar",
+        options: [
+            "A. nuqson",
+            "B. yetishmovchilik",
+            "C. zarar",
+            "D. kamchilik"
+        ]
+    },
+    {
+        numb: 14,
+        question: "to lay",
+        answer: "B. qo'ymoq",
+        options: [
+            "A. ko'tarmoq",
+            "B. qo'ymoq",
+            "C. otmoq",
+            "D. urmoq"
+        ]
+    },
+    {
+        numb: 15,
+        question: "peace",
+        answer: "D. tinchlik",
+        options: [
+            "A. urush",
+            "B. jang",
+            "C. bezovtalik",
+            "D. tinchlik"
+        ]
+    },
+    {
+        numb: 16,
+        question: "prince",
+        answer: "C. shahzoda",
+        options: [
+            "A. qirol",
+            "B. ritsar",
+            "C. shahzoda",
+            "D. gersog"
+        ]
+    },
+    {
+        numb: 17,
+        question: "to protect",
+        answer: "A. himoya qilmoq",
+        options: [
+            "A. himoya qilmoq",
+            "B. hujum qilmoq",
+            "C. jarohatlamoq",
+            "D. yo'q qilmoq"
+        ]
+    },
+    {
+        numb: 18,
+        question: "to sense",
+        answer: "B. sezmoq",
+        options: [
+            "A. hidlamoq",
+            "B. sezmoq",
+            "C. tatib ko'rmoq",
+            "D. eshitmoq"
+        ]
+    },
+    {
+        numb: 19,
+        question: "sudden",
+        answer: "A. kutilmagan",
+        options: [
+            "A. kutilmagan",
+            "B. sekin",
+            "C. shiddatli",
+            "D. doimiy"
+        ]
+    },
+    {
+        numb: 20,
+        question: "therefore",
+        answer: "D. shu sababli",
+        options: [
+            "A. shunga qaramay",
+            "B. baribir",
+            "C. lekin",
+            "D. shu sababli"
+        ]
+    }
+];
