1a4a0b2d84384e38d68e6d1dc0d21d4160557702
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-06-25 01:00:03 +0500

    Create savollar8.js

---
+++ savollar8.js
@@ -0,0 +1,222 @@
+let savollar8 = [
+    {
+        numb: 1,
+        question: "to accept",
+        answer: "A. qabul qilmoq",
+        options: [
+            "A. qabul qilmoq",
+            "B. rad etmoq",
+            "C. bekor qilmoq",
+            "D. e'lon qilmoq"
+        ]
+    },
+    {
+        numb: 2,
+        question: "to arrange",
+        answer: "B. tartibga solmoq",
+        options: [
+            "A. tartibsiz qilmoq",
+            "B. tartibga solmoq",
+            "C. bekor qilmoq",
+            "D. o'zgartirmoq"
+        ]
+    },
+    {
+        numb: 3,
+        question: "to attend",
+        answer: "A. bormoq",
+        options: [
+            "A. bormoq",
+            "B. yo'qlamoq",
+            "C. beparvo bo'lmoq",
+            "D. unutmoq"
+        ]
+    },
+    {
+        numb: 4,
+        question: "to balance",
+        answer: "C. muvozanat saqlamoq",
+        options: [
+            "A. to'g'ri kelmoq",
+            "B. o'zgarmoq",
+            "C. muvozanat saqlamoq",
+            "D. qo'llab-quvvatlamoq"
+        ]
+    },
+    {
+        numb: 5,
+        question: "contrast",
+        answer: "D. katta farq",
+        options: [
+            "A. o'xshashlik",
+            "B. tenglik",
+            "C. umumiylik",
+            "D. katta farq"
+        ]
+    },
+    {
+        numb: 6,
+        question: "to encourage",
+        answer: "A. ruhlantirmoq",
+        options: [
+            "A. ruhlantirmoq",
+            "B. qoralamoq",
+            "C. oldini olmoq",
+            "D. e'tiborsiz qoldirmoq"
+        ]
+    },
+    {
+        numb: 7,
+        question: "familiar",
+        answer: "B. tanish",
+        options: [
+            "A. oila",
+            "B. tanish",
+            "C. yangi",
+            "D. begona"
+        ]
+    },
+    {
+        numb: 8,
+        question: "to grab",
+        answer: "C. olmoq",
+        options: [
+            "A. tashlamoq",
+            "B. bermoq",
+            "C. olmoq",
+            "D. o'zgartirmoq"
+        ]
+    },
+    {
+        numb: 9,
+        question: "to hang",
+        answer: "A. osmoq",
+        options: [
+            "A. osmoq",
+            "B. qo'ymoq",
+            "C. turgazmoq",
+            "D. ko'tarmoq"
+        ]
+    },
+    {
+        numb: 10,
+        question: "huge",
+        answer: "D. ulkan",
+        options: [
+            "A. kichik",
+            "B. o'rtacha",
+            "C. tor",
+            "D. ulkan"
+        ]
+    },
+    {
+        numb: 11,
+        question: "necessary",
+        answer: "B. zarur",
+        options: [
+            "A. ortiqcha",
+            "B. zarur",
+            "C. ixtiyoriy",
+            "D. majburiy"
+        ]
+    },
+    {
+        numb: 12,
+        question: "pattern",
+        answer: "C. uslub",
+        options: [
+            "A. jarayon",
+            "B. tavsiya",
+            "C. uslub",
+            "D. vosita"
+        ]
+    },
+    {
+        numb: 13,
+        question: "to propose",
+        answer: "B. taklif qilmoq",
+        options: [
+            "A. maslahat bermoq",
+            "B. taklif qilmoq",
+            "C. tanbeh bermoq",
+            "D. inkor qilmoq"
+        ]
+    },
+    {
+        numb: 14,
+        question: "purpose",
+        answer: "A. maqsad",
+        options: [
+            "A. maqsad",
+            "B. tasodif",
+            "C. tilak",
+            "D. orzu"
+        ]
+    },
+    {
+        numb: 15,
+        question: "to release",
+        answer: "B. ozod qilmoq",
+        options: [
+            "A. ushlab turmoq",
+            "B. ozod qilmoq",
+            "C. qamamoq",
+            "D. cheklamoq"
+        ]
+    },
+    {
+        numb: 16,
+        question: "to require",
+        answer: "C. talab qilmoq",
+        options: [
+            "A. joriy qilmoq",
+            "B. bildirmoq",
+            "C. talab qilmoq",
+            "D. taqiqlamoq"
+        ]
+    },
+    {
+        numb: 17,
+        question: "single",
+        answer: "D. bir dona",
+        options: [
+            "A. ozroq",
+            "B. bir nechta",
+            "C. begona",
+            "D. bir dona"
+        ]
+    },
+    {
+        numb: 18,
+        question: "success",
+        answer: "A. muvaffaqiyat",
+        options: [
+            "A. muvaffaqiyat",
+            "B. omad",
+            "C. quvonch",
+            "D. hursandchilik"
+        ]
+    },
+    {
+        numb: 19,
+        question: "to tear",
+        answer: "B. yirtmoq",
+        options: [
+            "A. tikmoq",
+            "B. yirtmoq",
+            "C. birlashtirmoq",
+            "D. yopishtirmoq"
+        ]
+    },
+    {
+        numb: 20,
+        question: "theory",
+        answer: "C. nazariya",
+        options: [
+            "A. amaliyot",
+            "B. holat",
+            "C. nazariya",
+            "D. tajriba"
+        ]
+    }
+];
