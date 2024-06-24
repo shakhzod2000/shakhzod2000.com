c7aa5b02df8aa48fb6e993c6a5083993b42fba30
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-06-25 01:00:24 +0500

    Create questions8.js

---
+++ questions8.js
@@ -0,0 +1,222 @@
+let questions8 = [
+    {
+        numb: 1,
+        question: "to accept",
+        answer: "A. akzeptieren",
+        options: [
+            "A. akzeptieren",
+            "B. ablehnen",
+            "C. stornieren",
+            "D. ankündigen"
+        ]
+    },
+    {
+        numb: 2,
+        question: "to arrange",
+        answer: "B. arrangieren",
+        options: [
+            "A. durcheinanderbringen",
+            "B. arrangieren",
+            "C. stornieren",
+            "D. ändern"
+        ]
+    },
+    {
+        numb: 3,
+        question: "to attend",
+        answer: "A. teilnehmen",
+        options: [
+            "A. teilnehmen",
+            "B. besuchen",
+            "C. ignorieren",
+            "D. vergessen"
+        ]
+    },
+    {
+        numb: 4,
+        question: "to balance",
+        answer: "C. balancieren",
+        options: [
+            "A. entsprechen",
+            "B. verändern",
+            "C. balancieren",
+            "D. unterstützen"
+        ]
+    },
+    {
+        numb: 5,
+        question: "contrast",
+        answer: "D. Kontrast",
+        options: [
+            "A. Ähnlichkeit",
+            "B. Gleichheit",
+            "C. Gemeinsamkeit",
+            "D. Kontrast"
+        ]
+    },
+    {
+        numb: 6,
+        question: "to encourage",
+        answer: "A. ermutigen",
+        options: [
+            "A. ermutigen",
+            "B. kritisieren",
+            "C. verhindern",
+            "D. ignorieren"
+        ]
+    },
+    {
+        numb: 7,
+        question: "familiar",
+        answer: "B. vertraut",
+        options: [
+            "A. Familie",
+            "B. vertraut",
+            "C. neu",
+            "D. fremd"
+        ]
+    },
+    {
+        numb: 8,
+        question: "to grab",
+        answer: "C. greifen",
+        options: [
+            "A. werfen",
+            "B. geben",
+            "C. greifen",
+            "D. ändern"
+        ]
+    },
+    {
+        numb: 9,
+        question: "to hang",
+        answer: "A. hängen",
+        options: [
+            "A. hängen",
+            "B. legen",
+            "C. aufstellen",
+            "D. heben"
+        ]
+    },
+    {
+        numb: 10,
+        question: "huge",
+        answer: "D. riesig",
+        options: [
+            "A. klein",
+            "B. mittelgroß",
+            "C. eng",
+            "D. riesig"
+        ]
+    },
+    {
+        numb: 11,
+        question: "necessary",
+        answer: "B. notwendig",
+        options: [
+            "A. überflüssig",
+            "B. notwendig",
+            "C. freiwillig",
+            "D. obligatorisch"
+        ]
+    },
+    {
+        numb: 12,
+        question: "pattern",
+        answer: "C. Muster",
+        options: [
+            "A. Prozess",
+            "B. Empfehlung",
+            "C. Muster",
+            "D. Methode"
+        ]
+    },
+    {
+        numb: 13,
+        question: "to propose",
+        answer: "B. vorschlagen",
+        options: [
+            "A. beraten",
+            "B. vorschlagen",
+            "C. tadeln",
+            "D. leugnen"
+        ]
+    },
+    {
+        numb: 14,
+        question: "purpose",
+        answer: "A. Zweck",
+        options: [
+            "A. Zweck",
+            "B. Zufall",
+            "C. Wunsch",
+            "D. Traum"
+        ]
+    },
+    {
+        numb: 15,
+        question: "to release",
+        answer: "B. freilassen",
+        options: [
+            "A. festhalten",
+            "B. freilassen",
+            "C. einsperren",
+            "D. einschränken"
+        ]
+    },
+    {
+        numb: 16,
+        question: "to require",
+        answer: "C. erfordern",
+        options: [
+            "A. einführen",
+            "B. mitteilen",
+            "C. erfordern",
+            "D. verbieten"
+        ]
+    },
+    {
+        numb: 17,
+        question: "single",
+        answer: "D. einzeln",
+        options: [
+            "A. wenige",
+            "B. mehrere",
+            "C. fremd",
+            "D. einzeln"
+        ]
+    },
+    {
+        numb: 18,
+        question: "success",
+        answer: "A. Erfolg",
+        options: [
+            "A. Erfolg",
+            "B. Glück",
+            "C. Freude",
+            "D. Zufriedenheit"
+        ]
+    },
+    {
+        numb: 19,
+        question: "to tear",
+        answer: "B. zerreißen",
+        options: [
+            "A. nähen",
+            "B. zerreißen",
+            "C. verbinden",
+            "D. kleben"
+        ]
+    },
+    {
+        numb: 20,
+        question: "theory",
+        answer: "C. Theorie",
+        options: [
+            "A. Praxis",
+            "B. Zustand",
+            "C. Theorie",
+            "D. Experiment"
+        ]
+    }
+];
