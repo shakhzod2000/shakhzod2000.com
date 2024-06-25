fff7c3c6ad88eccd477fc3aacb7fcae4e8810731
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-06-25 20:18:45 +0500

    Create savollar11.js

---
+++ savollar11.js
@@ -0,0 +1,222 @@
+let savollar11 = [
+    {
+        numb: 1,
+        question: "anymore",
+        answer: "A. ortiq",
+        options: [
+            "A. ortiq",
+            "B. hanuzgacha",
+            "C. yana bir marta",
+            "D. ko'proq"
+        ]
+    },
+    {
+        numb: 2,
+        question: "asleep",
+        answer: "B. uxlayotgan",
+        options: [
+            "A. uyg'oq",
+            "B. uxlayotgan",
+            "C. charchagan",
+            "D. dam olgan"
+        ]
+    },
+    {
+        numb: 3,
+        question: "berry",
+        answer: "A. malina",
+        options: [
+            "A. malina",
+            "B. yong'oq",
+            "C. olma",
+            "D. gul"
+        ]
+    },
+    {
+        numb: 4,
+        question: "to collect",
+        answer: "C. to'plamoq",
+        options: [
+            "A. yo'qotmoq",
+            "B. tarqatmoq",
+            "C. to'plamoq",
+            "D. sotib olmoq"
+        ]
+    },
+    {
+        numb: 5,
+        question: "to compete",
+        answer: "D. musobaqalashmoq",
+        options: [
+            "A. hamkorlik qilmoq",
+            "B. yordam bermoq",
+            "C. taslim bo'lmoq",
+            "D. musobaqalashmoq"
+        ]
+    },
+    {
+        numb: 6,
+        question: "conversation",
+        answer: "A. suhbat",
+        options: [
+            "A. suhbat",
+            "B. monolog",
+            "C. muhokama",
+            "D. ma'ruza"
+        ]
+    },
+    {
+        numb: 7,
+        question: "creature",
+        answer: "C. mavjudot",
+        options: [
+            "A. o'simlik",
+            "B. bino",
+            "C. mavjudot",
+            "D. mashina"
+        ]
+    },
+    {
+        numb: 8,
+        question: "decision",
+        answer: "B. qaror",
+        options: [
+            "A. shubha",
+            "B. qaror",
+            "C. imkoniyat",
+            "D. afsus"
+        ]
+    },
+    {
+        numb: 9,
+        question: "either",
+        answer: "D. yo",
+        options: [
+            "A. na",
+            "B. ham",
+            "C. kabi",
+            "D. yo"
+        ]
+    },
+    {
+        numb: 10,
+        question: "forest",
+        answer: "A. o'rmon",
+        options: [
+            "A. o'rmon",
+            "B. sahro",
+            "C. okean",
+            "D. shahar"
+        ]
+    },
+    {
+        numb: 11,
+        question: "ground",
+        answer: "B. yer",
+        options: [
+            "A. osmon",
+            "B. yer",
+            "C. tog'",
+            "D. suv"
+        ]
+    },
+    {
+        numb: 12,
+        question: "to introduce",
+        answer: "A. tanishtirmoq",
+        options: [
+            "A. tanishtirmoq",
+            "B. yashirmoq",
+            "C. xayrlashmoq",
+            "D. e'tiborsiz qoldirmoq"
+        ]
+    },
+    {
+        numb: 13,
+        question: "to marry",
+        answer: "C. turmush qurmoq",
+        options: [
+            "A. ajrashmoq",
+            "B. sevmoq",
+            "C. turmush qurmoq",
+            "D. uchrashmoq"
+        ]
+    },
+    {
+        numb: 14,
+        question: "to prepare",
+        answer: "B. tayyorlamoq",
+        options: [
+            "A. unutmoq",
+            "B. tayyorlamoq",
+            "C. kechiktirmoq",
+            "D. rad etmoq"
+        ]
+    },
+    {
+        numb: 15,
+        question: "to sail",
+        answer: "A. qayiqda suzmoq",
+        options: [
+            "A. qayiqda suzmoq",
+            "B. uchmoq",
+            "C. haydamoq",
+            "D. cho'milmoq"
+        ]
+    },
+    {
+        numb: 16,
+        question: "serious",
+        answer: "C. jiddiy",
+        options: [
+            "A. kulgili",
+            "B. do'stona",
+            "C. jiddiy",
+            "D. xavfsiz"
+        ]
+    },
+    {
+        numb: 17,
+        question: "to spend",
+        answer: "D. o'tkazmoq",
+        options: [
+            "A. tejamoq",
+            "B. topmoq",
+            "C. yo'qotmoq",
+            "D. o'tkazmoq"
+        ]
+    },
+    {
+        numb: 18,
+        question: "strange",
+        answer: "B. g'alati",
+        options: [
+            "A. oddiy",
+            "B. g'alati",
+            "C. odatiy",
+            "D. an'anaviy"
+        ]
+    },
+    {
+        numb: 19,
+        question: "truth",
+        answer: "A. haqiqat",
+        options: [
+            "A. haqiqat",
+            "B. yolg'on",
+            "C. mish-mish",
+            "D. fantastika"
+        ]
+    },
+    {
+        numb: 20,
+        question: "to wake",
+        answer: "C. uyg'onmoq",
+        options: [
+            "A. uxlamoq",
+            "B. tush ko'rmoq",
+            "C. uyg'onmoq",
+            "D. dam olmoq"
+        ]
+    }
+];
