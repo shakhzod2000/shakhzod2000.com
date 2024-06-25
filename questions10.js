fb84cf47280aea64e8d6833887d82b5fd28b6c11
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-06-25 19:36:42 +0500

    Create questions10.js

---
+++ questions10.js
@@ -0,0 +1,222 @@
+let questions10 = [
+    {
+        numb: 1,
+        question: "benefit",
+        answer: "A. Vorteil",
+        options: [
+            "A. Vorteil",
+            "B. Nachteil",
+            "C. Problem",
+            "D. Hindernis"
+        ]
+    },
+    {
+        numb: 2,
+        question: "certain",
+        answer: "B. sicher",
+        options: [
+            "A. unsicher",
+            "B. sicher",
+            "C. zweifelnd",
+            "D. unklar"
+        ]
+    },
+    {
+        numb: 3,
+        question: "chance",
+        answer: "A. Gelegenheit",
+        options: [
+            "A. Gelegenheit",
+            "B. Risiko",
+            "C. Verlust",
+            "D. Nachteil"
+        ]
+    },
+    {
+        numb: 4,
+        question: "effect",
+        answer: "C. Wirkung",
+        options: [
+            "A. Ursache",
+            "B. Grund",
+            "C. Wirkung",
+            "D. Ergebnis"
+        ]
+    },
+    {
+        numb: 5,
+        question: "essential",
+        answer: "D. wesentlich",
+        options: [
+            "A. unwichtig",
+            "B. nebensächlich",
+            "C. irrelevant",
+            "D. wesentlich"
+        ]
+    },
+    {
+        numb: 6,
+        question: "far",
+        answer: "B. weit",
+        options: [
+            "A. nah",
+            "B. weit",
+            "C. eng",
+            "D. kurz"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to focus",
+        answer: "A. fokussieren",
+        options: [
+            "A. fokussieren",
+            "B. ignorieren",
+            "C. ablenken",
+            "D. verwirren"
+        ]
+    },
+    {
+        numb: 8,
+        question: "function",
+        answer: "D. Funktion",
+        options: [
+            "A. Fehler",
+            "B. Schaden",
+            "C. Problem",
+            "D. Funktion"
+        ]
+    },
+    {
+        numb: 9,
+        question: "grass",
+        answer: "C. Gras",
+        options: [
+            "A. Baum",
+            "B. Busch",
+            "C. Gras",
+            "D. Blume"
+        ]
+    },
+    {
+        numb: 10,
+        question: "to guard",
+        answer: "B. bewachen",
+        options: [
+            "A. ignorieren",
+            "B. bewachen",
+            "C. gefährden",
+            "D. vernachlässigen"
+        ]
+    },
+    {
+        numb: 11,
+        question: "image",
+        answer: "A. Bild",
+        options: [
+            "A. Bild",
+            "B. Geräusch",
+            "C. Geruch",
+            "D. Gefühl"
+        ]
+    },
+    {
+        numb: 12,
+        question: "immediate",
+        answer: "C. sofortig",
+        options: [
+            "A. verzögert",
+            "B. allmählich",
+            "C. sofortig",
+            "D. langsam"
+        ]
+    },
+    {
+        numb: 13,
+        question: "primary",
+        answer: "D. hauptsächlich",
+        options: [
+            "A. nebensächlich",
+            "B. unwichtig",
+            "C. zweitrangig",
+            "D. hauptsächlich"
+        ]
+    },
+    {
+        numb: 14,
+        question: "proud",
+        answer: "B. stolz",
+        options: [
+            "A. beschämt",
+            "B. stolz",
+            "C. enttäuscht",
+            "D. traurig"
+        ]
+    },
+    {
+        numb: 15,
+        question: "to remain",
+        answer: "A. bleiben",
+        options: [
+            "A. bleiben",
+            "B. gehen",
+            "C. verschwinden",
+            "D. abweichen"
+        ]
+    },
+    {
+        numb: 16,
+        question: "to rest",
+        answer: "C. ausruhen",
+        options: [
+            "A. arbeiten",
+            "B. laufen",
+            "C. ausruhen",
+            "D. rennen"
+        ]
+    },
+    {
+        numb: 17,
+        question: "separate",
+        answer: "D. getrennt",
+        options: [
+            "A. verbunden",
+            "B. zusammen",
+            "C. vereint",
+            "D. getrennt"
+        ]
+    },
+    {
+        numb: 18,
+        question: "site",
+        answer: "A. Ort",
+        options: [
+            "A. Ort",
+            "B. Zeit",
+            "C. Person",
+            "D. Objekt"
+        ]
+    },
+    {
+        numb: 19,
+        question: "tail",
+        answer: "B. Schwanz",
+        options: [
+            "A. Kopf",
+            "B. Schwanz",
+            "C. Pfote",
+            "D. Ohr"
+        ]
+    },
+    {
+        numb: 20,
+        question: "trouble",
+        answer: "C. Problem",
+        options: [
+            "A. Lösung",
+            "B. Freude",
+            "C. Problem",
+            "D. Erfolg"
+        ]
+    }
+];
