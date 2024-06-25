f2cea7fa9a1b3efee9e0f89c2c53ff1c2b05a4a6
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-06-25 19:38:32 +0500

    Create savollar10.js

---
+++ savollar10.js
@@ -0,0 +1,222 @@
+let savollar10 = [
+    {
+        numb: 1,
+        question: "benefit",
+        answer: "A. foyda",
+        options: [
+            "A. foyda",
+            "B. zarar",
+            "C. muammo",
+            "D. to'siq"
+        ]
+    },
+    {
+        numb: 2,
+        question: "certain",
+        answer: "B. ishonchi komil",
+        options: [
+            "A. ikkilangan",
+            "B. ishonchi komil",
+            "C. shubhali",
+            "D. noma'lum"
+        ]
+    },
+    {
+        numb: 3,
+        question: "chance",
+        answer: "A. imkoniyat",
+        options: [
+            "A. imkoniyat",
+            "B. xavf",
+            "C. yo'qotish",
+            "D. zarar"
+        ]
+    },
+    {
+        numb: 4,
+        question: "effect",
+        answer: "C. ta'sir",
+        options: [
+            "A. sabab",
+            "B. asos",
+            "C. ta'sir",
+            "D. natija"
+        ]
+    },
+    {
+        numb: 5,
+        question: "essential",
+        answer: "D. muhim",
+        options: [
+            "A. ahamiyatsiz",
+            "B. ikkilamchi",
+            "C. befarq",
+            "D. muhim"
+        ]
+    },
+    {
+        numb: 6,
+        question: "far",
+        answer: "B. uzoq",
+        options: [
+            "A. yaqin",
+            "B. uzoq",
+            "C. tor",
+            "D. qisqa"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to focus",
+        answer: "A. diqqatni qaratmoq",
+        options: [
+            "A. diqqatni qaratmoq",
+            "B. hayol qilmoq",
+            "C. chalg'imoq",
+            "D. anglamoq"
+        ]
+    },
+    {
+        numb: 8,
+        question: "function",
+        answer: "D. vazifa",
+        options: [
+            "A. xato",
+            "B. zarar",
+            "C. muammo",
+            "D. vazifa"
+        ]
+    },
+    {
+        numb: 9,
+        question: "grass",
+        answer: "C. o't",
+        options: [
+            "A. daraxt",
+            "B. buta",
+            "C. o't",
+            "D. gul"
+        ]
+    },
+    {
+        numb: 10,
+        question: "to guard",
+        answer: "B. qo'riqlamoq",
+        options: [
+            "A. e'tiborsiz qoldirmoq",
+            "B. qo'riqlamoq",
+            "C. xavfga solmoq",
+            "D. beparvo bo'lmoq"
+        ]
+    },
+    {
+        numb: 11,
+        question: "image",
+        answer: "A. rasm",
+        options: [
+            "A. rasm",
+            "B. ovoz",
+            "C. hid",
+            "D. tuyg'u"
+        ]
+    },
+    {
+        numb: 12,
+        question: "immediate",
+        answer: "C. tezkor",
+        options: [
+            "A. kechikkan",
+            "B. keyinroq",
+            "C. tezkor",
+            "D. sekin"
+        ]
+    },
+    {
+        numb: 13,
+        question: "primary",
+        answer: "D. asosiy",
+        options: [
+            "A. ikkinchi darajali",
+            "B. ahamiyatsiz",
+            "C. ikkilamchi",
+            "D. asosiy"
+        ]
+    },
+    {
+        numb: 14,
+        question: "proud",
+        answer: "B. faxrlangan",
+        options: [
+            "A. uyatli",
+            "B. faxrlangan",
+            "C. xafa",
+            "D. qayg'uli"
+        ]
+    },
+    {
+        numb: 15,
+        question: "to remain",
+        answer: "A. qolmoq",
+        options: [
+            "A. qolmoq",
+            "B. ketmoq",
+            "C. yo'qolmoq",
+            "D. chetga chiqmoq"
+        ]
+    },
+    {
+        numb: 16,
+        question: "to rest",
+        answer: "C. dam olmoq",
+        options: [
+            "A. ishlamoq",
+            "B. yugurmoq",
+            "C. dam olmoq",
+            "D. chopmoq"
+        ]
+    },
+    {
+        numb: 17,
+        question: "separate",
+        answer: "D. alohida",
+        options: [
+            "A. birlashgan",
+            "B. birga",
+            "C. birlashgan",
+            "D. alohida"
+        ]
+    },
+    {
+        numb: 18,
+        question: "site",
+        answer: "A. joy",
+        options: [
+            "A. joy",
+            "B. vaqt",
+            "C. shaxs",
+            "D. obyekt"
+        ]
+    },
+    {
+        numb: 19,
+        question: "tail",
+        answer: "B. dum",
+        options: [
+            "A. bosh",
+            "B. dum",
+            "C. panja",
+            "D. quloq"
+        ]
+    },
+    {
+        numb: 20,
+        question: "trouble",
+        answer: "C. muammo",
+        options: [
+            "A. yechim",
+            "B. quvonch",
+            "C. muammo",
+            "D. muvaffaqiyat"
+        ]
+    }
+];
