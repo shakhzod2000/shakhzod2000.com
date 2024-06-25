1d6a6bd6fb1c5367c1af103fe324db98f2bbcfa1
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-06-25 19:37:12 +0500

    Create savollar9.js

---
+++ savollar9.js
@@ -0,0 +1,222 @@
+let savollar9 = [
+    {
+        numb: 1,
+        question: "against",
+        answer: "A. qarshi",
+        options: [
+            "A. qarshi",
+            "B. uchun",
+            "C. yonida",
+            "D. ortida"
+        ]
+    },
+    {
+        numb: 2,
+        question: "beach",
+        answer: "B. plyaj",
+        options: [
+            "A. tog'",
+            "B. plyaj",
+            "C. o'rmon",
+            "D. ko'l"
+        ]
+    },
+    {
+        numb: 3,
+        question: "to damage",
+        answer: "A. shikastlamoq",
+        options: [
+            "A. shikastlamoq",
+            "B. tuzatmoq",
+            "C. qurmoq",
+            "D. e'tiborsiz qoldirmoq"
+        ]
+    },
+    {
+        numb: 4,
+        question: "to discover",
+        answer: "C. kashf qilmoq",
+        options: [
+            "A. yo'qotmoq",
+            "B. vayron qilmoq",
+            "C. kashf qilmoq",
+            "D. yashirmoq"
+        ]
+    },
+    {
+        numb: 5,
+        question: "emotion",
+        answer: "D. his-tuyg'u",
+        options: [
+            "A. fikr",
+            "B. harakat",
+            "C. xotira",
+            "D. his-tuyg'u"
+        ]
+    },
+    {
+        numb: 6,
+        question: "to fix",
+        answer: "B. tuzatmoq",
+        options: [
+            "A. zarar yetkazmoq",
+            "B. tuzatmoq",
+            "C. kechiktirmoq",
+            "D. e'tiborsiz qoldirmoq"
+        ]
+    },
+    {
+        numb: 7,
+        question: "frank",
+        answer: "A. samimiy",
+        options: [
+            "A. samimiy",
+            "B. muloyim",
+            "C. tortinchoq",
+            "D. yaxshi"
+        ]
+    },
+    {
+        numb: 8,
+        question: "to identify",
+        answer: "C. aniqlamoq",
+        options: [
+            "A. yashirmoq",
+            "B. yo'qotmoq",
+            "C. aniqlamoq",
+            "D. unutmoq"
+        ]
+    },
+    {
+        numb: 9,
+        question: "island",
+        answer: "B. orol",
+        options: [
+            "A. qit'a",
+            "B. orol",
+            "C. ko'l",
+            "D. cho'l"
+        ]
+    },
+    {
+        numb: 10,
+        question: "ocean",
+        answer: "D. okean",
+        options: [
+            "A. daryo",
+            "B. ko'l",
+            "C. sharshara",
+            "D. okean"
+        ]
+    },
+    {
+        numb: 11,
+        question: "perhaps",
+        answer: "A. balki",
+        options: [
+            "A. balki",
+            "B. aniq",
+            "C. hech qachon",
+            "D. har doim"
+        ]
+    },
+    {
+        numb: 12,
+        question: "pleasant",
+        answer: "C. yoqimli",
+        options: [
+            "A. yoqimsiz",
+            "B. zerikarli",
+            "C. yoqimli",
+            "D. stressli"
+        ]
+    },
+    {
+        numb: 13,
+        question: "to prevent",
+        answer: "A. to'sqinlik qilmoq",
+        options: [
+            "A. to'sqinlik qilmoq",
+            "B. sabab bo'lmoq",
+            "C. qo'llab-quvvatlamoq",
+            "D. rag'batlantirmoq"
+        ]
+    },
+    {
+        numb: 14,
+        question: "rock",
+        answer: "B. tosh",
+        options: [
+            "A. qum",
+            "B. tosh",
+            "C. daraxt",
+            "D. suv"
+        ]
+    },
+    {
+        numb: 15,
+        question: "to save",
+        answer: "A. saqlamoq",
+        options: [
+            "A. saqlamoq",
+            "B. vayron qilmoq",
+            "C. yo'qotmoq",
+            "D. sotmoq"
+        ]
+    },
+    {
+        numb: 16,
+        question: "to step",
+        answer: "D. qadam tashlamoq",
+        options: [
+            "A. yugurmoq",
+            "B. sakramoq",
+            "C. uchmoq",
+            "D. qadam tashlamoq"
+        ]
+    },
+    {
+        numb: 17,
+        question: "still",
+        answer: "B. hali ham",
+        options: [
+            "A. hech qachon",
+            "B. hali ham",
+            "C. to'satdan",
+            "D. darhol"
+        ]
+    },
+    {
+        numb: 18,
+        question: "taste",
+        answer: "A. ta'm",
+        options: [
+            "A. ta'm",
+            "B. rang",
+            "C. hid",
+            "D. tovush"
+        ]
+    },
+    {
+        numb: 19,
+        question: "to throw",
+        answer: "C. otmoq",
+        options: [
+            "A. ushlamoq",
+            "B. ushlab turmoq",
+            "C. otmoq",
+            "D. ko'tarmoq"
+        ]
+    },
+    {
+        numb: 20,
+        question: "wave",
+        answer: "D. to'lqin",
+        options: [
+            "A. shamol",
+            "B. bo'ron",
+            "C. chaqmoq",
+            "D. to'lqin"
+        ]
+    }
+];
