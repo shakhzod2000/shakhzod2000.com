943a0ef9c4d28c76f4e19da59e933134b5eb8f9e
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-06-25 19:36:15 +0500

    Create questions9.js

---
+++ questions9.js
@@ -0,0 +1,222 @@
+let questions9 = [
+    {
+        numb: 1,
+        question: "against",
+        answer: "A. gegen",
+        options: [
+            "A. gegen",
+            "B. für",
+            "C. neben",
+            "D. hinter"
+        ]
+    },
+    {
+        numb: 2,
+        question: "beach",
+        answer: "B. Strand",
+        options: [
+            "A. Berg",
+            "B. Strand",
+            "C. Wald",
+            "D. See"
+        ]
+    },
+    {
+        numb: 3,
+        question: "to damage",
+        answer: "A. beschädigen",
+        options: [
+            "A. beschädigen",
+            "B. reparieren",
+            "C. bauen",
+            "D. ignorieren"
+        ]
+    },
+    {
+        numb: 4,
+        question: "to discover",
+        answer: "C. entdecken",
+        options: [
+            "A. verlieren",
+            "B. zerstören",
+            "C. entdecken",
+            "D. verstecken"
+        ]
+    },
+    {
+        numb: 5,
+        question: "emotion",
+        answer: "D. Gefühl",
+        options: [
+            "A. Gedanke",
+            "B. Handlung",
+            "C. Erinnerung",
+            "D. Gefühl"
+        ]
+    },
+    {
+        numb: 6,
+        question: "to fix",
+        answer: "B. reparieren",
+        options: [
+            "A. beschädigen",
+            "B. reparieren",
+            "C. verschieben",
+            "D. ignorieren"
+        ]
+    },
+    {
+        numb: 7,
+        question: "frank",
+        answer: "A. ehrlich",
+        options: [
+            "A. ehrlich",
+            "B. höflich",
+            "C. schüchtern",
+            "D. böse"
+        ]
+    },
+    {
+        numb: 8,
+        question: "to identify",
+        answer: "C. identifizieren",
+        options: [
+            "A. verstecken",
+            "B. verlieren",
+            "C. identifizieren",
+            "D. vergessen"
+        ]
+    },
+    {
+        numb: 9,
+        question: "island",
+        answer: "B. Insel",
+        options: [
+            "A. Kontinent",
+            "B. Insel",
+            "C. See",
+            "D. Wüste"
+        ]
+    },
+    {
+        numb: 10,
+        question: "ocean",
+        answer: "D. Ozean",
+        options: [
+            "A. Fluss",
+            "B. See",
+            "C. Wasserfall",
+            "D. Ozean"
+        ]
+    },
+    {
+        numb: 11,
+        question: "perhaps",
+        answer: "A. vielleicht",
+        options: [
+            "A. vielleicht",
+            "B. sicher",
+            "C. niemals",
+            "D. immer"
+        ]
+    },
+    {
+        numb: 12,
+        question: "pleasant",
+        answer: "C. angenehm",
+        options: [
+            "A. unangenehm",
+            "B. langweilig",
+            "C. angenehm",
+            "D. stressig"
+        ]
+    },
+    {
+        numb: 13,
+        question: "to prevent",
+        answer: "A. verhindern",
+        options: [
+            "A. verhindern",
+            "B. verursachen",
+            "C. unterstützen",
+            "D. fördern"
+        ]
+    },
+    {
+        numb: 14,
+        question: "rock",
+        answer: "B. Felsen",
+        options: [
+            "A. Sand",
+            "B. Felsen",
+            "C. Baum",
+            "D. Wasser"
+        ]
+    },
+    {
+        numb: 15,
+        question: "to save",
+        answer: "A. retten",
+        options: [
+            "A. retten",
+            "B. zerstören",
+            "C. verlieren",
+            "D. verkaufen"
+        ]
+    },
+    {
+        numb: 16,
+        question: "to step",
+        answer: "D. treten",
+        options: [
+            "A. rennen",
+            "B. springen",
+            "C. fliegen",
+            "D. treten"
+        ]
+    },
+    {
+        numb: 17,
+        question: "still",
+        answer: "B. immer noch",
+        options: [
+            "A. niemals",
+            "B. immer noch",
+            "C. plötzlich",
+            "D. sofort"
+        ]
+    },
+    {
+        numb: 18,
+        question: "taste",
+        answer: "A. Geschmack",
+        options: [
+            "A. Geschmack",
+            "B. Farbe",
+            "C. Geruch",
+            "D. Geräusch"
+        ]
+    },
+    {
+        numb: 19,
+        question: "to throw",
+        answer: "C. werfen",
+        options: [
+            "A. fangen",
+            "B. halten",
+            "C. werfen",
+            "D. heben"
+        ]
+    },
+    {
+        numb: 20,
+        question: "wave",
+        answer: "D. Welle",
+        options: [
+            "A. Wind",
+            "B. Sturm",
+            "C. Blitz",
+            "D. Welle"
+        ]
+    }
+];
