9a187927988f12ae4eb94da555015165c88a670e
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-06-27 00:29:23 +0500

    Create savollar12.js

---
+++ savollar12.js
@@ -0,0 +1,222 @@
+let savollar12 = [
+    {
+        numb: 1,
+        question: "alone",
+        answer: "A. yolg'iz",
+        options: [
+            "A. yolg'iz",
+            "B. birga",
+            "C. hamroh",
+            "D. samimiy"
+        ]
+    },
+    {
+        numb: 2,
+        question: "apartment",
+        answer: "B. kvartira",
+        options: [
+            "A. uy",
+            "B. kvartira",
+            "C. xona",
+            "D. ofis"
+        ]
+    },
+    {
+        numb: 3,
+        question: "article",
+        answer: "C. maqola",
+        options: [
+            "A. kitob",
+            "B. yozuv",
+            "C. maqola",
+            "D. eylon"
+        ]
+    },
+    {
+        numb: 4,
+        question: "artist",
+        answer: "D. rassom",
+        options: [
+            "A. yozuvchi",
+            "B. musiqachi",
+            "C. raqqos",
+            "D. rassom"
+        ]
+    },
+    {
+        numb: 5,
+        question: "attitude",
+        answer: "A. munosabat",
+        options: [
+            "A. munosabat",
+            "B. xatti-harakat",
+            "C. ko'rinish",
+            "D. kayfiyat"
+        ]
+    },
+    {
+        numb: 6,
+        question: "to compare",
+        answer: "B. taqqoslamoq",
+        options: [
+            "A. ajratish",
+            "B. taqqoslamoq",
+            "C. baholash",
+            "D. qaror qabul qilish"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to judge",
+        answer: "C. baho bermoq",
+        options: [
+            "A. eytiborsiz qoldirish",
+            "B. oldini olish",
+            "C. baho bermoq",
+            "D. qadrlash"
+        ]
+    },
+    {
+        numb: 8,
+        question: "magazine",
+        answer: "A. jurnal",
+        options: [
+            "A. jurnal",
+            "B. gazeta",
+            "C. kitob",
+            "D. buklet"
+        ]
+    },
+    {
+        numb: 9,
+        question: "material",
+        answer: "D. material",
+        options: [
+            "A. asbob",
+            "B. mashina",
+            "C. jihoz",
+            "D. material"
+        ]
+    },
+    {
+        numb: 10,
+        question: "meal",
+        answer: "B. ovqatlanish vaqti",
+        options: [
+            "A. taom",
+            "B. ovqatlanish vaqti",
+            "C. yengil ovqat",
+            "D. ichimlik"
+        ]
+    },
+    {
+        numb: 11,
+        question: "method",
+        answer: "A. usul",
+        options: [
+            "A. usul",
+            "B. muammo",
+            "C. reja",
+            "D. g'oya"
+        ]
+    },
+    {
+        numb: 12,
+        question: "neighbor",
+        answer: "B. qo'shni",
+        options: [
+            "A. do'st",
+            "B. qo'shni",
+            "C. hamkasb",
+            "D. qarindosh"
+        ]
+    },
+    {
+        numb: 13,
+        question: "professional",
+        answer: "C. kasbiy",
+        options: [
+            "A. havaskor",
+            "B. talaba",
+            "C. kasbiy",
+            "D. boshlovchi"
+        ]
+    },
+    {
+        numb: 14,
+        question: "profit",
+        answer: "D. foyda",
+        options: [
+            "A. zarar",
+            "B. xarajat",
+            "C. qarz",
+            "D. foyda"
+        ]
+    },
+    {
+        numb: 15,
+        question: "quality",
+        answer: "B. sifat",
+        options: [
+            "A. miqdor",
+            "B. sifat",
+            "C. narx",
+            "D. uslub"
+        ]
+    },
+    {
+        numb: 16,
+        question: "shape",
+        answer: "A. shakl",
+        options: [
+            "A. shakl",
+            "B. rang",
+            "C. o'lcham",
+            "D. tuzilish"
+        ]
+    },
+    {
+        numb: 17,
+        question: "space",
+        answer: "D. bo'sh joy",
+        options: [
+            "A. obyekt",
+            "B. mebel",
+            "C. rasm",
+            "D. bo'sh joy"
+        ]
+    },
+    {
+        numb: 18,
+        question: "stair",
+        answer: "B. zina",
+        options: [
+            "A. eshik",
+            "B. zina",
+            "C. deraza",
+            "D. balkon"
+        ]
+    },
+    {
+        numb: 19,
+        question: "symbol",
+        answer: "C. ramz",
+        options: [
+            "A. ism",
+            "B. harf",
+            "C. ramz",
+            "D. raqam"
+        ]
+    },
+    {
+        numb: 20,
+        question: "thin",
+        answer: "A. ozg'in",
+        options: [
+            "A. ozg'in",
+            "B. semiz",
+            "C. keng",
+            "D. tor"
+        ]
+    }
+];
