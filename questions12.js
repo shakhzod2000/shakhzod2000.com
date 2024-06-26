dab5ee42dcf8602904136a9c732564aeba9056dc
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-06-27 00:30:13 +0500

    Create questions12.js

---
+++ questions12.js
@@ -0,0 +1,222 @@
+let questions12 = [
+    {
+        numb: 1,
+        question: "alone",
+        answer: "A. allein",
+        options: [
+            "A. allein",
+            "B. zusammen",
+            "C. begleitet",
+            "D. gesellig"
+        ]
+    },
+    {
+        numb: 2,
+        question: "apartment",
+        answer: "B. Wohnung",
+        options: [
+            "A. Haus",
+            "B. Wohnung",
+            "C. Zimmer",
+            "D. Büro"
+        ]
+    },
+    {
+        numb: 3,
+        question: "article",
+        answer: "C. Artikel",
+        options: [
+            "A. Buch",
+            "B. Notiz",
+            "C. Artikel",
+            "D. Anzeige"
+        ]
+    },
+    {
+        numb: 4,
+        question: "artist",
+        answer: "D. Künstler",
+        options: [
+            "A. Schriftsteller",
+            "B. Musiker",
+            "C. Tänzer",
+            "D. Künstler"
+        ]
+    },
+    {
+        numb: 5,
+        question: "attitude",
+        answer: "A. Einstellung",
+        options: [
+            "A. Einstellung",
+            "B. Verhalten",
+            "C. Aussehen",
+            "D. Stimmung"
+        ]
+    },
+    {
+        numb: 6,
+        question: "to compare",
+        answer: "B. vergleichen",
+        options: [
+            "A. trennen",
+            "B. vergleichen",
+            "C. bewerten",
+            "D. entscheiden"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to judge",
+        answer: "C. beurteilen",
+        options: [
+            "A. ignorieren",
+            "B. vermeiden",
+            "C. beurteilen",
+            "D. schätzen"
+        ]
+    },
+    {
+        numb: 8,
+        question: "magazine",
+        answer: "A. Zeitschrift",
+        options: [
+            "A. Zeitschrift",
+            "B. Zeitung",
+            "C. Buch",
+            "D. Broschüre"
+        ]
+    },
+    {
+        numb: 9,
+        question: "material",
+        answer: "D. Material",
+        options: [
+            "A. Werkzeug",
+            "B. Maschine",
+            "C. Ausrüstung",
+            "D. Material"
+        ]
+    },
+    {
+        numb: 10,
+        question: "meal",
+        answer: "B. Mahlzeit",
+        options: [
+            "A. Gericht",
+            "B. Mahlzeit",
+            "C. Snack",
+            "D. Getränk"
+        ]
+    },
+    {
+        numb: 11,
+        question: "method",
+        answer: "A. Methode",
+        options: [
+            "A. Methode",
+            "B. Problem",
+            "C. Plan",
+            "D. Idee"
+        ]
+    },
+    {
+        numb: 12,
+        question: "neighbor",
+        answer: "B. Nachbar",
+        options: [
+            "A. Freund",
+            "B. Nachbar",
+            "C. Kollege",
+            "D. Verwandter"
+        ]
+    },
+    {
+        numb: 13,
+        question: "professional",
+        answer: "C. professionell",
+        options: [
+            "A. Amateur",
+            "B. Schüler",
+            "C. professionell",
+            "D. Anfänger"
+        ]
+    },
+    {
+        numb: 14,
+        question: "profit",
+        answer: "D. Gewinn",
+        options: [
+            "A. Verlust",
+            "B. Ausgabe",
+            "C. Schulden",
+            "D. Gewinn"
+        ]
+    },
+    {
+        numb: 15,
+        question: "quality",
+        answer: "B. Qualität",
+        options: [
+            "A. Menge",
+            "B. Qualität",
+            "C. Preis",
+            "D. Stil"
+        ]
+    },
+    {
+        numb: 16,
+        question: "shape",
+        answer: "A. Form",
+        options: [
+            "A. Form",
+            "B. Farbe",
+            "C. Größe",
+            "D. Textur"
+        ]
+    },
+    {
+        numb: 17,
+        question: "space",
+        answer: "D. Raum",
+        options: [
+            "A. Objekt",
+            "B. Möbel",
+            "C. Bild",
+            "D. Raum"
+        ]
+    },
+    {
+        numb: 18,
+        question: "stair",
+        answer: "B. Treppe",
+        options: [
+            "A. Tür",
+            "B. Treppe",
+            "C. Fenster",
+            "D. Balkon"
+        ]
+    },
+    {
+        numb: 19,
+        question: "symbol",
+        answer: "C. Symbol",
+        options: [
+            "A. Name",
+            "B. Buchstabe",
+            "C. Symbol",
+            "D. Zahl"
+        ]
+    },
+    {
+        numb: 20,
+        question: "thin",
+        answer: "A. dünn",
+        options: [
+            "A. dünn",
+            "B. dick",
+            "C. breit",
+            "D. schmal"
+        ]
+    }
+];
