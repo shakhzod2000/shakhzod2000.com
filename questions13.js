8a3567b601162dd970429a44491ad526f6e72210
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-06-28 01:01:05 +0500

    Create questions13.js

---
+++ questions13.js
@@ -0,0 +1,222 @@
+let questions13 = [
+    {
+        numb: 1,
+        question: "blood",
+        answer: "A. Blut",
+        options: [
+            "A. Blut",
+            "B. Wunde",
+            "C. Verletzung",
+            "D. Infektion"
+        ]
+    },
+    {
+        numb: 2,
+        question: "to burn",
+        answer: "B. brennen",
+        options: [
+            "A. löschen",
+            "B. brennen",
+            "C. ersticken",
+            "D. fritieren"
+        ]
+    },
+    {
+        numb: 3,
+        question: "cell",
+        answer: "D. Zelle",
+        options: [
+            "A. Kiste",
+            "B. Schachtel",
+            "C. Raum",
+            "D. Zelle"
+        ]
+    },
+    {
+        numb: 4,
+        question: "to contain",
+        answer: "B. enthalten",
+        options: [
+            "A. ausschließen",
+            "B. enthalten",
+            "C. verbergen",
+            "D. vermeiden"
+        ]
+    },
+    {
+        numb: 5,
+        question: "correct",
+        answer: "A. korrekt",
+        options: [
+            "A. korrekt",
+            "B. falsch",
+            "C. ungenau",
+            "D. zweifelhaft"
+        ]
+    },
+    {
+        numb: 6,
+        question: "crop",
+        answer: "C. Ernte",
+        options: [
+            "A. Produkt",
+            "B. Samen",
+            "C. Ernte",
+            "D. Dünger"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to demand",
+        answer: "B. fordern",
+        options: [
+            "A. anbieten",
+            "B. fordern",
+            "C. bitten",
+            "D. vorschlagen"
+        ]
+    },
+    {
+        numb: 8,
+        question: "equal",
+        answer: "D. gleich",
+        options: [
+            "A. unterschiedlich",
+            "B. ungerecht",
+            "C. selten",
+            "D. gleich"
+        ]
+    },
+    {
+        numb: 9,
+        question: "to feed",
+        answer: "B. füttern",
+        options: [
+            "A. hungern",
+            "B. füttern",
+            "C. dursten",
+            "D. kaufen"
+        ]
+    },
+    {
+        numb: 10,
+        question: "hole",
+        answer: "A. Loch",
+        options: [
+            "A. Loch",
+            "B. Hügel",
+            "C. Welle",
+            "D. Hohlraum"
+        ]
+    },
+    {
+        numb: 11,
+        question: "to increase",
+        answer: "B. erhöhen",
+        options: [
+            "A. verringern",
+            "B. erhöhen",
+            "C. stabilisieren",
+            "D. beibehalten"
+        ]
+    },
+    {
+        numb: 12,
+        question: "lord",
+        answer: "C. Herr",
+        options: [
+            "A. Bauer",
+            "B. Ritter",
+            "C. Herr",
+            "D. König"
+        ]
+    },
+    {
+        numb: 13,
+        question: "to owe",
+        answer: "D. schulden",
+        options: [
+            "A. schenken",
+            "B. leihen",
+            "C. sparen",
+            "D. schulden"
+        ]
+    },
+    {
+        numb: 14,
+        question: "position",
+        answer: "C. Position",
+        options: [
+            "A. Bewegung",
+            "B. Platz",
+            "C. Position",
+            "D. Richtung"
+        ]
+    },
+    {
+        numb: 15,
+        question: "to raise",
+        answer: "A. heben",
+        options: [
+            "A. heben",
+            "B. senken",
+            "C. drücken",
+            "D. ziehen"
+        ]
+    },
+    {
+        numb: 16,
+        question: "responsible",
+        answer: "B. verantwortlich",
+        options: [
+            "A. sorglos",
+            "B. verantwortlich",
+            "C. unzuverlässig",
+            "D. unsicher"
+        ]
+    },
+    {
+        numb: 17,
+        question: "sight",
+        answer: "D. Anblick",
+        options: [
+            "A. Gefühl",
+            "B. Ton",
+            "C. Geruch",
+            "D. Anblick"
+        ]
+    },
+    {
+        numb: 18,
+        question: "spot",
+        answer: "B. Stelle",
+        options: [
+            "A. Fleck",
+            "B. Stelle",
+            "C. Linie",
+            "D. Punkt"
+        ]
+    },
+    {
+        numb: 19,
+        question: "structure",
+        answer: "C. Struktur",
+        options: [
+            "A. Material",
+            "B. Fundamente",
+            "C. Struktur",
+            "D. Oberflächen"
+        ]
+    },
+    {
+        numb: 20,
+        question: "whole",
+        answer: "B. ganz",
+        options: [
+            "A. halb",
+            "B. ganz",
+            "C. Stück",
+            "D. Teil"
+        ]
+    }
+];
