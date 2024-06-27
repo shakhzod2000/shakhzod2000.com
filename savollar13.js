5c8c7f36d2161ec7236e3ea00554a782ceb8bf74
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-06-28 01:01:23 +0500

    Create savollar13.js

---
+++ savollar13.js
@@ -0,0 +1,222 @@
+let savollar13 = [
+    {
+        numb: 1,
+        question: "blood",
+        answer: "A. qon",
+        options: [
+            "A. qon",
+            "B. yara",
+            "C. jarohat",
+            "D. infeksiya"
+        ]
+    },
+    {
+        numb: 2,
+        question: "to burn",
+        answer: "B. olovda yoqmoq",
+        options: [
+            "A. o'chirmoq",
+            "B. olovda yoqmoq",
+            "C. bug'moq",
+            "D. qovurmoq"
+        ]
+    },
+    {
+        numb: 3,
+        question: "cell",
+        answer: "D. qamoqdagi xona",
+        options: [
+            "A. yashik",
+            "B. quti",
+            "C. xona",
+            "D. qamoqdagi xona"
+        ]
+    },
+    {
+        numb: 4,
+        question: "to contain",
+        answer: "B. o'z ichiga olmoq",
+        options: [
+            "A. chiqarib tashlamoq",
+            "B. o'z ichiga olmoq",
+            "C. yashirmoq",
+            "D. oldini olmoq"
+        ]
+    },
+    {
+        numb: 5,
+        question: "correct",
+        answer: "A. to'g'ri",
+        options: [
+            "A. to'g'ri",
+            "B. noto'g'ri",
+            "C. noaniq",
+            "D. shubhali"
+        ]
+    },
+    {
+        numb: 6,
+        question: "crop",
+        answer: "C. hosil",
+        options: [
+            "A. mahsulot",
+            "B. urug'",
+            "C. hosil",
+            "D. o'g'it"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to demand",
+        answer: "B. talab qilmoq",
+        options: [
+            "A. taklif qilmoq",
+            "B. talab qilmoq",
+            "C. so'ramoq",
+            "D. taklif etmoq"
+        ]
+    },
+    {
+        numb: 8,
+        question: "equal",
+        answer: "A. teng",
+        options: [
+            "A. teng",
+            "B. farqli",
+            "C. adolatsiz",
+            "D. kamdan-kam"
+        ]
+    },
+    {
+        numb: 9,
+        question: "to feed",
+        answer: "D. ovqatlantirmoq",
+        options: [
+            "A. och qolmoq",
+            "B. chanqamoq",
+            "C. sotib olmoq",
+            "D. ovqatlantirmoq"
+        ]
+    },
+    {
+        numb: 10,
+        question: "hole",
+        answer: "A. teshik",
+        options: [
+            "A. teshik",
+            "B. tepalik",
+            "C. to'lqin",
+            "D. bo'shliq"
+        ]
+    },
+    {
+        numb: 11,
+        question: "to increase",
+        answer: "B. oshirmoq",
+        options: [
+            "A. kamaytirmoq",
+            "B. oshirmoq",
+            "C. barqaror qilmoq",
+            "D. saqlab qolmoq"
+        ]
+    },
+    {
+        numb: 12,
+        question: "lord",
+        answer: "C. hukmdor",
+        options: [
+            "A. dehqon",
+            "B. ritsar",
+            "C. hukmdor",
+            "D. qirol"
+        ]
+    },
+    {
+        numb: 13,
+        question: "to owe",
+        answer: "A. qarz bo'lmoq",
+        options: [
+            "A. qarz bo'lmoq",
+            "B. qarzga bermoq",
+            "C. tejamoq",
+            "D. hadya qilmoq"
+        ]
+    },
+    {
+        numb: 14,
+        question: "position",
+        answer: "C. holat",
+        options: [
+            "A. harakat",
+            "B. joy",
+            "C. holat",
+            "D. yo'nalish"
+        ]
+    },
+    {
+        numb: 15,
+        question: "to raise",
+        answer: "D. ko'tarmoq",
+        options: [
+            "A. tushirmoq",
+            "B. bosmoq",
+            "C. tortmoq",
+            "D. ko'tarmoq"
+        ]
+    },
+    {
+        numb: 16,
+        question: "responsible",
+        answer: "B. mas'uliyatli",
+        options: [
+            "A. beparvo",
+            "B. mas'uliyatli",
+            "C. ishonchsiz",
+            "D. xavotirli"
+        ]
+    },
+    {
+        numb: 17,
+        question: "sight",
+        answer: "A. manzara",
+        options: [
+            "A. manzara",
+            "B. tovush",
+            "C. hid",
+            "D. his-tuyg'u"
+        ]
+    },
+    {
+        numb: 18,
+        question: "spot",
+        answer: "B. joy",
+        options: [
+            "A. sport",
+            "B. joy",
+            "C. chiziq",
+            "D. o'rindiq"
+        ]
+    },
+    {
+        numb: 19,
+        question: "structure",
+        answer: "C. imorat",
+        options: [
+            "A. material",
+            "B. poydevorlar",
+            "C. imorat",
+            "D. yuzalar"
+        ]
+    },
+    {
+        numb: 20,
+        question: "whole",
+        answer: "D. butun",
+        options: [
+            "A. yarim",
+            "B. bo'lak",
+            "C. qism",
+            "D. butun"
+        ]
+    }
+];
