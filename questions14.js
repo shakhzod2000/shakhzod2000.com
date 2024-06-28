4f4ef8458446c6e1941a2a485f03bf49de807018
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-06-28 17:04:36 +0500

    Create questions14.js

---
+++ questions14.js
@@ -0,0 +1,222 @@
+let questions14 = [
+    {
+        numb: 1,
+        question: "coach",
+        answer: "A. Trainer",
+        options: [
+            "A. Trainer",
+            "B. Spieler",
+            "C. Schiedsrichter",
+            "D. Zuschauer"
+        ]
+    },
+    {
+        numb: 2,
+        question: "to control",
+        answer: "B. kontrollieren",
+        options: [
+            "A. ignorieren",
+            "B. kontrollieren",
+            "C. verlieren",
+            "D. stoppen"
+        ]
+    },
+    {
+        numb: 3,
+        question: "description",
+        answer: "D. Beschreibung",
+        options: [
+            "A. Erklärung",
+            "B. Definition",
+            "C. Übersetzung",
+            "D. Beschreibung"
+        ]
+    },
+    {
+        numb: 4,
+        question: "direct",
+        answer: "C. direkt",
+        options: [
+            "A. indirekt",
+            "B. kompliziert",
+            "C. direkt",
+            "D. umweg"
+        ]
+    },
+    {
+        numb: 5,
+        question: "exam",
+        answer: "A. Prüfung",
+        options: [
+            "A. Prüfung",
+            "B. Lektion",
+            "C. Übung",
+            "D. Hausaufgabe"
+        ]
+    },
+    {
+        numb: 6,
+        question: "example",
+        answer: "B. Beispiel",
+        options: [
+            "A. Thema",
+            "B. Beispiel",
+            "C. Erklärung",
+            "D. Modell"
+        ]
+    },
+    {
+        numb: 7,
+        question: "limit",
+        answer: "C. Grenze",
+        options: [
+            "A. Freiheit",
+            "B. Möglichkeit",
+            "C. Grenze",
+            "D. Ausnahme"
+        ]
+    },
+    {
+        numb: 8,
+        question: "local",
+        answer: "A. lokal",
+        options: [
+            "A. lokal",
+            "B. international",
+            "C. national",
+            "D. entfernt"
+        ]
+    },
+    {
+        numb: 9,
+        question: "magical",
+        answer: "D. magisch",
+        options: [
+            "A. gewöhnlich",
+            "B. langweilig",
+            "C. normal",
+            "D. magisch"
+        ]
+    },
+    {
+        numb: 10,
+        question: "mail",
+        answer: "B. Post",
+        options: [
+            "A. E-Mail",
+            "B. Post",
+            "C. Nachricht",
+            "D. Paket"
+        ]
+    },
+    {
+        numb: 11,
+        question: "novel",
+        answer: "C. Roman",
+        options: [
+            "A. Gedicht",
+            "B. Zeitschrift",
+            "C. Roman",
+            "D. Bericht"
+        ]
+    },
+    {
+        numb: 12,
+        question: "outline",
+        answer: "A. Gliederung",
+        options: [
+            "A. Gliederung",
+            "B. Inhalt",
+            "C. Einleitung",
+            "D. Zusammenfassung"
+        ]
+    },
+    {
+        numb: 13,
+        question: "poet",
+        answer: "D. Dichter",
+        options: [
+            "A. Romanautor",
+            "B. Journalist",
+            "C. Redakteur",
+            "D. Dichter"
+        ]
+    },
+    {
+        numb: 14,
+        question: "to print",
+        answer: "C. drucken",
+        options: [
+            "A. schreiben",
+            "B. lesen",
+            "C. drucken",
+            "D. senden"
+        ]
+    },
+    {
+        numb: 15,
+        question: "scene",
+        answer: "A. Szene",
+        options: [
+            "A. Szene",
+            "B. Film",
+            "C. Kapitel",
+            "D. Buch"
+        ]
+    },
+    {
+        numb: 16,
+        question: "sheet",
+        answer: "B. Blatt",
+        options: [
+            "A. Buch",
+            "B. Blatt",
+            "C. Papier",
+            "D. Seite"
+        ]
+    },
+    {
+        numb: 17,
+        question: "silly",
+        answer: "D. albern",
+        options: [
+            "A. schlau",
+            "B. ernst",
+            "C. weise",
+            "D. albern"
+        ]
+    },
+    {
+        numb: 18,
+        question: "store",
+        answer: "C. Geschäft",
+        options: [
+            "A. Schule",
+            "B. Büro",
+            "C. Geschäft",
+            "D. Fabrik"
+        ]
+    },
+    {
+        numb: 19,
+        question: "to suffer",
+        answer: "A. leiden",
+        options: [
+            "A. leiden",
+            "B. lachen",
+            "C. spielen",
+            "D. singen"
+        ]
+    },
+    {
+        numb: 20,
+        question: "technology",
+        answer: "B. Technologie",
+        options: [
+            "A. Natur",
+            "B. Technologie",
+            "C. Kunst",
+            "D. Musik"
+        ]
+    }
+];
