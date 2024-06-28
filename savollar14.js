43e2d43174c755c3ea1c7be234696be63b97f6d9
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-06-28 17:07:22 +0500

    Create savollar14.js

---
+++ savollar14.js
@@ -0,0 +1,222 @@
+let savollar14 = [
+    {
+        numb: 1,
+        question: "coach",
+        answer: "A. murabbiy",
+        options: [
+            "A. murabbiy",
+            "B. o'yinchi",
+            "C. hakam",
+            "D. tomoshabin"
+        ]
+    },
+    {
+        numb: 2,
+        question: "to control",
+        answer: "B. nazorat qilmoq",
+        options: [
+            "A. e'tiborsiz qoldirmoq",
+            "B. nazorat qilmoq",
+            "C. yo'qotmoq",
+            "D. to'xtatmoq"
+        ]
+    },
+    {
+        numb: 3,
+        question: "description",
+        answer: "D. tavsif",
+        options: [
+            "A. tushuntirish",
+            "B. ta'rif",
+            "C. tarjima",
+            "D. tavsif"
+        ]
+    },
+    {
+        numb: 4,
+        question: "direct",
+        answer: "C. to'g'ridan-to'g'ri",
+        options: [
+            "A. bilvosita",
+            "B. murakkab",
+            "C. to'g'ridan-to'g'ri",
+            "D. chetlab o'tmoq"
+        ]
+    },
+    {
+        numb: 5,
+        question: "exam",
+        answer: "A. imtihon",
+        options: [
+            "A. imtihon",
+            "B. dars",
+            "C. mashq",
+            "D. uy vazifasi"
+        ]
+    },
+    {
+        numb: 6,
+        question: "example",
+        answer: "B. misol",
+        options: [
+            "A. mavzu",
+            "B. misol",
+            "C. tushuntirish",
+            "D. model"
+        ]
+    },
+    {
+        numb: 7,
+        question: "limit",
+        answer: "C. chegara",
+        options: [
+            "A. erkinlik",
+            "B. imkoniyat",
+            "C. chegara",
+            "D. istisno"
+        ]
+    },
+    {
+        numb: 8,
+        question: "local",
+        answer: "A. mahalliy",
+        options: [
+            "A. mahalliy",
+            "B. xalqaro",
+            "C. milliy",
+            "D. uzoq"
+        ]
+    },
+    {
+        numb: 9,
+        question: "magical",
+        answer: "D. sehrli",
+        options: [
+            "A. oddiy",
+            "B. zerikarli",
+            "C. normal",
+            "D. sehrli"
+        ]
+    },
+    {
+        numb: 10,
+        question: "mail",
+        answer: "B. maktub",
+        options: [
+            "A. elektron pochta",
+            "B. maktub",
+            "C. xabar",
+            "D. paket"
+        ]
+    },
+    {
+        numb: 11,
+        question: "novel",
+        answer: "C. roman",
+        options: [
+            "A. she'r",
+            "B. jurnal",
+            "C. roman",
+            "D. hisobot"
+        ]
+    },
+    {
+        numb: 12,
+        question: "outline",
+        answer: "A. reja",
+        options: [
+            "A. reja",
+            "B. mazmun",
+            "C. kirish",
+            "D. xulosa"
+        ]
+    },
+    {
+        numb: 13,
+        question: "poet",
+        answer: "D. shoir",
+        options: [
+            "A. roman yozuvchisi",
+            "B. jurnalist",
+            "C. muharrir",
+            "D. shoir"
+        ]
+    },
+    {
+        numb: 14,
+        question: "to print",
+        answer: "C. bosib chiqarmoq",
+        options: [
+            "A. yozmoq",
+            "B. o'qimoq",
+            "C. bosib chiqarmoq",
+            "D. yubormoq"
+        ]
+    },
+    {
+        numb: 15,
+        question: "scene",
+        answer: "A. sahna",
+        options: [
+            "A. sahna",
+            "B. film",
+            "C. bob",
+            "D. kitob"
+        ]
+    },
+    {
+        numb: 16,
+        question: "sheet",
+        answer: "B. varoq",
+        options: [
+            "A. kitob",
+            "B. varoq",
+            "C. qog'oz",
+            "D. sahifa"
+        ]
+    },
+    {
+        numb: 17,
+        question: "silly",
+        answer: "D. ahmoqona",
+        options: [
+            "A. aqlli",
+            "B. jiddiy",
+            "C. dono",
+            "D. ahmoqona"
+        ]
+    },
+    {
+        numb: 18,
+        question: "store",
+        answer: "C. do'kon",
+        options: [
+            "A. maktab",
+            "B. ofis",
+            "C. do'kon",
+            "D. zavod"
+        ]
+    },
+    {
+        numb: 19,
+        question: "to suffer",
+        answer: "A. azoblanmoq",
+        options: [
+            "A. azoblanmoq",
+            "B. kulmoq",
+            "C. o'ynamoq",
+            "D. kuylamoq"
+        ]
+    },
+    {
+        numb: 20,
+        question: "technology",
+        answer: "B. texnologiya",
+        options: [
+            "A. tabiat",
+            "B. texnologiya",
+            "C. san'at",
+            "D. musiqa"
+        ]
+    }
+];
