1fde42e463d6eb3ff957ee5b70dc7f5c19ca550d
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-06-28 17:08:18 +0500

    Create savollar15.js

---
+++ savollar15.js
@@ -0,0 +1,222 @@
+let savollar15 = [
+    {
+        numb: 1,
+        question: "across",
+        answer: "B. bo'ylab",
+        options: [
+            "A. ostida",
+            "B. bo'ylab",
+            "C. yonida",
+            "D. orqali"
+        ]
+    },
+    {
+        numb: 2,
+        question: "to breathe",
+        answer: "C. nafas olmoq",
+        options: [
+            "A. yugurmoq",
+            "B. ichmoq",
+            "C. nafas olmoq",
+            "D. uxlamoq"
+        ]
+    },
+    {
+        numb: 3,
+        question: "characteristic",
+        answer: "A. xususiyat",
+        options: [
+            "A. xususiyat",
+            "B. xato",
+            "C. xulq-atvor",
+            "D. odat"
+        ]
+    },
+    {
+        numb: 4,
+        question: "to consume",
+        answer: "D. iste'mol qilmoq",
+        options: [
+            "A. kuzatmoq",
+            "B. tushunmoq",
+            "C. tushuntirmoq",
+            "D. iste'mol qilmoq"
+        ]
+    },
+    {
+        numb: 5,
+        question: "to excite",
+        answer: "B. hayajonlantirmoq",
+        options: [
+            "A. zeriktirmoq",
+            "B. hayajonlantirmoq",
+            "C. qo'rqitmoq",
+            "D. tinchlantirmoq"
+        ]
+    },
+    {
+        numb: 6,
+        question: "extreme",
+        answer: "A. haddan tashqari",
+        options: [
+            "A. haddan tashqari",
+            "B. normal",
+            "C. o'rtacha",
+            "D. kam"
+        ]
+    },
+    {
+        numb: 7,
+        question: "fear",
+        answer: "D. qo'rquv",
+        options: [
+            "A. jasorat",
+            "B. quvonch",
+            "C. sevgi",
+            "D. qo'rquv"
+        ]
+    },
+    {
+        numb: 8,
+        question: "fortunate",
+        answer: "B. omadli",
+        options: [
+            "A. qayg'uli",
+            "B. omadli",
+            "C. baxtsiz",
+            "D. umidsiz"
+        ]
+    },
+    {
+        numb: 9,
+        question: "to happen to",
+        answer: "A. tasodifan sodir bo'lmoq",
+        options: [
+            "A. tasodifan sodir bo'lmoq",
+            "B. oldini olmoq",
+            "C. rejalashtirmoq",
+            "D. nazorat qilmoq"
+        ]
+    },
+    {
+        numb: 10,
+        question: "length",
+        answer: "C. uzunlik",
+        options: [
+            "A. kenglik",
+            "B. balandlik",
+            "C. uzunlik",
+            "D. chuqurlik"
+        ]
+    },
+    {
+        numb: 11,
+        question: "mistake",
+        answer: "B. xato",
+        options: [
+            "A. muvaffaqiyat",
+            "B. xato",
+            "C. yechim",
+            "D. imkoniyat"
+        ]
+    },
+    {
+        numb: 12,
+        question: "to observe",
+        answer: "A. kuzatmoq",
+        options: [
+            "A. kuzatmoq",
+            "B. e'tiborsiz qoldirmoq",
+            "C. uxlamoq",
+            "D. o'ylamoq"
+        ]
+    },
+    {
+        numb: 13,
+        question: "opportunity",
+        answer: "D. imkoniyat",
+        options: [
+            "A. qiyinchilik",
+            "B. muammo",
+            "C. xavf",
+            "D. imkoniyat"
+        ]
+    },
+    {
+        numb: 14,
+        question: "prize",
+        answer: "C. mukofot",
+        options: [
+            "A. yo'qotish",
+            "B. jazo",
+            "C. mukofot",
+            "D. vazifa"
+        ]
+    },
+    {
+        numb: 15,
+        question: "race",
+        answer: "B. poyga",
+        options: [
+            "A. sayr",
+            "B. poyga",
+            "C. tanaffus",
+            "D. o'yin"
+        ]
+    },
+    {
+        numb: 16,
+        question: "to realize",
+        answer: "A. anglamoq",
+        options: [
+            "A. anglamoq",
+            "B. be'parvo qoldirmoq",
+            "C. oldini olmoq",
+            "D. unutmoq"
+        ]
+    },
+    {
+        numb: 17,
+        question: "to respond",
+        answer: "B. javob bermoq",
+        options: [
+            "A. jim turmoq",
+            "B. javob bermoq",
+            "C. unutmoq",
+            "D. o'ylamoq"
+        ]
+    },
+    {
+        numb: 18,
+        question: "risk",
+        answer: "A. xavfli tavakkal",
+        options: [
+            "A. xavfli tavakkal",
+            "B. xavfsizlik",
+            "C. muvaffaqiyat",
+            "D. foyda"
+        ]
+    },
+    {
+        numb: 19,
+        question: "to wonder",
+        answer: "C. hayron bo'lmoq",
+        options: [
+            "A. bilmoq",
+            "B. oldini olmoq",
+            "C. hayron bo'lmoq",
+            "D. tushuntirmoq"
+        ]
+    },
+    {
+        numb: 20,
+        question: "yet",
+        answer: "B. hali",
+        options: [
+            "A. allaqachon",
+            "B. hali",
+            "C. keyinroq",
+            "D. darhol"
+        ]
+    }
+];
