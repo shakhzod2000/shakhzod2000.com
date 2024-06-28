e3ae68ee5c19dc0db785335be54ce37877cb590f
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-06-28 17:06:38 +0500

    Create questions15.js

---
+++ questions15.js
@@ -0,0 +1,222 @@
+let questions15 = [
+    {
+        numb: 1,
+        question: "across",
+        answer: "B. über",
+        options: [
+            "A. unter",
+            "B. über",
+            "C. neben",
+            "D. durch"
+        ]
+    },
+    {
+        numb: 2,
+        question: "to breathe",
+        answer: "C. atmen",
+        options: [
+            "A. laufen",
+            "B. trinken",
+            "C. atmen",
+            "D. schlafen"
+        ]
+    },
+    {
+        numb: 3,
+        question: "characteristic",
+        answer: "A. Merkmal",
+        options: [
+            "A. Merkmal",
+            "B. Fehler",
+            "C. Verhalten",
+            "D. Angewohnheit"
+        ]
+    },
+    {
+        numb: 4,
+        question: "to consume",
+        answer: "D. konsumieren",
+        options: [
+            "A. beobachten",
+            "B. verstehen",
+            "C. erklären",
+            "D. konsumieren"
+        ]
+    },
+    {
+        numb: 5,
+        question: "to excite",
+        answer: "B. begeistern",
+        options: [
+            "A. langweilen",
+            "B. begeistern",
+            "C. erschrecken",
+            "D. beruhigen"
+        ]
+    },
+    {
+        numb: 6,
+        question: "extreme",
+        answer: "A. extrem",
+        options: [
+            "A. extrem",
+            "B. normal",
+            "C. durchschnittlich",
+            "D. gering"
+        ]
+    },
+    {
+        numb: 7,
+        question: "fear",
+        answer: "D. Angst",
+        options: [
+            "A. Mut",
+            "B. Freude",
+            "C. Liebe",
+            "D. Angst"
+        ]
+    },
+    {
+        numb: 8,
+        question: "fortunate",
+        answer: "B. glücklich",
+        options: [
+            "A. traurig",
+            "B. glücklich",
+            "C. unglücklich",
+            "D. verzweifelt"
+        ]
+    },
+    {
+        numb: 9,
+        question: "to happen",
+        answer: "A. passieren",
+        options: [
+            "A. passieren",
+            "B. vermeiden",
+            "C. planen",
+            "D. kontrollieren"
+        ]
+    },
+    {
+        numb: 10,
+        question: "length",
+        answer: "C. Länge",
+        options: [
+            "A. Breite",
+            "B. Höhe",
+            "C. Länge",
+            "D. Tiefe"
+        ]
+    },
+    {
+        numb: 11,
+        question: "mistake",
+        answer: "B. Fehler",
+        options: [
+            "A. Erfolg",
+            "B. Fehler",
+            "C. Lösung",
+            "D. Möglichkeit"
+        ]
+    },
+    {
+        numb: 12,
+        question: "to observe",
+        answer: "A. beobachten",
+        options: [
+            "A. beobachten",
+            "B. ignorieren",
+            "C. schlafen",
+            "D. denken"
+        ]
+    },
+    {
+        numb: 13,
+        question: "opportunity",
+        answer: "D. Gelegenheit",
+        options: [
+            "A. Herausforderung",
+            "B. Problem",
+            "C. Risiko",
+            "D. Gelegenheit"
+        ]
+    },
+    {
+        numb: 14,
+        question: "prize",
+        answer: "C. Preis",
+        options: [
+            "A. Verlust",
+            "B. Strafe",
+            "C. Preis",
+            "D. Aufgabe"
+        ]
+    },
+    {
+        numb: 15,
+        question: "race",
+        answer: "B. Rennen",
+        options: [
+            "A. Spaziergang",
+            "B. Rennen",
+            "C. Pause",
+            "D. Spiel"
+        ]
+    },
+    {
+        numb: 16,
+        question: "to realize",
+        answer: "A. realisieren",
+        options: [
+            "A. realisieren",
+            "B. ignorieren",
+            "C. vermeiden",
+            "D. vergessen"
+        ]
+    },
+    {
+        numb: 17,
+        question: "to respond",
+        answer: "B. antworten",
+        options: [
+            "A. schweigen",
+            "B. antworten",
+            "C. vergessen",
+            "D. denken"
+        ]
+    },
+    {
+        numb: 18,
+        question: "risk",
+        answer: "A. Risiko",
+        options: [
+            "A. Risiko",
+            "B. Sicherheit",
+            "C. Erfolg",
+            "D. Gewinn"
+        ]
+    },
+    {
+        numb: 19,
+        question: "to wonder",
+        answer: "C. sich fragen",
+        options: [
+            "A. wissen",
+            "B. vermeiden",
+            "C. sich fragen",
+            "D. erklären"
+        ]
+    },
+    {
+        numb: 20,
+        question: "yet",
+        answer: "B. noch",
+        options: [
+            "A. schon",
+            "B. noch",
+            "C. später",
+            "D. sofort"
+        ]
+    }
+];
