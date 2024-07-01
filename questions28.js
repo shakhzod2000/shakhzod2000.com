532e0948c9dd73d8f0761e4f46952a6f9722a605
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 23:25:02 +0500

    Create questions28.js

---
+++ questions28.js
@@ -0,0 +1,222 @@
+let questions28 = [
+    {
+        numb: 1,
+        question: "band",
+        answer: "B. Musikgruppe",
+        options: [
+            "A. Bande",
+            "B. Musikgruppe",
+            "C. Team",
+            "D. Crew"
+        ]
+    },
+    {
+        numb: 2,
+        question: "barely",
+        answer: "C. kaum",
+        options: [
+            "A. häufig",
+            "B. nie",
+            "C. kaum",
+            "D. immer"
+        ]
+    },
+    {
+        numb: 3,
+        question: "boring",
+        answer: "A. langweilig",
+        options: [
+            "A. langweilig",
+            "B. spannend",
+            "C. gefährlich",
+            "D. nützlich"
+        ]
+    },
+    {
+        numb: 4,
+        question: "to cancel",
+        answer: "D. absagen",
+        options: [
+            "A. planen",
+            "B. bestätigen",
+            "C. vorschlagen",
+            "D. absagen"
+        ]
+    },
+    {
+        numb: 5,
+        question: "driveway",
+        answer: "B. Auffahrt",
+        options: [
+            "A. Gehweg",
+            "B. Auffahrt",
+            "C. Autobahn",
+            "D. Radweg"
+        ]
+    },
+    {
+        numb: 6,
+        question: "garbage",
+        answer: "C. Müll",
+        options: [
+            "A. Schmutz",
+            "B. Essen",
+            "C. Müll",
+            "D. Kleidung"
+        ]
+    },
+    {
+        numb: 7,
+        question: "instrument",
+        answer: "D. Instrument",
+        options: [
+            "A. Werkzeug",
+            "B. Spielzeug",
+            "C. Gerät",
+            "D. Instrument"
+        ]
+    },
+    {
+        numb: 8,
+        question: "list",
+        answer: "B. Liste",
+        options: [
+            "A. Karte",
+            "B. Liste",
+            "C. Tabelle",
+            "D. Diagramm"
+        ]
+    },
+    {
+        numb: 9,
+        question: "magic",
+        answer: "C. Zauber",
+        options: [
+            "A. Trick",
+            "B. Illusion",
+            "C. Zauber",
+            "D. Wissenschaft"
+        ]
+    },
+    {
+        numb: 10,
+        question: "message",
+        answer: "D. Nachricht",
+        options: [
+            "A. Brief",
+            "B. Anruf",
+            "C. E-Mail",
+            "D. Nachricht"
+        ]
+    },
+    {
+        numb: 11,
+        question: "to notice",
+        answer: "A. bemerken",
+        options: [
+            "A. bemerken",
+            "B. ignorieren",
+            "C. übersehen",
+            "D. vergessen"
+        ]
+    },
+    {
+        numb: 12,
+        question: "to own",
+        answer: "B. besitzen",
+        options: [
+            "A. mieten",
+            "B. besitzen",
+            "C. leihen",
+            "D. verkaufen"
+        ]
+    },
+    {
+        numb: 13,
+        question: "to predict",
+        answer: "C. vorhersagen",
+        options: [
+            "A. wiederholen",
+            "B. beschreiben",
+            "C. vorhersagen",
+            "D. erklären"
+        ]
+    },
+    {
+        numb: 14,
+        question: "professor",
+        answer: "D. Professor",
+        options: [
+            "A. Schüler",
+            "B. Arzt",
+            "C. Forscher",
+            "D. Professor"
+        ]
+    },
+    {
+        numb: 15,
+        question: "to rush",
+        answer: "B. eilen",
+        options: [
+            "A. schlendern",
+            "B. eilen",
+            "C. zögern",
+            "D. verweilen"
+        ]
+    },
+    {
+        numb: 16,
+        question: "schedule",
+        answer: "C. Zeitplan",
+        options: [
+            "A. Kalender",
+            "B. Uhrzeit",
+            "C. Zeitplan",
+            "D. Aufgabe"
+        ]
+    },
+    {
+        numb: 17,
+        question: "to share",
+        answer: "A. teilen",
+        options: [
+            "A. teilen",
+            "B. behalten",
+            "C. wegnehmen",
+            "D. verbergen"
+        ]
+    },
+    {
+        numb: 18,
+        question: "stage",
+        answer: "D. Bühne",
+        options: [
+            "A. Kino",
+            "B. Konzert",
+            "C. Theater",
+            "D. Bühne"
+        ]
+    },
+    {
+        numb: 19,
+        question: "storm",
+        answer: "C. Sturm",
+        options: [
+            "A. Regen",
+            "B. Schnee",
+            "C. Sturm",
+            "D. Sonne"
+        ]
+    },
+    {
+        numb: 20,
+        question: "within",
+        answer: "B. innerhalb",
+        options: [
+            "A. außerhalb",
+            "B. innerhalb",
+            "C. neben",
+            "D. zwischen"
+        ]
+    }
+];
