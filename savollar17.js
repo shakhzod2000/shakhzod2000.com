eb8081a94a5c469ddb0bcb45daaac83f06a9b8c2
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 17:44:26 +0500

    Create savollar17.js

---
+++ savollar17.js
@@ -0,0 +1,222 @@
+let savollar17 = [
+    {
+        numb: 1,
+        question: "to appreciate",
+        answer: "A. qadrlamoq",
+        options: [
+            "A. qadrlamoq",
+            "B. mensimaslik",
+            "C. e'tiborsiz qoldirmoq",
+            "D. past baholamoq"
+        ]
+    },
+    {
+        numb: 2,
+        question: "available",
+        answer: "B. mavjud",
+        options: [
+            "A. band",
+            "B. mavjud",
+            "C. cheklangan",
+            "D. yopiq"
+        ]
+    },
+    {
+        numb: 3,
+        question: "to beat",
+        answer: "A. yengmoq",
+        options: [
+            "A. yengmoq",
+            "B. yutqazmoq",
+            "C. oldini olmoq",
+            "D. zarar ko'rmoq"
+        ]
+    },
+    {
+        numb: 4,
+        question: "bright",
+        answer: "C. yorqin",
+        options: [
+            "A. qorong'i",
+            "B. xira",
+            "C. yorqin",
+            "D. to'nk"
+        ]
+    },
+    {
+        numb: 5,
+        question: "to celebrate",
+        answer: "D. nishonlamoq",
+        options: [
+            "A. motam tutmoq",
+            "B. e'tiborsiz qoldirmoq",
+            "C. unutmoq",
+            "D. nishonlamoq"
+        ]
+    },
+    {
+        numb: 6,
+        question: "to determine",
+        answer: "B. tanlamoq",
+        options: [
+            "A. shubhalanmoq",
+            "B. tanlamoq",
+            "C. ikkilanmoq",
+            "D. o'zgartirmoq"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to disappear",
+        answer: "A. yo'qolmoq ketmoq",
+        options: [
+            "A. yo'qolmoq ketmoq",
+            "B. paydo bo'lmoq",
+            "C. qolmoq",
+            "D. mavjud bo'lmoq"
+        ]
+    },
+    {
+        numb: 8,
+        question: "else",
+        answer: "C. boshqa",
+        options: [
+            "A. oldin",
+            "B. shuning uchun",
+            "C. boshqa",
+            "D. deyarli"
+        ]
+    },
+    {
+        numb: 9,
+        question: "fair",
+        answer: "A. adolatli",
+        options: [
+            "A. adolatli",
+            "B. adolatsiz",
+            "C. noto'g'ri",
+            "D. hiylakor"
+        ]
+    },
+    {
+        numb: 10,
+        question: "to flow",
+        answer: "D. oqmoq",
+        options: [
+            "A. turg'un",
+            "B. to'xtamoq",
+            "C. to'smoq",
+            "D. oqmoq"
+        ]
+    },
+    {
+        numb: 11,
+        question: "forward",
+        answer: "B. oldinga",
+        options: [
+            "A. orqaga",
+            "B. oldinga",
+            "C. turg'un",
+            "D. yon tomonga"
+        ]
+    },
+    {
+        numb: 12,
+        question: "hill",
+        answer: "C. tepalik",
+        options: [
+            "A. vodiy",
+            "B. tekislik",
+            "C. tepalik",
+            "D. tog'"
+        ]
+    },
+    {
+        numb: 13,
+        question: "level",
+        answer: "A. daraja",
+        options: [
+            "A. daraja",
+            "B. og'ish",
+            "C. moyillik",
+            "D. qiyshiq"
+        ]
+    },
+    {
+        numb: 14,
+        question: "lone",
+        answer: "D. yolg'iz",
+        options: [
+            "A. birga",
+            "B. jamiyatda",
+            "C. hamroh",
+            "D. yolg'iz"
+        ]
+    },
+    {
+        numb: 15,
+        question: "puddle",
+        answer: "B. ko'lmak",
+        options: [
+            "A. daryo",
+            "B. ko'lmak",
+            "C. ko'l",
+            "D. okean"
+        ]
+    },
+    {
+        numb: 16,
+        question: "response",
+        answer: "C. javob",
+        options: [
+            "A. savol",
+            "B. da'vo",
+            "C. javob",
+            "D. inkor"
+        ]
+    },
+    {
+        numb: 17,
+        question: "season",
+        answer: "A. fasl",
+        options: [
+            "A. fasl",
+            "B. oy",
+            "C. hafta",
+            "D. o'n yillik"
+        ]
+    },
+    {
+        numb: 18,
+        question: "solution",
+        answer: "B. yechim",
+        options: [
+            "A. muammo",
+            "B. yechim",
+            "C. savol",
+            "D. vazifa"
+        ]
+    },
+    {
+        numb: 19,
+        question: "to waste",
+        answer: "C. isrof qilmoq",
+        options: [
+            "A. tejamoq",
+            "B. foydalamoq",
+            "C. isrof qilmoq",
+            "D. saqlamoq"
+        ]
+    },
+    {
+        numb: 20,
+        question: "whether",
+        answer: "A. yoki",
+        options: [
+            "A. yoki",
+            "B. va",
+            "C. yoki",
+            "D. chunki"
+        ]
+    }
+];
