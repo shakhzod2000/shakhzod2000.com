1079ec1a840e2336aef38a2d2a0115dad5b59960
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 23:35:53 +0500

    Create questions29.js

---
+++ questions29.js
@@ -0,0 +1,222 @@
+let questions29 = [
+    {
+        numb: 1,
+        question: "to advertise",
+        answer: "A. werben",
+        options: [
+            "A. werben",
+            "B. kaufen",
+            "C. planen",
+            "D. bauen"
+        ]
+    },
+    {
+        numb: 2,
+        question: "to assign",
+        answer: "B. zuweisen",
+        options: [
+            "A. ablehnen",
+            "B. zuweisen",
+            "C. beenden",
+            "D. vergessen"
+        ]
+    },
+    {
+        numb: 3,
+        question: "audience",
+        answer: "C. Publikum",
+        options: [
+            "A. Schauspieler",
+            "B. Wettbewerb",
+            "C. Publikum",
+            "D. Trainer"
+        ]
+    },
+    {
+        numb: 4,
+        question: "breakfast",
+        answer: "A. Frühstück",
+        options: [
+            "A. Frühstück",
+            "B. Mittagessen",
+            "C. Abendessen",
+            "D. Snack"
+        ]
+    },
+    {
+        numb: 5,
+        question: "competition",
+        answer: "D. Wettbewerb",
+        options: [
+            "A. Training",
+            "B. Zuschauer",
+            "C. Herausforderung",
+            "D. Wettbewerb"
+        ]
+    },
+    {
+        numb: 6,
+        question: "cool",
+        answer: "B. kühl",
+        options: [
+            "A. heiß",
+            "B. kühl",
+            "C. warm",
+            "D. trocken"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to gain",
+        answer: "A. gewinnen",
+        options: [
+            "A. gewinnen",
+            "B. verlieren",
+            "C. vermeiden",
+            "D. vernachlässigen"
+        ]
+    },
+    {
+        numb: 8,
+        question: "importance",
+        answer: "C. Bedeutung",
+        options: [
+            "A. Reichtum",
+            "B. Spaß",
+            "C. Bedeutung",
+            "D. Freude"
+        ]
+    },
+    {
+        numb: 9,
+        question: "knowledge",
+        answer: "A. Wissen",
+        options: [
+            "A. Wissen",
+            "B. Stärke",
+            "C. Schwäche",
+            "D. Gedächtnis"
+        ]
+    },
+    {
+        numb: 10,
+        question: "major",
+        answer: "D. groß",
+        options: [
+            "A. klein",
+            "B. unwichtig",
+            "C. einfach",
+            "D. groß"
+        ]
+    },
+    {
+        numb: 11,
+        question: "mean",
+        answer: "C. gemein",
+        options: [
+            "A. freundlich",
+            "B. glücklich",
+            "C. gemein",
+            "D. traurig"
+        ]
+    },
+    {
+        numb: 12,
+        question: "to prefer",
+        answer: "A. bevorzugen",
+        options: [
+            "A. bevorzugen",
+            "B. ablehnen",
+            "C. vergessen",
+            "D. ignorieren"
+        ]
+    },
+    {
+        numb: 13,
+        question: "president",
+        answer: "B. Präsident",
+        options: [
+            "A. Schüler",
+            "B. Präsident",
+            "C. Lehrer",
+            "D. Freund"
+        ]
+    },
+    {
+        numb: 14,
+        question: "progress",
+        answer: "C. Fortschritt",
+        options: [
+            "A. Rückschritt",
+            "B. Misserfolg",
+            "C. Fortschritt",
+            "D. Stillstand"
+        ]
+    },
+    {
+        numb: 15,
+        question: "respect",
+        answer: "A. Respekt",
+        options: [
+            "A. Respekt",
+            "B. Verachtung",
+            "C. Gleichgültigkeit",
+            "D. Abneigung"
+        ]
+    },
+    {
+        numb: 16,
+        question: "rich",
+        answer: "B. reich",
+        options: [
+            "A. arm",
+            "B. reich",
+            "C. sparsam",
+            "D. großzügig"
+        ]
+    },
+    {
+        numb: 17,
+        question: "skill",
+        answer: "A. Fähigkeit",
+        options: [
+            "A. Fähigkeit",
+            "B. Ahnungslosigkeit",
+            "C. Zufall",
+            "D. Pech"
+        ]
+    },
+    {
+        numb: 18,
+        question: "somehow",
+        answer: "C. irgendwie",
+        options: [
+            "A. sicher",
+            "B. definitiv",
+            "C. irgendwie",
+            "D. genau"
+        ]
+    },
+    {
+        numb: 19,
+        question: "strength",
+        answer: "B. Stärke",
+        options: [
+            "A. Schwäche",
+            "B. Stärke",
+            "C. Anfälligkeit",
+            "D. Zärtlichkeit"
+        ]
+    },
+    {
+        numb: 20,
+        question: "to vote",
+        answer: "A. wählen",
+        options: [
+            "A. wählen",
+            "B. ignorieren",
+            "C. ablehnen",
+            "D. verlassen"
+        ]
+    }
+];
