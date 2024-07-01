1a2f5fc1e4c4a0e910362583ca61ed6e08203767
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 23:25:39 +0500

    Create savollar28.js

---
+++ savollar28.js
@@ -0,0 +1,222 @@
+let savollar28 = [
+    {
+        numb: 1,
+        question: "band",
+        answer: "B. musiqa guruhi",
+        options: [
+            "A. to'da",
+            "B. musiqa guruhi",
+            "C. jamoa",
+            "D. ekipaj"
+        ]
+    },
+    {
+        numb: 2,
+        question: "barely",
+        answer: "C. zo'rg'a",
+        options: [
+            "A. tez-tez",
+            "B. hech qachon",
+            "C. zo'rg'a",
+            "D. har doim"
+        ]
+    },
+    {
+        numb: 3,
+        question: "boring",
+        answer: "A. zerikarli",
+        options: [
+            "A. zerikarli",
+            "B. qiziqarli",
+            "C. xavfli",
+            "D. foydali"
+        ]
+    },
+    {
+        numb: 4,
+        question: "to cancel",
+        answer: "D. bekor qilmoq",
+        options: [
+            "A. rejalashtirmoq",
+            "B. tasdiqlamoq",
+            "C. taklif qilmoq",
+            "D. bekor qilmoq"
+        ]
+    },
+    {
+        numb: 5,
+        question: "driveway",
+        answer: "B. yo'lak",
+        options: [
+            "A. piyodalar yo'li",
+            "B. yo'lak",
+            "C. avtomobil yo'li",
+            "D. velosiped yo'li"
+        ]
+    },
+    {
+        numb: 6,
+        question: "garbage",
+        answer: "C. axlat",
+        options: [
+            "A. ifloslik",
+            "B. oziq-ovqat",
+            "C. axlat",
+            "D. kiyim-kechak"
+        ]
+    },
+    {
+        numb: 7,
+        question: "instrument",
+        answer: "D. asbob",
+        options: [
+            "A. vosita",
+            "B. o'yinchoq",
+            "C. qurilma",
+            "D. asbob"
+        ]
+    },
+    {
+        numb: 8,
+        question: "list",
+        answer: "B. ro'yxat",
+        options: [
+            "A. karta",
+            "B. ro'yxat",
+            "C. jadval",
+            "D. diagramma"
+        ]
+    },
+    {
+        numb: 9,
+        question: "magic",
+        answer: "C. sehr",
+        options: [
+            "A. fokus",
+            "B. illuziya",
+            "C. sehr",
+            "D. fan"
+        ]
+    },
+    {
+        numb: 10,
+        question: "message",
+        answer: "D. xabar",
+        options: [
+            "A. maktub",
+            "B. qo'ng'iroq",
+            "C. elektron pochta",
+            "D. xabar"
+        ]
+    },
+    {
+        numb: 11,
+        question: "to notice",
+        answer: "A. payqamoq",
+        options: [
+            "A. payqamoq",
+            "B. e'tiborsiz qoldirmoq",
+            "C. o'tkazib yubormoq",
+            "D. unutmoq"
+        ]
+    },
+    {
+        numb: 12,
+        question: "to own",
+        answer: "B. egalik qilmoq",
+        options: [
+            "A. ijaraga olmoq",
+            "B. egalik qilmoq",
+            "C. qarzga olmoq",
+            "D. sotmoq"
+        ]
+    },
+    {
+        numb: 13,
+        question: "to predict",
+        answer: "C. bashorat qilmoq",
+        options: [
+            "A. takrorlamoq",
+            "B. tasvirlamoq",
+            "C. bashorat qilmoq",
+            "D. tushuntirmoq"
+        ]
+    },
+    {
+        numb: 14,
+        question: "professor",
+        answer: "D. professor",
+        options: [
+            "A. o'quvchi",
+            "B. shifokor",
+            "C. tadqiqotchi",
+            "D. professor"
+        ]
+    },
+    {
+        numb: 15,
+        question: "to rush",
+        answer: "B. shoshilmoq",
+        options: [
+            "A. sekin yurmoq",
+            "B. shoshilmoq",
+            "C. ikkilamoq",
+            "D. to'xtamoq"
+        ]
+    },
+    {
+        numb: 16,
+        question: "schedule",
+        answer: "C. jadval",
+        options: [
+            "A. taqvim",
+            "B. vaqt",
+            "C. jadval",
+            "D. vazifa"
+        ]
+    },
+    {
+        numb: 17,
+        question: "to share",
+        answer: "A. bo'lishmoq",
+        options: [
+            "A. bo'lishmoq",
+            "B. saqlamoq",
+            "C. olib qo'ymoq",
+            "D. yashirmoq"
+        ]
+    },
+    {
+        numb: 18,
+        question: "stage",
+        answer: "D. sahna",
+        options: [
+            "A. kinoteatr",
+            "B. konsert",
+            "C. teatr",
+            "D. sahna"
+        ]
+    },
+    {
+        numb: 19,
+        question: "storm",
+        answer: "C. bo'ron",
+        options: [
+            "A. yomg'ir",
+            "B. qor",
+            "C. bo'ron",
+            "D. quyosh"
+        ]
+    },
+    {
+        numb: 20,
+        question: "within",
+        answer: "B. ichida",
+        options: [
+            "A. tashqarida",
+            "B. ichida",
+            "C. yonida",
+            "D. o'rtasida"
+        ]
+    }
+];
