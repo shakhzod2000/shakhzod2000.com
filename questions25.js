8458b9ab3d9bccb5a313820b8087b94e38a4ea51
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 17:28:53 +0500

    Create questions25.js

---
+++ questions25.js
@@ -0,0 +1,222 @@
+let questions25 = [
+    {
+        numb: 1,
+        question: "to associate",
+        answer: "A. verbinden",
+        options: [
+            "A. verbinden",
+            "B. trennen",
+            "C. ignorieren",
+            "D. verwechseln"
+        ]
+    },
+    {
+        numb: 2,
+        question: "environment",
+        answer: "B. Umwelt",
+        options: [
+            "A. Gebäude",
+            "B. Umwelt",
+            "C. Arbeitsplatz",
+            "D. Land"
+        ]
+    },
+    {
+        numb: 3,
+        question: "factory",
+        answer: "C. Fabrik",
+        options: [
+            "A. Büro",
+            "B. Lagerhaus",
+            "C. Fabrik",
+            "D. Werkstatt"
+        ]
+    },
+    {
+        numb: 4,
+        question: "feature",
+        answer: "A. Merkmal",
+        options: [
+            "A. Merkmal",
+            "B. Zubehör",
+            "C. Zubehör",
+            "D. Fehler"
+        ]
+    },
+    {
+        numb: 5,
+        question: "instance",
+        answer: "D. Beispiel",
+        options: [
+            "A. Ereignis",
+            "B. Problem",
+            "C. Moment",
+            "D. Beispiel"
+        ]
+    },
+    {
+        numb: 6,
+        question: "to involve",
+        answer: "A. einbeziehen",
+        options: [
+            "A. einbeziehen",
+            "B. vermeiden",
+            "C. ausschließen",
+            "D. ignorieren"
+        ]
+    },
+    {
+        numb: 7,
+        question: "medicine",
+        answer: "B. Medizin",
+        options: [
+            "A. Nahrung",
+            "B. Medizin",
+            "C. Wasser",
+            "D. Schlaf"
+        ]
+    },
+    {
+        numb: 8,
+        question: "mix",
+        answer: "C. Mischung",
+        options: [
+            "A. Einzelteil",
+            "B. Ganze",
+            "C. Mischung",
+            "D. Trennung"
+        ]
+    },
+    {
+        numb: 9,
+        question: "to organize",
+        answer: "A. organisieren",
+        options: [
+            "A. organisieren",
+            "B. vernachlässigen",
+            "C. auflösen",
+            "D. zerstören"
+        ]
+    },
+    {
+        numb: 10,
+        question: "period",
+        answer: "B. Zeitraum",
+        options: [
+            "A. Punkt",
+            "B. Zeitraum",
+            "C. Zeitpunkt",
+            "D. Epoche"
+        ]
+    },
+    {
+        numb: 11,
+        question: "to populate",
+        answer: "C. besiedeln",
+        options: [
+            "A. zerstören",
+            "B. vernachlässigen",
+            "C. besiedeln",
+            "D. leeren"
+        ]
+    },
+    {
+        numb: 12,
+        question: "to produce",
+        answer: "A. produzieren",
+        options: [
+            "A. produzieren",
+            "B. konsumieren",
+            "C. zerstören",
+            "D. speichern"
+        ]
+    },
+    {
+        numb: 13,
+        question: "range",
+        answer: "D. Auswahl",
+        options: [
+            "A. Punkt",
+            "B. Grenze",
+            "C. Einheit",
+            "D. Auswahl"
+        ]
+    },
+    {
+        numb: 14,
+        question: "to recognize",
+        answer: "B. erkennen",
+        options: [
+            "A. ignorieren",
+            "B. erkennen",
+            "C. verwechseln",
+            "D. übersehen"
+        ]
+    },
+    {
+        numb: 15,
+        question: "regular",
+        answer: "A. regelmäßig",
+        options: [
+            "A. regelmäßig",
+            "B. selten",
+            "C. einmalig",
+            "D. ungleichmäßig"
+        ]
+    },
+    {
+        numb: 16,
+        question: "sign",
+        answer: "C. Schild",
+        options: [
+            "A. Symbol",
+            "B. Wegweiser",
+            "C. Schild",
+            "D. Karte"
+        ]
+    },
+    {
+        numb: 17,
+        question: "tip",
+        answer: "A. Spitze",
+        options: [
+            "A. Spitze",
+            "B. Basis",
+            "C. Seite",
+            "D. Mitte"
+        ]
+    },
+    {
+        numb: 18,
+        question: "tradition",
+        answer: "B. Tradition",
+        options: [
+            "A. Innovation",
+            "B. Tradition",
+            "C. Veränderung",
+            "D. Bruch"
+        ]
+    },
+    {
+        numb: 19,
+        question: "trash",
+        answer: "A. Müll",
+        options: [
+            "A. Müll",
+            "B. Schatz",
+            "C. Gut",
+            "D. Wert"
+        ]
+    },
+    {
+        numb: 20,
+        question: "wide",
+        answer: "B. breit",
+        options: [
+            "A. eng",
+            "B. breit",
+            "C. kurz",
+            "D. klein"
+        ]
+    }
+];
