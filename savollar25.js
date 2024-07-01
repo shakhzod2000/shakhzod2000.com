ecf09534ba7fceb8fb2cdc7241fd4d376e481330
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 17:55:38 +0500

    Create savollar25.js

---
+++ savollar25.js
@@ -0,0 +1,222 @@
+let savollar25 = [
+    {
+        numb: 1,
+        question: "to associate",
+        answer: "A. bog'lamoq",
+        options: [
+            "A. bog'lamoq",
+            "B. ajratmoq",
+            "C. e'tiborsiz qoldirmoq",
+            "D. adashtirmoq"
+        ]
+    },
+    {
+        numb: 2,
+        question: "environment",
+        answer: "B. atrof muhit",
+        options: [
+            "A. bino",
+            "B. atrof muhit",
+            "C. ish joyi",
+            "D. yer"
+        ]
+    },
+    {
+        numb: 3,
+        question: "factory",
+        answer: "C. zavod",
+        options: [
+            "A. ofis",
+            "B. omborxona",
+            "C. zavod",
+            "D. ustaxona"
+        ]
+    },
+    {
+        numb: 4,
+        question: "feature",
+        answer: "A. xususiyat",
+        options: [
+            "A. xususiyat",
+            "B. aksessuar",
+            "C. qo'shimcha",
+            "D. xato"
+        ]
+    },
+    {
+        numb: 5,
+        question: "instance",
+        answer: "D. misol",
+        options: [
+            "A. hodisa",
+            "B. muammo",
+            "C. lahza",
+            "D. misol"
+        ]
+    },
+    {
+        numb: 6,
+        question: "to involve",
+        answer: "A. jalb etmoq",
+        options: [
+            "A. jalb etmoq",
+            "B. oldini olmoq",
+            "C. chiqarib tashlamoq",
+            "D. e'tiborsiz qoldirmoq"
+        ]
+    },
+    {
+        numb: 7,
+        question: "medicine",
+        answer: "B. dori",
+        options: [
+            "A. oziq-ovqat",
+            "B. dori",
+            "C. suv",
+            "D. uyqu"
+        ]
+    },
+    {
+        numb: 8,
+        question: "mix",
+        answer: "C. aralashma",
+        options: [
+            "A. yagona narsa",
+            "B. butun",
+            "C. aralashma",
+            "D. ajratish"
+        ]
+    },
+    {
+        numb: 9,
+        question: "to organize",
+        answer: "A. tashkillashtirmoq",
+        options: [
+            "A. tashkillashtirmoq",
+            "B. e'tiborsiz qoldirmoq",
+            "C. bekor qilmoq",
+            "D. buzmoq"
+        ]
+    },
+    {
+        numb: 10,
+        question: "period",
+        answer: "B. davr",
+        options: [
+            "A. nuqta",
+            "B. davr",
+            "C. vaqt",
+            "D. epoxa"
+        ]
+    },
+    {
+        numb: 11,
+        question: "to populate",
+        answer: "C. yashamoq",
+        options: [
+            "A. yo'q qilmoq",
+            "B. e'tiborsiz qoldirmoq",
+            "C. yashamoq",
+            "D. bo'shatmoq"
+        ]
+    },
+    {
+        numb: 12,
+        question: "to produce",
+        answer: "A. ishlab chiqarmoq",
+        options: [
+            "A. ishlab chiqarmoq",
+            "B. iste'mol qilmoq",
+            "C. yo'q qilmoq",
+            "D. saqlamoq"
+        ]
+    },
+    {
+        numb: 13,
+        question: "range",
+        answer: "D. qator",
+        options: [
+            "A. nuqta",
+            "B. chegara",
+            "C. birlik",
+            "D. qator"
+        ]
+    },
+    {
+        numb: 14,
+        question: "to recognize",
+        answer: "B. tanimoq",
+        options: [
+            "A. e'tiborsiz qoldirmoq",
+            "B. tanimoq",
+            "C. adashtirmoq",
+            "D. sezmaslik"
+        ]
+    },
+    {
+        numb: 15,
+        question: "regular",
+        answer: "A. muntazam",
+        options: [
+            "A. muntazam",
+            "B. kamdan-kam",
+            "C. bir martalik",
+            "D. notekis"
+        ]
+    },
+    {
+        numb: 16,
+        question: "sign",
+        answer: "C. belgi",
+        options: [
+            "A. ramz",
+            "B. yo'l ko'rsatkich",
+            "C. belgi",
+            "D. xarita"
+        ]
+    },
+    {
+        numb: 17,
+        question: "tip",
+        answer: "A. uch",
+        options: [
+            "A. uch",
+            "B. asos",
+            "C. yon",
+            "D. markaz"
+        ]
+    },
+    {
+        numb: 18,
+        question: "tradition",
+        answer: "B. an'ana",
+        options: [
+            "A. innovatsiya",
+            "B. an'ana",
+            "C. o'zgarish",
+            "D. uzilish"
+        ]
+    },
+    {
+        numb: 19,
+        question: "trash",
+        answer: "A. axlat",
+        options: [
+            "A. axlat",
+            "B. boylik",
+            "C. yaxshi narsa",
+            "D. qiymat"
+        ]
+    },
+    {
+        numb: 20,
+        question: "wide",
+        answer: "B. keng",
+        options: [
+            "A. tor",
+            "B. keng",
+            "C. qisqa",
+            "D. kichik"
+        ]
+    }
+];
