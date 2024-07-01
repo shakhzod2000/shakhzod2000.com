cc8f500c7cb7786dd6ccddf2a09d7e0e4f38317a
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 17:53:22 +0500

    Create savollar21.js

---
+++ savollar21.js
@@ -0,0 +1,222 @@
+let savollar21 = [
+    {
+        numb: 1,
+        question: "to appear",
+        answer: "A. ko'rinmoq",
+        options: [
+            "A. ko'rinmoq",
+            "B. g'oyib bo'lmoq",
+            "C. yashirinmoq",
+            "D. e'tiborsiz qoldirmoq"
+        ]
+    },
+    {
+        numb: 2,
+        question: "base",
+        answer: "B. asos",
+        options: [
+            "A. cho'qqi",
+            "B. asos",
+            "C. o'rtasi",
+            "D. chekka"
+        ]
+    },
+    {
+        numb: 3,
+        question: "brain",
+        answer: "C. miya",
+        options: [
+            "A. yurak",
+            "B. o'pka",
+            "C. miya",
+            "D. oshqozon"
+        ]
+    },
+    {
+        numb: 4,
+        question: "career",
+        answer: "D. faoliyat",
+        options: [
+            "A. hobi",
+            "B. vazifa",
+            "C. ish kuni",
+            "D. faoliyat"
+        ]
+    },
+    {
+        numb: 5,
+        question: "clerk",
+        answer: "A. do'kon xodimi",
+        options: [
+            "A. do'kon xodimi",
+            "B. mijoz",
+            "C. menejer",
+            "D. egasi"
+        ]
+    },
+    {
+        numb: 6,
+        question: "effort",
+        answer: "B. harakat",
+        options: [
+            "A. osonlik",
+            "B. harakat",
+            "C. bekorchilik",
+            "D. dangasalik"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to enter",
+        answer: "C. kirmoq",
+        options: [
+            "A. chiqish",
+            "B. chetlashmoq",
+            "C. kirmoq",
+            "D. chiqib ketmoq"
+        ]
+    },
+    {
+        numb: 8,
+        question: "excellent",
+        answer: "A. a'lo",
+        options: [
+            "A. a'lo",
+            "B. yomon",
+            "C. o'rtacha",
+            "D. yetarli emas"
+        ]
+    },
+    {
+        numb: 9,
+        question: "hero",
+        answer: "D. qahramon",
+        options: [
+            "A. qo'rqoq",
+            "B. tomoshabin",
+            "C. yovuz odam",
+            "D. qahramon"
+        ]
+    },
+    {
+        numb: 10,
+        question: "to hurry",
+        answer: "B. shoshilmoq",
+        options: [
+            "A. sekin yurmoq",
+            "B. shoshilmoq",
+            "C. turgan joyida qolmoq",
+            "D. sayr qilmoq"
+        ]
+    },
+    {
+        numb: 11,
+        question: "to inform",
+        answer: "A. xabar bermoq",
+        options: [
+            "A. xabar bermoq",
+            "B. e'tiborsiz qoldirmoq",
+            "C. yolg'on gapirmoq",
+            "D. jim bo'lmoq"
+        ]
+    },
+    {
+        numb: 12,
+        question: "later",
+        answer: "B. keyinroq",
+        options: [
+            "A. oldinroq",
+            "B. keyinroq",
+            "C. darhol",
+            "D. hech qachon"
+        ]
+    },
+    {
+        numb: 13,
+        question: "to leave",
+        answer: "A. tark etmoq",
+        options: [
+            "A. tark etmoq",
+            "B. qolmoq",
+            "C. kelmoq",
+            "D. kirish"
+        ]
+    },
+    {
+        numb: 14,
+        question: "to locate",
+        answer: "D. topmoq",
+        options: [
+            "A. yo'qotmoq",
+            "B. yashirmoq",
+            "C. unutmoq",
+            "D. topmoq"
+        ]
+    },
+    {
+        numb: 15,
+        question: "nurse",
+        answer: "A. hamshira",
+        options: [
+            "A. hamshira",
+            "B. shifokor",
+            "C. bemor",
+            "D. tashrif buyuruvchi"
+        ]
+    },
+    {
+        numb: 16,
+        question: "operation",
+        answer: "C. operatsiya",
+        options: [
+            "A. tekshiruv",
+            "B. maslahat",
+            "C. operatsiya",
+            "D. davolash"
+        ]
+    },
+    {
+        numb: 17,
+        question: "pain",
+        answer: "B. og'riq",
+        options: [
+            "A. xursandchilik",
+            "B. og'riq",
+            "C. lazzat",
+            "D. baxt"
+        ]
+    },
+    {
+        numb: 18,
+        question: "to refuse",
+        answer: "A. rad etmoq",
+        options: [
+            "A. rad etmoq",
+            "B. qabul qilmoq",
+            "C. so'ramoq",
+            "D. tasdiqlamoq"
+        ]
+    },
+    {
+        numb: 19,
+        question: "though",
+        answer: "B. ga qaramasdan",
+        options: [
+            "A. chunki",
+            "B. ga qaramasdan",
+            "C. agar",
+            "D. uchun"
+        ]
+    },
+    {
+        numb: 20,
+        question: "various",
+        answer: "D. turli xil",
+        options: [
+            "A. bir xil",
+            "B. o'xshash",
+            "C. yagona",
+            "D. turli xil"
+        ]
+    }
+];
