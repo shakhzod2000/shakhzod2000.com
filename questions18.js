bb810a34062e2bdf8e43b46b11316356f431b432
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 17:17:27 +0500

    Create questions18.js

---
+++ questions18.js
@@ -0,0 +1,222 @@
+let questions18 = [
+    {
+        numb: 1,
+        question: "to argue",
+        answer: "A. streiten",
+        options: [
+            "A. streiten",
+            "B. zustimmen",
+            "C. schweigen",
+            "D. erklären"
+        ]
+    },
+    {
+        numb: 2,
+        question: "to communicate",
+        answer: "B. kommunizieren",
+        options: [
+            "A. schweigen",
+            "B. kommunizieren",
+            "C. missverstehen",
+            "D. trennen"
+        ]
+    },
+    {
+        numb: 3,
+        question: "crowd",
+        answer: "C. Gedränge",
+        options: [
+            "A. Einzelperson",
+            "B. Ruhe",
+            "C. Gedränge",
+            "D. Einsamkeit"
+        ]
+    },
+    {
+        numb: 4,
+        question: "to depend",
+        answer: "A. abhängen",
+        options: [
+            "A. abhängen",
+            "B. bestimmen",
+            "C. verweigern",
+            "D. ändern"
+        ]
+    },
+    {
+        numb: 5,
+        question: "dish",
+        answer: "D. Teller",
+        options: [
+            "A. Tasse",
+            "B. Glas",
+            "C. Schüssel",
+            "D. Teller"
+        ]
+    },
+    {
+        numb: 6,
+        question: "empty",
+        answer: "B. leer",
+        options: [
+            "A. voll",
+            "B. leer",
+            "C. schwer",
+            "D. klein"
+        ]
+    },
+    {
+        numb: 7,
+        question: "exact",
+        answer: "C. genau",
+        options: [
+            "A. ungefähr",
+            "B. etwa",
+            "C. genau",
+            "D. ungenau"
+        ]
+    },
+    {
+        numb: 8,
+        question: "fresh",
+        answer: "A. frisch",
+        options: [
+            "A. frisch",
+            "B. alt",
+            "C. faul",
+            "D. trocken"
+        ]
+    },
+    {
+        numb: 9,
+        question: "to gather",
+        answer: "D. sammeln",
+        options: [
+            "A. verstreuen",
+            "B. verlieren",
+            "C. geben",
+            "D. sammeln"
+        ]
+    },
+    {
+        numb: 10,
+        question: "to indicate",
+        answer: "B. hindeuten",
+        options: [
+            "A. verstecken",
+            "B. hindeuten",
+            "C. verschleiern",
+            "D. ignorieren"
+        ]
+    },
+    {
+        numb: 11,
+        question: "item",
+        answer: "A. Gegenstand",
+        options: [
+            "A. Gegenstand",
+            "B. Person",
+            "C. Ort",
+            "D. Idee"
+        ]
+    },
+    {
+        numb: 12,
+        question: "to offer",
+        answer: "C. anbieten",
+        options: [
+            "A. nehmen",
+            "B. verweigern",
+            "C. anbieten",
+            "D. verstecken"
+        ]
+    },
+    {
+        numb: 13,
+        question: "price",
+        answer: "D. Preis",
+        options: [
+            "A. Wert",
+            "B. Rabatt",
+            "C. Steuer",
+            "D. Preis"
+        ]
+    },
+    {
+        numb: 14,
+        question: "product",
+        answer: "B. Produkt",
+        options: [
+            "A. Dienst",
+            "B. Produkt",
+            "C. Idee",
+            "D. Theorie"
+        ]
+    },
+    {
+        numb: 15,
+        question: "property",
+        answer: "A. Eigentum",
+        options: [
+            "A. Eigentum",
+            "B. Miete",
+            "C. Hypothek",
+            "D. Schuld"
+        ]
+    },
+    {
+        numb: 16,
+        question: "to purchase",
+        answer: "D. kaufen",
+        options: [
+            "A. verkaufen",
+            "B. verleihen",
+            "C. tauschen",
+            "D. kaufen"
+        ]
+    },
+    {
+        numb: 17,
+        question: "to recommend",
+        answer: "C. empfehlen",
+        options: [
+            "A. ablehnen",
+            "B. vergessen",
+            "C. empfehlen",
+            "D. unterlassen"
+        ]
+    },
+    {
+        numb: 18,
+        question: "to select",
+        answer: "B. auswählen",
+        options: [
+            "A. ignorieren",
+            "B. auswählen",
+            "C. fallen lassen",
+            "D. vergessen"
+        ]
+    },
+    {
+        numb: 19,
+        question: "tool",
+        answer: "A. Werkzeug",
+        options: [
+            "A. Werkzeug",
+            "B. Spielzeug",
+            "C. Gerät",
+            "D. Maschine"
+        ]
+    },
+    {
+        numb: 20,
+        question: "to treat",
+        answer: "D. behandeln",
+        options: [
+            "A. ignorieren",
+            "B. vergessen",
+            "C. vermeiden",
+            "D. behandeln"
+        ]
+    }
+];
