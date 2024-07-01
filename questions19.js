bfea808da8a283a961a507f28788831b2191d2eb
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 17:19:48 +0500

    Create questions19.js

---
+++ questions19.js
@@ -0,0 +1,222 @@
+let questions19 = [
+    {
+        numb: 1,
+        question: "alive",
+        answer: "B. lebendig",
+        options: [
+            "A. tot",
+            "B. lebendig",
+            "C. krank",
+            "D. müde"
+        ]
+    },
+    {
+        numb: 2,
+        question: "bone",
+        answer: "C. Knochen",
+        options: [
+            "A. Haut",
+            "B. Fleisch",
+            "C. Knochen",
+            "D. Muskel"
+        ]
+    },
+    {
+        numb: 3,
+        question: "to bother",
+        answer: "A. sich bemühen",
+        options: [
+            "A. sich bemühen",
+            "B. ignorieren",
+            "C. ärgern",
+            "D. stören"
+        ]
+    },
+    {
+        numb: 4,
+        question: "captain",
+        answer: "D. Kapitän",
+        options: [
+            "A. Soldat",
+            "B. Pilot",
+            "C. Offizier",
+            "D. Kapitän"
+        ]
+    },
+    {
+        numb: 5,
+        question: "conclusion",
+        answer: "B. Schluss",
+        options: [
+            "A. Anfang",
+            "B. Schluss",
+            "C. Mitte",
+            "D. Folge"
+        ]
+    },
+    {
+        numb: 6,
+        question: "doubt",
+        answer: "C. Zweifel",
+        options: [
+            "A. Sicherheit",
+            "B. Vertrauen",
+            "C. Zweifel",
+            "D. Glaube"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to explore",
+        answer: "A. erforschen",
+        options: [
+            "A. erforschen",
+            "B. vermeiden",
+            "C. ignorieren",
+            "D. schützen"
+        ]
+    },
+    {
+        numb: 8,
+        question: "foreign",
+        answer: "D. fremd",
+        options: [
+            "A. heimisch",
+            "B. vertraut",
+            "C. lokal",
+            "D. fremd"
+        ]
+    },
+    {
+        numb: 9,
+        question: "glad",
+        answer: "B. froh",
+        options: [
+            "A. traurig",
+            "B. froh",
+            "C. wütend",
+            "D. besorgt"
+        ]
+    },
+    {
+        numb: 10,
+        question: "however",
+        answer: "A. jedoch",
+        options: [
+            "A. jedoch",
+            "B. deshalb",
+            "C. außerdem",
+            "D. trotzdem"
+        ]
+    },
+    {
+        numb: 11,
+        question: "injustice",
+        answer: "C. Ungerechtigkeit",
+        options: [
+            "A. Fairness",
+            "B. Gerechtigkeit",
+            "C. Ungerechtigkeit",
+            "D. Ordnung"
+        ]
+    },
+    {
+        numb: 12,
+        question: "international",
+        answer: "D. international",
+        options: [
+            "A. national",
+            "B. lokal",
+            "C. regional",
+            "D. international"
+        ]
+    },
+    {
+        numb: 13,
+        question: "lawyer",
+        answer: "A. Anwalt",
+        options: [
+            "A. Anwalt",
+            "B. Richter",
+            "C. Polizist",
+            "D. Zeuge"
+        ]
+    },
+    {
+        numb: 14,
+        question: "to mention",
+        answer: "B. erwähnen",
+        options: [
+            "A. schweigen",
+            "B. erwähnen",
+            "C. ignorieren",
+            "D. verbergen"
+        ]
+    },
+    {
+        numb: 15,
+        question: "policy",
+        answer: "C. Richtlinie",
+        options: [
+            "A. Meinung",
+            "B. Vorschlag",
+            "C. Richtlinie",
+            "D. Gesetz"
+        ]
+    },
+    {
+        numb: 16,
+        question: "social",
+        answer: "A. sozial",
+        options: [
+            "A. sozial",
+            "B. allein",
+            "C. zurückgezogen",
+            "D. individuell"
+        ]
+    },
+    {
+        numb: 17,
+        question: "speech",
+        answer: "D. Rede",
+        options: [
+            "A. Gespräch",
+            "B. Diskussion",
+            "C. Interview",
+            "D. Rede"
+        ]
+    },
+    {
+        numb: 18,
+        question: "staff",
+        answer: "B. Belegschaft",
+        options: [
+            "A. Besucher",
+            "B. Belegschaft",
+            "C. Gäste",
+            "D. Kunden"
+        ]
+    },
+    {
+        numb: 19,
+        question: "toward",
+        answer: "C. in Richtung",
+        options: [
+            "A. weg von",
+            "B. um",
+            "C. in Richtung",
+            "D. entfernt von"
+        ]
+    },
+    {
+        numb: 20,
+        question: "wood",
+        answer: "A. Holz",
+        options: [
+            "A. Holz",
+            "B. Metall",
+            "C. Stein",
+            "D. Glas"
+        ]
+    }
+];
