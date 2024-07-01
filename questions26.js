79bbddf5d7d7ab1681dc6b377b6212d2ea7cb857
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 17:29:30 +0500

    Create questions26.js

---
+++ questions26.js
@@ -0,0 +1,222 @@
+let questions26 = [
+    {
+        numb: 1,
+        question: "advice",
+        answer: "A. Ratschlag",
+        options: [
+            "A. Ratschlag",
+            "B. Vorwurf",
+            "C. Warnung",
+            "D. Anweisung"
+        ]
+    },
+    {
+        numb: 2,
+        question: "along",
+        answer: "B. entlang",
+        options: [
+            "A. durch",
+            "B. entlang",
+            "C. gegenüber",
+            "D. hinter"
+        ]
+    },
+    {
+        numb: 3,
+        question: "attention",
+        answer: "C. Aufmerksamkeit",
+        options: [
+            "A. Zustimmung",
+            "B. Interesse",
+            "C. Aufmerksamkeit",
+            "D. Respekt"
+        ]
+    },
+    {
+        numb: 4,
+        question: "to attract",
+        answer: "A. anziehen",
+        options: [
+            "A. anziehen",
+            "B. abstoßen",
+            "C. ignorieren",
+            "D. loslassen"
+        ]
+    },
+    {
+        numb: 5,
+        question: "to climb",
+        answer: "D. klettern",
+        options: [
+            "A. laufen",
+            "B. springen",
+            "C. rennen",
+            "D. klettern"
+        ]
+    },
+    {
+        numb: 6,
+        question: "to drop",
+        answer: "B. fallen lassen",
+        options: [
+            "A. aufheben",
+            "B. fallen lassen",
+            "C. halten",
+            "D. werfen"
+        ]
+    },
+    {
+        numb: 7,
+        question: "final",
+        answer: "C. letzte",
+        options: [
+            "A. erste",
+            "B. mittlere",
+            "C. letzte",
+            "D. vorherige"
+        ]
+    },
+    {
+        numb: 8,
+        question: "further",
+        answer: "A. weiter",
+        options: [
+            "A. weiter",
+            "B. näher",
+            "C. vorher",
+            "D. später"
+        ]
+    },
+    {
+        numb: 9,
+        question: "to imply",
+        answer: "D. andeuten",
+        options: [
+            "A. erklären",
+            "B. sagen",
+            "C. zeigen",
+            "D. andeuten"
+        ]
+    },
+    {
+        numb: 10,
+        question: "to maintain",
+        answer: "B. aufrechterhalten",
+        options: [
+            "A. ändern",
+            "B. aufrechterhalten",
+            "C. zerstören",
+            "D. vermeiden"
+        ]
+    },
+    {
+        numb: 11,
+        question: "neither",
+        answer: "C. weder",
+        options: [
+            "A. entweder",
+            "B. sowohl",
+            "C. weder",
+            "D. noch"
+        ]
+    },
+    {
+        numb: 12,
+        question: "otherwise",
+        answer: "D. ansonsten",
+        options: [
+            "A. jedoch",
+            "B. obwohl",
+            "C. deshalb",
+            "D. ansonsten"
+        ]
+    },
+    {
+        numb: 13,
+        question: "physical",
+        answer: "A. körperlich",
+        options: [
+            "A. körperlich",
+            "B. geistig",
+            "C. emotional",
+            "D. intellektuell"
+        ]
+    },
+    {
+        numb: 14,
+        question: "to prove",
+        answer: "B. beweisen",
+        options: [
+            "A. vermuten",
+            "B. beweisen",
+            "C. behaupten",
+            "D. widerlegen"
+        ]
+    },
+    {
+        numb: 15,
+        question: "to react",
+        answer: "A. reagieren",
+        options: [
+            "A. reagieren",
+            "B. ignorieren",
+            "C. beschuldigen",
+            "D. verändern"
+        ]
+    },
+    {
+        numb: 16,
+        question: "to ride",
+        answer: "D. reiten",
+        options: [
+            "A. springen",
+            "B. schwimmen",
+            "C. fliegen",
+            "D. reiten"
+        ]
+    },
+    {
+        numb: 17,
+        question: "situated",
+        answer: "C. gelegen",
+        options: [
+            "A. verloren",
+            "B. entfernt",
+            "C. gelegen",
+            "D. gefunden"
+        ]
+    },
+    {
+        numb: 18,
+        question: "society",
+        answer: "B. Gesellschaft",
+        options: [
+            "A. Familie",
+            "B. Gesellschaft",
+            "C. Freundeskreis",
+            "D. Gemeinschaft"
+        ]
+    },
+    {
+        numb: 19,
+        question: "standard",
+        answer: "D. Standard",
+        options: [
+            "A. Ausnahme",
+            "B. Durchschnitt",
+            "C. Grenze",
+            "D. Standard"
+        ]
+    },
+    {
+        numb: 20,
+        question: "to suggest",
+        answer: "A. vorschlagen",
+        options: [
+            "A. vorschlagen",
+            "B. ablehnen",
+            "C. zwingen",
+            "D. ignorieren"
+        ]
+    }
+];
