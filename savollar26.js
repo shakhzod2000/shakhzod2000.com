ec2da62230eb530e611d1bed90b7fa687807d0ee
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 17:56:10 +0500

    Create savollar26.js

---
+++ savollar26.js
@@ -0,0 +1,222 @@
+let savollar26 = [
+    {
+        numb: 1,
+        question: "advice",
+        answer: "a. maslahat",
+        options: [
+            "a. maslahat",
+            "b. ayblov",
+            "c. ogohlantirish",
+            "d. buyruq"
+        ]
+    },
+    {
+        numb: 2,
+        question: "along",
+        answer: "b. bo'ylab",
+        options: [
+            "a. orqali",
+            "b. bo'ylab",
+            "c. qarshi",
+            "d. orqasida"
+        ]
+    },
+    {
+        numb: 3,
+        question: "attention",
+        answer: "c. e'tibor",
+        options: [
+            "a. rozi bo'lish",
+            "b. qiziqish",
+            "c. e'tibor",
+            "d. hurmat"
+        ]
+    },
+    {
+        numb: 4,
+        question: "to attract",
+        answer: "a. jalb qilmoq",
+        options: [
+            "a. jalb qilmoq",
+            "b. itarmoq",
+            "c. e'tiborsiz qoldirmoq",
+            "d. qo'yib yubormoq"
+        ]
+    },
+    {
+        numb: 5,
+        question: "to climb",
+        answer: "d. tirmashib chiqmoq",
+        options: [
+            "a. yurmoq",
+            "b. sakramoq",
+            "c. yugurmoq",
+            "d. tirmashib chiqmoq"
+        ]
+    },
+    {
+        numb: 6,
+        question: "to drop",
+        answer: "b. tomchilamoq",
+        options: [
+            "a. ko'tarmoq",
+            "b. tomchilamoq",
+            "c. ushlab turmoq",
+            "d. tashlamoq"
+        ]
+    },
+    {
+        numb: 7,
+        question: "final",
+        answer: "c. oxirgi",
+        options: [
+            "a. birinchi",
+            "b. o'rta",
+            "c. oxirgi",
+            "d. oldingi"
+        ]
+    },
+    {
+        numb: 8,
+        question: "further",
+        answer: "a. uzoqroq",
+        options: [
+            "a. uzoqroq",
+            "b. yaqinroq",
+            "c. oldinroq",
+            "d. keyinroq"
+        ]
+    },
+    {
+        numb: 9,
+        question: "to imply",
+        answer: "d. nazarda tutmoq",
+        options: [
+            "a. tushuntirmoq",
+            "b. aytyapti",
+            "c. ko'rsatmoq",
+            "d. nazarda tutmoq"
+        ]
+    },
+    {
+        numb: 10,
+        question: "to maintain",
+        answer: "b. saqlamoq",
+        options: [
+            "a. o'zgartirmoq",
+            "b. saqlamoq",
+            "c. buzmoq",
+            "d. oldini olmoq"
+        ]
+    },
+    {
+        numb: 11,
+        question: "neither",
+        answer: "c. na",
+        options: [
+            "a. yoki",
+            "b. ham",
+            "c. na",
+            "d. hamda"
+        ]
+    },
+    {
+        numb: 12,
+        question: "otherwise",
+        answer: "d. aks holda",
+        options: [
+            "a. lekin",
+            "b. garchi",
+            "c. shu sababli",
+            "d. aks holda"
+        ]
+    },
+    {
+        numb: 13,
+        question: "physical",
+        answer: "a. jismoniy",
+        options: [
+            "a. jismoniy",
+            "b. ruhiy",
+            "c. hissiy",
+            "d. intelektual"
+        ]
+    },
+    {
+        numb: 14,
+        question: "to prove",
+        answer: "b. isbotlamoq",
+        options: [
+            "a. taxmin qilmoq",
+            "b. isbotlamoq",
+            "c. da'vo qilmoq",
+            "d. rad etmoq"
+        ]
+    },
+    {
+        numb: 15,
+        question: "to react",
+        answer: "a. munosabat bildirmoq",
+        options: [
+            "a. munosabat bildirmoq",
+            "b. e'tiborsiz qoldirmoq",
+            "c. ayblamoq",
+            "d. o'zgartirmoq"
+        ]
+    },
+    {
+        numb: 16,
+        question: "to ride",
+        answer: "d. minmoq",
+        options: [
+            "a. sakramoq",
+            "b. suzmoq",
+            "c. uchoq",
+            "d. minmoq"
+        ]
+    },
+    {
+        numb: 17,
+        question: "situated",
+        answer: "c. joylashgan",
+        options: [
+            "a. yo'qolgan",
+            "b. uzoq",
+            "c. joylashgan",
+            "d. topilgan"
+        ]
+    },
+    {
+        numb: 18,
+        question: "society",
+        answer: "b. jamiyat",
+        options: [
+            "a. oila",
+            "b. jamiyat",
+            "c. do'stlar",
+            "d. jamoa"
+        ]
+    },
+    {
+        numb: 19,
+        question: "standard",
+        answer: "d. me'zon",
+        options: [
+            "a. istisno",
+            "b. o'rtacha",
+            "c. chegara",
+            "d. me'zon"
+        ]
+    },
+    {
+        numb: 20,
+        question: "to suggest",
+        answer: "a. taklif qilmoq",
+        options: [
+            "a. taklif qilmoq",
+            "b. rad etmoq",
+            "c. majburlamoq",
+            "d. e'tiborsiz qoldirmoq"
+        ]
+    }
+];
