43acbf93b5b1d9f79c44d2e48a788ed44aa4c453
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 17:53:48 +0500

    Create savollar22.js

---
+++ savollar22.js
@@ -0,0 +1,222 @@
+let savollar22 = [
+    {
+        numb: 1,
+        question: "actual",
+        answer: "A. haqiqiy",
+        options: [
+            "A. haqiqiy",
+            "B. soxta",
+            "C. tasavvuriy",
+            "D. ehtimoliy"
+        ]
+    },
+    {
+        numb: 2,
+        question: "to amaze",
+        answer: "B. hayratda qoldirmoq",
+        options: [
+            "A. zeriktirmoq",
+            "B. hayratda qoldirmoq",
+            "C. chalg'itmoq",
+            "D. hafsalasi pir bo'lmoq"
+        ]
+    },
+    {
+        numb: 3,
+        question: "charge",
+        answer: "D. to'lov",
+        options: [
+            "A. chegirma",
+            "B. sovg'a",
+            "C. qarz",
+            "D. to'lov"
+        ]
+    },
+    {
+        numb: 4,
+        question: "to comfort",
+        answer: "C. taskin bermoq",
+        options: [
+            "A. e'tiborsiz qoldirmoq",
+            "B. tanqid qilmoq",
+            "C. taskin bermoq",
+            "D. qo'rqitmoq"
+        ]
+    },
+    {
+        numb: 5,
+        question: "to contact",
+        answer: "B. aloqaga chiqmoq",
+        options: [
+            "A. chetlab o'tmoq",
+            "B. aloqaga chiqmoq",
+            "C. e'tiborsiz qoldirmoq",
+            "D. unutmoq"
+        ]
+    },
+    {
+        numb: 6,
+        question: "customer",
+        answer: "A. mijoz",
+        options: [
+            "A. mijoz",
+            "B. sotuvchi",
+            "C. ishlab chiqaruvchi",
+            "D. yetkazib beruvchi"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to deliver",
+        answer: "D. yetkazib bermoq",
+        options: [
+            "A. saqlamoq",
+            "B. qabul qilmoq",
+            "C. o'rab bermoq",
+            "D. yetkazib bermoq"
+        ]
+    },
+    {
+        numb: 8,
+        question: "to earn",
+        answer: "B. ishlab topmoq",
+        options: [
+            "A. sarflamoq",
+            "B. ishlab topmoq",
+            "C. isrof qilmoq",
+            "D. qarz olmoq"
+        ]
+    },
+    {
+        numb: 9,
+        question: "gate",
+        answer: "C. darvoza",
+        options: [
+            "A. deraza",
+            "B. eshik",
+            "C. darvoza",
+            "D. panjara"
+        ]
+    },
+    {
+        numb: 10,
+        question: "to include",
+        answer: "A. o'z ichiga olmoq",
+        options: [
+            "A. o'z ichiga olmoq",
+            "B. chiqarib tashlamoq",
+            "C. ajratmoq",
+            "D. olib tashlamoq"
+        ]
+    },
+    {
+        numb: 11,
+        question: "to manage",
+        answer: "D. boshqarmoq",
+        options: [
+            "A. e'tiborsiz qoldirmoq",
+            "B. buzmoq",
+            "C. qo'llab-quvvatlamoq",
+            "D. boshqarmoq"
+        ]
+    },
+    {
+        numb: 12,
+        question: "mystery",
+        answer: "B. sir",
+        options: [
+            "A. aniqlik",
+            "B. sir",
+            "C. ishonch",
+            "D. shaffoflik"
+        ]
+    },
+    {
+        numb: 13,
+        question: "to occur",
+        answer: "A. sodir bo'lmoq",
+        options: [
+            "A. sodir bo'lmoq",
+            "B. oldini olmoq",
+            "C. unutmoq",
+            "D. rejalashtirmoq"
+        ]
+    },
+    {
+        numb: 14,
+        question: "opposite",
+        answer: "C. teskari",
+        options: [
+            "A. o'xshashlik",
+            "B. taqqoslash",
+            "C. teskari",
+            "D. parallel"
+        ]
+    },
+    {
+        numb: 15,
+        question: "plate",
+        answer: "B. likopcha",
+        options: [
+            "A. stakan",
+            "B. likopcha",
+            "C. kosa",
+            "D. kosani"
+        ]
+    },
+    {
+        numb: 16,
+        question: "to receive",
+        answer: "A. qabul qilmoq",
+        options: [
+            "A. qabul qilmoq",
+            "B. yubormoq",
+            "C. yo'qotmoq",
+            "D. rad etmoq"
+        ]
+    },
+    {
+        numb: 17,
+        question: "reward",
+        answer: "C. mukofot",
+        options: [
+            "A. jazo",
+            "B. majburiyat",
+            "C. mukofot",
+            "D. qiyinchilik"
+        ]
+    },
+    {
+        numb: 18,
+        question: "to set",
+        answer: "B. qo'ymoq",
+        options: [
+            "A. tashlamoq",
+            "B. qo'ymoq",
+            "C. olmoq",
+            "D. sindirmoq"
+        ]
+    },
+    {
+        numb: 19,
+        question: "to steal",
+        answer: "A. o'g'irlamoq",
+        options: [
+            "A. o'g'irlamoq",
+            "B. sotib olmoq",
+            "C. topmoq",
+            "D. yo'qotmoq"
+        ]
+    },
+    {
+        numb: 20,
+        question: "thief",
+        answer: "D. o'g'ri",
+        options: [
+            "A. militsioner",
+            "B. sotuvchi",
+            "C. mijoz",
+            "D. o'g'ri"
+        ]
+    }
+];
