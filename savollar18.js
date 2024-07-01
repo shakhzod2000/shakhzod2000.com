8fae33194501f1f2467a8824decc002795e79f2b
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 17:49:37 +0500

    Create savollar18.js

---
+++ savollar18.js
@@ -0,0 +1,222 @@
+let savollar18 = [
+    {
+        numb: 1,
+        question: "to argue",
+        answer: "A. bahslashmoq",
+        options: [
+            "A. bahslashmoq",
+            "B. rozi bo'lmoq",
+            "C. jim bo'lmoq",
+            "D. tushuntirmoq"
+        ]
+    },
+    {
+        numb: 2,
+        question: "to communicate",
+        answer: "B. muloqot qilmoq",
+        options: [
+            "A. jim bo'lmoq",
+            "B. muloqot qilmoq",
+            "C. noto'g'ri tushunmoq",
+            "D. ajratmoq"
+        ]
+    },
+    {
+        numb: 3,
+        question: "crowd",
+        answer: "C. olomon",
+        options: [
+            "A. yakka shaxs",
+            "B. tinchlik",
+            "C. olomon",
+            "D. yolg'izlik"
+        ]
+    },
+    {
+        numb: 4,
+        question: "to depend",
+        answer: "A. bog'liq bo'lmoq",
+        options: [
+            "A. bog'liq bo'lmoq",
+            "B. aniqlamoq",
+            "C. rad etmoq",
+            "D. o'zgartirmoq"
+        ]
+    },
+    {
+        numb: 5,
+        question: "dish",
+        answer: "D. idish",
+        options: [
+            "A. chashka",
+            "B. stakan",
+            "C. kosa",
+            "D. idish"
+        ]
+    },
+    {
+        numb: 6,
+        question: "empty",
+        answer: "B. bo'sh",
+        options: [
+            "A. to'la",
+            "B. bo'sh",
+            "C. og'ir",
+            "D. kichik"
+        ]
+    },
+    {
+        numb: 7,
+        question: "exact",
+        answer: "C. aniq",
+        options: [
+            "A. taxminan",
+            "B. qariyb",
+            "C. aniq",
+            "D. noaniq"
+        ]
+    },
+    {
+        numb: 8,
+        question: "fresh",
+        answer: "A. yangi",
+        options: [
+            "A. yangi",
+            "B. eski",
+            "C. chirigan",
+            "D. quruq"
+        ]
+    },
+    {
+        numb: 9,
+        question: "to gather",
+        answer: "D. yig'moq",
+        options: [
+            "A. sochib yubormoq",
+            "B. yo'qotmoq",
+            "C. bermoq",
+            "D. yig'moq"
+        ]
+    },
+    {
+        numb: 10,
+        question: "to indicate",
+        answer: "B. ko'rsatmoq",
+        options: [
+            "A. yashirmoq",
+            "B. ko'rsatmoq",
+            "C. yashirmoq",
+            "D. e'tibor bermaslik"
+        ]
+    },
+    {
+        numb: 11,
+        question: "item",
+        answer: "A. buyum",
+        options: [
+            "A. buyum",
+            "B. shaxs",
+            "C. joy",
+            "D. g'oya"
+        ]
+    },
+    {
+        numb: 12,
+        question: "to offer",
+        answer: "C. taklif qilmoq",
+        options: [
+            "A. olmoq",
+            "B. rad etmoq",
+            "C. taklif qilmoq",
+            "D. yashirmoq"
+        ]
+    },
+    {
+        numb: 13,
+        question: "price",
+        answer: "D. narx",
+        options: [
+            "A. qiymat",
+            "B. chegirma",
+            "C. soliq",
+            "D. narx"
+        ]
+    },
+    {
+        numb: 14,
+        question: "product",
+        answer: "B. mahsulot",
+        options: [
+            "A. xizmat",
+            "B. mahsulot",
+            "C. g'oya",
+            "D. nazariya"
+        ]
+    },
+    {
+        numb: 15,
+        question: "property",
+        answer: "A. mulk",
+        options: [
+            "A. mulk",
+            "B. ijaraga olish",
+            "C. ipoteka",
+            "D. qarz"
+        ]
+    },
+    {
+        numb: 16,
+        question: "to purchase",
+        answer: "D. sotib olmoq",
+        options: [
+            "A. sotmoq",
+            "B. qarzga bermoq",
+            "C. almashtirmoq",
+            "D. sotib olmoq"
+        ]
+    },
+    {
+        numb: 17,
+        question: "to recommend",
+        answer: "C. tavsiya qilmoq",
+        options: [
+            "A. rad etmoq",
+            "B. unutmoq",
+            "C. tavsiya qilmoq",
+            "D. qilmaslik"
+        ]
+    },
+    {
+        numb: 18,
+        question: "to select",
+        answer: "B. tanlamoq",
+        options: [
+            "A. e'tibor bermaslik",
+            "B. tanlamoq",
+            "C. tashlab yubormoq",
+            "D. unutmoq"
+        ]
+    },
+    {
+        numb: 19,
+        question: "tool",
+        answer: "A. asbob",
+        options: [
+            "A. asbob",
+            "B. o'yinchoq",
+            "C. qurilma",
+            "D. mashina"
+        ]
+    },
+    {
+        numb: 20,
+        question: "to treat",
+        answer: "D. muomala qilmoq",
+        options: [
+            "A. e'tibor bermaslik",
+            "B. unutmoq",
+            "C. chetlab o'tmoq",
+            "D. muomala qilmoq"
+        ]
+    }
+];
