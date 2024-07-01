aff6fda6985b9b7d1fa6333262d9d3ef70c2d1fc
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 17:55:00 +0500

    Create savollar24.js

---
+++ savollar24.js
@@ -0,0 +1,222 @@
+let savollar24 = [
+    {
+        numb: 1,
+        question: "block",
+        answer: "C. bo'lak",
+        options: [
+            "A. tosh",
+            "B. yog'och",
+            "C. bo'lak",
+            "D. muz"
+        ]
+    },
+    {
+        numb: 2,
+        question: "to cheer",
+        answer: "B. qichqirmoq",
+        options: [
+            "A. yig'lamoq",
+            "B. qichqirmoq",
+            "C. shivirlash",
+            "D. baqirmoq"
+        ]
+    },
+    {
+        numb: 3,
+        question: "complex",
+        answer: "A. murakkab",
+        options: [
+            "A. murakkab",
+            "B. oddiy",
+            "C. aniq",
+            "D. to'g'ri"
+        ]
+    },
+    {
+        numb: 4,
+        question: "critic",
+        answer: "D. tanqidchi",
+        options: [
+            "A. san'atkor",
+            "B. muallif",
+            "C. aktyor",
+            "D. tanqidchi"
+        ]
+    },
+    {
+        numb: 5,
+        question: "event",
+        answer: "C. voqea",
+        options: [
+            "A. hikoya",
+            "B. holat",
+            "C. voqea",
+            "D. xabar"
+        ]
+    },
+    {
+        numb: 6,
+        question: "to exercise",
+        answer: "A. mashq qilmoq",
+        options: [
+            "A. mashq qilmoq",
+            "B. uxlash",
+            "C. ovqatlanmoq",
+            "D. o'qish"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to fit",
+        answer: "D. mos kelmoq",
+        options: [
+            "A. ochmoq",
+            "B. yopmoq",
+            "C. o'zgartirmoq",
+            "D. mos kelmoq"
+        ]
+    },
+    {
+        numb: 8,
+        question: "friendship",
+        answer: "B. do'stlik",
+        options: [
+            "A. munosabat",
+            "B. do'stlik",
+            "C. oila",
+            "D. sheriklik"
+        ]
+    },
+    {
+        numb: 9,
+        question: "guide",
+        answer: "C. yo'lboshchi",
+        options: [
+            "A. xarita",
+            "B. yo'l",
+            "C. yo'lboshchi",
+            "D. maqsad"
+        ]
+    },
+    {
+        numb: 10,
+        question: "lack",
+        answer: "A. yetishmovchilik",
+        options: [
+            "A. yetishmovchilik",
+            "B. ko'plab",
+            "C. ehtiyoj",
+            "D. istak"
+        ]
+    },
+    {
+        numb: 11,
+        question: "passage",
+        answer: "B. yo'lak",
+        options: [
+            "A. xona",
+            "B. yo'lak",
+            "C. kirish",
+            "D. koridor"
+        ]
+    },
+    {
+        numb: 12,
+        question: "to perform",
+        answer: "D. ijro etmoq",
+        options: [
+            "A. uxlash",
+            "B. o'rganish",
+            "C. o'tirmoq",
+            "D. ijro etmoq"
+        ]
+    },
+    {
+        numb: 13,
+        question: "pressure",
+        answer: "A. bosim",
+        options: [
+            "A. bosim",
+            "B. tinchlik",
+            "C. yordam",
+            "D. zavq"
+        ]
+    },
+    {
+        numb: 14,
+        question: "probable",
+        answer: "B. ehtimol",
+        options: [
+            "A. imkonsiz",
+            "B. ehtimol",
+            "C. kamdan-kam",
+            "D. noma'lum"
+        ]
+    },
+    {
+        numb: 15,
+        question: "public",
+        answer: "A. ommaviy",
+        options: [
+            "A. ommaviy",
+            "B. shaxsiy",
+            "C. maxfiy",
+            "D. eksklyuziv"
+        ]
+    },
+    {
+        numb: 16,
+        question: "to strike",
+        answer: "D. urmoq",
+        options: [
+            "A. yugurmoq",
+            "B. raqsga tushmoq",
+            "C. baqirmoq",
+            "D. urmoq"
+        ]
+    },
+    {
+        numb: 17,
+        question: "to support",
+        answer: "A. qo'llab-quvvatlamoq",
+        options: [
+            "A. qo'llab-quvvatlamoq",
+            "B. rad etmoq",
+            "C. e'tiborsiz qoldirmoq",
+            "D. unutmoq"
+        ]
+    },
+    {
+        numb: 18,
+        question: "task",
+        answer: "B. vazifa",
+        options: [
+            "A. o'yin",
+            "B. vazifa",
+            "C. savol",
+            "D. muammo"
+        ]
+    },
+    {
+        numb: 19,
+        question: "term",
+        answer: "A. atama",
+        options: [
+            "A. atama",
+            "B. gap",
+            "C. tushuntirish",
+            "D. ko'rsatma"
+        ]
+    },
+    {
+        numb: 20,
+        question: "to unite",
+        answer: "B. birlashmoq",
+        options: [
+            "A. ajratmoq",
+            "B. birlashmoq",
+            "C. jang qilmoq",
+            "D. muhokama qilmoq"
+        ]
+    }
+];
