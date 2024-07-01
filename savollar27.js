b09890e29e105162a4743dc69eebef8a313b2f3d
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 17:56:31 +0500

    Create savollar27.js

---
+++ savollar27.js
@@ -0,0 +1,222 @@
+let savollar27 = [
+    {
+        numb: 1,
+        question: "actually",
+        answer: "A. aslida",
+        options: [
+            "A. aslida",
+            "B. ehtimol",
+            "C. go'yo",
+            "D. balki"
+        ]
+    },
+    {
+        numb: 2,
+        question: "bite",
+        answer: "B. tishlamoq",
+        options: [
+            "A. tamaddi",
+            "B. tishlamoq",
+            "C. o'pmoq",
+            "D. yutmoq"
+        ]
+    },
+    {
+        numb: 3,
+        question: "coast",
+        answer: "C. qirg'oq",
+        options: [
+            "A. tog'",
+            "B. vodiy",
+            "C. qirg'oq",
+            "D. daryo"
+        ]
+    },
+    {
+        numb: 4,
+        question: "deal",
+        answer: "D. kelishuv",
+        options: [
+            "A. janjal",
+            "B. muhokama",
+            "C. muammo",
+            "D. kelishuv"
+        ]
+    },
+    {
+        numb: 5,
+        question: "desert",
+        answer: "A. cho'l",
+        options: [
+            "A. cho'l",
+            "B. o'rmon",
+            "C. orol",
+            "D. daryo"
+        ]
+    },
+    {
+        numb: 6,
+        question: "earthquake",
+        answer: "B. zilzila",
+        options: [
+            "A. suv toshqini",
+            "B. zilzila",
+            "C. bo'ron",
+            "D. olov"
+        ]
+    },
+    {
+        numb: 7,
+        question: "effective",
+        answer: "C. samarali",
+        options: [
+            "A. samarasiz",
+            "B. foydasiz",
+            "C. samarali",
+            "D. keraksiz"
+        ]
+    },
+    {
+        numb: 8,
+        question: "examine",
+        answer: "A. tekshirmoq",
+        options: [
+            "A. tekshirmoq",
+            "B. e'tiborsiz qoldirmoq",
+            "C. yo'q qilmoq",
+            "D. tashlamoq"
+        ]
+    },
+    {
+        numb: 9,
+        question: "false",
+        answer: "D. noto'g'ri",
+        options: [
+            "A. to'g'ri",
+            "B. haqiqat",
+            "C. chin",
+            "D. noto'g'ri"
+        ]
+    },
+    {
+        numb: 10,
+        question: "gift",
+        answer: "B. sovg'a",
+        options: [
+            "A. xarid",
+            "B. sovg'a",
+            "C. to'lov",
+            "D. sotuv"
+        ]
+    },
+    {
+        numb: 11,
+        question: "hunger",
+        answer: "C. ochlik",
+        options: [
+            "A. chanqoqlik",
+            "B. charchoq",
+            "C. ochlik",
+            "D. xursandlik"
+        ]
+    },
+    {
+        numb: 12,
+        question: "imagine",
+        answer: "A. tasavvur qilmoq",
+        options: [
+            "A. tasavvur qilmoq",
+            "B. unutmoq",
+            "C. e'tiborsiz qoldirmoq",
+            "D. mensimaslik"
+        ]
+    },
+    {
+        numb: 13,
+        question: "journey",
+        answer: "B. sayohat",
+        options: [
+            "A. ish",
+            "B. sayohat",
+            "C. o'yin",
+            "D. tanaffus"
+        ]
+    },
+    {
+        numb: 14,
+        question: "puzzle",
+        answer: "D. jumboq",
+        options: [
+            "A. yechim",
+            "B. topshiriq",
+            "C. hikoya",
+            "D. jumboq"
+        ]
+    },
+    {
+        numb: 15,
+        question: "quite",
+        answer: "A. ancha",
+        options: [
+            "A. ancha",
+            "B. ozroq",
+            "C. kamdan-kam",
+            "D. hech qachon"
+        ]
+    },
+    {
+        numb: 16,
+        question: "rather",
+        answer: "B. ko'proq",
+        options: [
+            "A. kamroq",
+            "B. ko'proq",
+            "C. ba'zida",
+            "D. har doim"
+        ]
+    },
+    {
+        numb: 17,
+        question: "specific",
+        answer: "C. aniq",
+        options: [
+            "A. umumiy",
+            "B. oddiy",
+            "C. aniq",
+            "D. tasodifiy"
+        ]
+    },
+    {
+        numb: 18,
+        question: "tour",
+        answer: "A. sayohat",
+        options: [
+            "A. sayohat",
+            "B. tushlik",
+            "C. ish",
+            "D. tanaffus"
+        ]
+    },
+    {
+        numb: 19,
+        question: "trip",
+        answer: "B. safar",
+        options: [
+            "A. ish",
+            "B. safar",
+            "C. tanaffus",
+            "D. mashg'ulot"
+        ]
+    },
+    {
+        numb: 20,
+        question: "value",
+        answer: "D. qiymat",
+        options: [
+            "A. xarajatlar",
+            "B. qarzlar",
+            "C. zarar",
+            "D. qiymat"
+        ]
+    }
+];
