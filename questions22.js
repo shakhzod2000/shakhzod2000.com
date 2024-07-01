6e8e89573daa97dc7add34df257fb427be175bc9
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 17:23:59 +0500

    Create questions22.js

---
+++ questions22.js
@@ -0,0 +1,222 @@
+let questions22 = [
+    {
+        numb: 1,
+        question: "actual",
+        answer: "A. tatsächlich",
+        options: [
+            "A. tatsächlich",
+            "B. gefälscht",
+            "C. imaginär",
+            "D. wahrscheinlich"
+        ]
+    },
+    {
+        numb: 2,
+        question: "to amaze",
+        answer: "B. erstaunen",
+        options: [
+            "A. langweilen",
+            "B. erstaunen",
+            "C. verwirren",
+            "D. enttäuschen"
+        ]
+    },
+    {
+        numb: 3,
+        question: "charge",
+        answer: "D. Gebühr",
+        options: [
+            "A. Rabatt",
+            "B. Geschenk",
+            "C. Schulden",
+            "D. Gebühr"
+        ]
+    },
+    {
+        numb: 4,
+        question: "to comfort",
+        answer: "C. trösten",
+        options: [
+            "A. ignorieren",
+            "B. kritisieren",
+            "C. trösten",
+            "D. erschrecken"
+        ]
+    },
+    {
+        numb: 5,
+        question: "to contact",
+        answer: "B. kontaktieren",
+        options: [
+            "A. meiden",
+            "B. kontaktieren",
+            "C. ignorieren",
+            "D. vergessen"
+        ]
+    },
+    {
+        numb: 6,
+        question: "customer",
+        answer: "A. Kunde",
+        options: [
+            "A. Kunde",
+            "B. Verkäufer",
+            "C. Hersteller",
+            "D. Lieferant"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to deliver",
+        answer: "D. liefern",
+        options: [
+            "A. behalten",
+            "B. empfangen",
+            "C. verpacken",
+            "D. liefern"
+        ]
+    },
+    {
+        numb: 8,
+        question: "to earn",
+        answer: "B. verdienen",
+        options: [
+            "A. ausgeben",
+            "B. verdienen",
+            "C. verschwenden",
+            "D. leihen"
+        ]
+    },
+    {
+        numb: 9,
+        question: "gate",
+        answer: "C. Tor",
+        options: [
+            "A. Fenster",
+            "B. Tür",
+            "C. Tor",
+            "D. Zaun"
+        ]
+    },
+    {
+        numb: 10,
+        question: "to include",
+        answer: "A. einschließen",
+        options: [
+            "A. einschließen",
+            "B. ausschließen",
+            "C. trennen",
+            "D. entfernen"
+        ]
+    },
+    {
+        numb: 11,
+        question: "to manage",
+        answer: "D. verwalten",
+        options: [
+            "A. ignorieren",
+            "B. zerstören",
+            "C. unterstützen",
+            "D. verwalten"
+        ]
+    },
+    {
+        numb: 12,
+        question: "mystery",
+        answer: "B. Rätsel",
+        options: [
+            "A. Klarheit",
+            "B. Rätsel",
+            "C. Gewissheit",
+            "D. Transparenz"
+        ]
+    },
+    {
+        numb: 13,
+        question: "to occur",
+        answer: "A. geschehen",
+        options: [
+            "A. geschehen",
+            "B. verhindern",
+            "C. vergessen",
+            "D. planen"
+        ]
+    },
+    {
+        numb: 14,
+        question: "opposite",
+        answer: "C. Gegenteil",
+        options: [
+            "A. Ähnlichkeit",
+            "B. Vergleich",
+            "C. Gegenteil",
+            "D. Parallele"
+        ]
+    },
+    {
+        numb: 15,
+        question: "plate",
+        answer: "B. Teller",
+        options: [
+            "A. Glas",
+            "B. Teller",
+            "C. Schüssel",
+            "D. Becher"
+        ]
+    },
+    {
+        numb: 16,
+        question: "to receive",
+        answer: "A. erhalten",
+        options: [
+            "A. erhalten",
+            "B. senden",
+            "C. verlieren",
+            "D. verweigern"
+        ]
+    },
+    {
+        numb: 17,
+        question: "reward",
+        answer: "C. Belohnung",
+        options: [
+            "A. Bestrafung",
+            "B. Verpflichtung",
+            "C. Belohnung",
+            "D. Herausforderung"
+        ]
+    },
+    {
+        numb: 18,
+        question: "to set",
+        answer: "B. setzen",
+        options: [
+            "A. werfen",
+            "B. setzen",
+            "C. nehmen",
+            "D. brechen"
+        ]
+    },
+    {
+        numb: 19,
+        question: "to steal",
+        answer: "A. stehlen",
+        options: [
+            "A. stehlen",
+            "B. kaufen",
+            "C. finden",
+            "D. verlieren"
+        ]
+    },
+    {
+        numb: 20,
+        question: "thief",
+        answer: "D. Dieb",
+        options: [
+            "A. Polizist",
+            "B. Verkäufer",
+            "C. Kunde",
+            "D. Dieb"
+        ]
+    }
+];
