10a6af7b724d997b15c6a04281d40879d5986ca0
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 17:23:18 +0500

    Create questions21.js

---
+++ questions21.js
@@ -0,0 +1,222 @@
+let questions21 = [
+    {
+        numb: 1,
+        question: "to appear",
+        answer: "A. erscheinen",
+        options: [
+            "A. erscheinen",
+            "B. verschwinden",
+            "C. verstecken",
+            "D. ignorieren"
+        ]
+    },
+    {
+        numb: 2,
+        question: "base",
+        answer: "B. Basis",
+        options: [
+            "A. Spitze",
+            "B. Basis",
+            "C. Mitte",
+            "D. Rand"
+        ]
+    },
+    {
+        numb: 3,
+        question: "brain",
+        answer: "C. Gehirn",
+        options: [
+            "A. Herz",
+            "B. Lunge",
+            "C. Gehirn",
+            "D. Magen"
+        ]
+    },
+    {
+        numb: 4,
+        question: "career",
+        answer: "D. Karriere",
+        options: [
+            "A. Hobby",
+            "B. Aufgabe",
+            "C. Arbeitstag",
+            "D. Karriere"
+        ]
+    },
+    {
+        numb: 5,
+        question: "clerk",
+        answer: "A. Angestellter",
+        options: [
+            "A. Angestellter",
+            "B. Kunde",
+            "C. Manager",
+            "D. Besitzer"
+        ]
+    },
+    {
+        numb: 6,
+        question: "effort",
+        answer: "B. Anstrengung",
+        options: [
+            "A. Leichtigkeit",
+            "B. Anstrengung",
+            "C. Müßiggang",
+            "D. Faulheit"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to enter",
+        answer: "C. eintreten",
+        options: [
+            "A. verlassen",
+            "B. meiden",
+            "C. eintreten",
+            "D. hinausgehen"
+        ]
+    },
+    {
+        numb: 8,
+        question: "excellent",
+        answer: "A. ausgezeichnet",
+        options: [
+            "A. ausgezeichnet",
+            "B. schlecht",
+            "C. mittelmäßig",
+            "D. ungenügend"
+        ]
+    },
+    {
+        numb: 9,
+        question: "hero",
+        answer: "D. Held",
+        options: [
+            "A. Feigling",
+            "B. Zuschauer",
+            "C. Bösewicht",
+            "D. Held"
+        ]
+    },
+    {
+        numb: 10,
+        question: "to hurry",
+        answer: "B. sich beeilen",
+        options: [
+            "A. trödeln",
+            "B. sich beeilen",
+            "C. verweilen",
+            "D. spazieren"
+        ]
+    },
+    {
+        numb: 11,
+        question: "to inform",
+        answer: "A. informieren",
+        options: [
+            "A. informieren",
+            "B. ignorieren",
+            "C. belügen",
+            "D. schweigen"
+        ]
+    },
+    {
+        numb: 12,
+        question: "later",
+        answer: "B. später",
+        options: [
+            "A. früher",
+            "B. später",
+            "C. sofort",
+            "D. nie"
+        ]
+    },
+    {
+        numb: 13,
+        question: "to leave",
+        answer: "A. verlassen",
+        options: [
+            "A. verlassen",
+            "B. bleiben",
+            "C. ankommen",
+            "D. eintreten"
+        ]
+    },
+    {
+        numb: 14,
+        question: "to locate",
+        answer: "D. finden",
+        options: [
+            "A. verlieren",
+            "B. verstecken",
+            "C. vergessen",
+            "D. finden"
+        ]
+    },
+    {
+        numb: 15,
+        question: "nurse",
+        answer: "A. Krankenschwester",
+        options: [
+            "A. Krankenschwester",
+            "B. Ärztin",
+            "C. Patient",
+            "D. Besucher"
+        ]
+    },
+    {
+        numb: 16,
+        question: "operation",
+        answer: "C. Operation",
+        options: [
+            "A. Untersuchung",
+            "B. Beratung",
+            "C. Operation",
+            "D. Therapie"
+        ]
+    },
+    {
+        numb: 17,
+        question: "pain",
+        answer: "B. Schmerz",
+        options: [
+            "A. Freude",
+            "B. Schmerz",
+            "C. Genuss",
+            "D. Glück"
+        ]
+    },
+    {
+        numb: 18,
+        question: "to refuse",
+        answer: "A. ablehnen",
+        options: [
+            "A. ablehnen",
+            "B. akzeptieren",
+            "C. fragen",
+            "D. bestätigen"
+        ]
+    },
+    {
+        numb: 19,
+        question: "though",
+        answer: "B. obwohl",
+        options: [
+            "A. weil",
+            "B. obwohl",
+            "C. falls",
+            "D. damit"
+        ]
+    },
+    {
+        numb: 20,
+        question: "various",
+        answer: "D. verschieden",
+        options: [
+            "A. identisch",
+            "B. gleich",
+            "C. einheitlich",
+            "D. verschieden"
+        ]
+    }
+];
