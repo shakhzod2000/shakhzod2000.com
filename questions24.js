dc5b9fa73526fa783ad2ae3fc8f7302405dcbdc8
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 17:25:17 +0500

    Create questions24.js

---
+++ questions24.js
@@ -0,0 +1,222 @@
+let questions24 = [
+    {
+        numb: 1,
+        question: "block",
+        answer: "C. Block",
+        options: [
+            "A. Stein",
+            "B. Holz",
+            "C. Block",
+            "D. Eis"
+        ]
+    },
+    {
+        numb: 2,
+        question: "to cheer",
+        answer: "B. jubeln",
+        options: [
+            "A. weinen",
+            "B. jubeln",
+            "C. flüstern",
+            "D. rufen"
+        ]
+    },
+    {
+        numb: 3,
+        question: "complex",
+        answer: "A. komplex",
+        options: [
+            "A. komplex",
+            "B. einfach",
+            "C. klar",
+            "D. direkt"
+        ]
+    },
+    {
+        numb: 4,
+        question: "critic",
+        answer: "D. Kritiker",
+        options: [
+            "A. Künstler",
+            "B. Autor",
+            "C. Schauspieler",
+            "D. Kritiker"
+        ]
+    },
+    {
+        numb: 5,
+        question: "event",
+        answer: "C. Ereignis",
+        options: [
+            "A. Geschichte",
+            "B. Zustand",
+            "C. Ereignis",
+            "D. Nachricht"
+        ]
+    },
+    {
+        numb: 6,
+        question: "to exercise",
+        answer: "A. trainieren",
+        options: [
+            "A. trainieren",
+            "B. schlafen",
+            "C. essen",
+            "D. lesen"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to fit",
+        answer: "D. passen",
+        options: [
+            "A. öffnen",
+            "B. schließen",
+            "C. verändern",
+            "D. passen"
+        ]
+    },
+    {
+        numb: 8,
+        question: "friendship",
+        answer: "B. Freundschaft",
+        options: [
+            "A. Beziehung",
+            "B. Freundschaft",
+            "C. Familie",
+            "D. Partnerschaft"
+        ]
+    },
+    {
+        numb: 9,
+        question: "guide",
+        answer: "C. Führer",
+        options: [
+            "A. Karte",
+            "B. Weg",
+            "C. Führer",
+            "D. Ziel"
+        ]
+    },
+    {
+        numb: 10,
+        question: "lack",
+        answer: "A. Mangel",
+        options: [
+            "A. Mangel",
+            "B. Überfluss",
+            "C. Bedarf",
+            "D. Wunsch"
+        ]
+    },
+    {
+        numb: 11,
+        question: "passage",
+        answer: "B. Durchgang",
+        options: [
+            "A. Raum",
+            "B. Durchgang",
+            "C. Eingang",
+            "D. Flur"
+        ]
+    },
+    {
+        numb: 12,
+        question: "to perform",
+        answer: "D. auftreten",
+        options: [
+            "A. schlafen",
+            "B. lernen",
+            "C. sitzen",
+            "D. auftreten"
+        ]
+    },
+    {
+        numb: 13,
+        question: "pressure",
+        answer: "A. Druck",
+        options: [
+            "A. Druck",
+            "B. Ruhe",
+            "C. Hilfe",
+            "D. Spaß"
+        ]
+    },
+    {
+        numb: 14,
+        question: "probable",
+        answer: "B. wahrscheinlich",
+        options: [
+            "A. unmöglich",
+            "B. wahrscheinlich",
+            "C. selten",
+            "D. ungewiss"
+        ]
+    },
+    {
+        numb: 15,
+        question: "public",
+        answer: "A. öffentlich",
+        options: [
+            "A. öffentlich",
+            "B. privat",
+            "C. geheim",
+            "D. exklusiv"
+        ]
+    },
+    {
+        numb: 16,
+        question: "to strike",
+        answer: "D. schlagen",
+        options: [
+            "A. laufen",
+            "B. tanzen",
+            "C. schreien",
+            "D. schlagen"
+        ]
+    },
+    {
+        numb: 17,
+        question: "to support",
+        answer: "A. unterstützen",
+        options: [
+            "A. unterstützen",
+            "B. ablehnen",
+            "C. ignorieren",
+            "D. vergessen"
+        ]
+    },
+    {
+        numb: 18,
+        question: "task",
+        answer: "B. Aufgabe",
+        options: [
+            "A. Spiel",
+            "B. Aufgabe",
+            "C. Frage",
+            "D. Problem"
+        ]
+    },
+    {
+        numb: 19,
+        question: "term",
+        answer: "A. Begriff",
+        options: [
+            "A. Begriff",
+            "B. Satz",
+            "C. Erklärung",
+            "D. Anweisung"
+        ]
+    },
+    {
+        numb: 20,
+        question: "to unite",
+        answer: "B. vereinen",
+        options: [
+            "A. trennen",
+            "B. vereinen",
+            "C. kämpfen",
+            "D. diskutieren"
+        ]
+    }
+];
