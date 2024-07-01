d83ac07ff8f6c9ad103970e847a7e07ac3c12cb1
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 17:35:30 +0500

    Create savollar16.js

---
+++ savollar16.js
@@ -0,0 +1,222 @@
+let savollar16 = [
+    {
+        numb: 1,
+        question: "academy",
+        answer: "A. akademiya",
+        options: [
+            "A. akademiya",
+            "B. maktab",
+            "C. universitet",
+            "D. kutubxona"
+        ]
+    },
+    {
+        numb: 2,
+        question: "ancient",
+        answer: "B. qadimiy",
+        options: [
+            "A. zamonaviy",
+            "B. qadimiy",
+            "C. yosh",
+            "D. yangi"
+        ]
+    },
+    {
+        numb: 3,
+        question: "board",
+        answer: "A. taxta",
+        options: [
+            "A. taxta",
+            "B. doska",
+            "C. devor",
+            "D. pol"
+        ]
+    },
+    {
+        numb: 4,
+        question: "century",
+        answer: "B. asr",
+        options: [
+            "A. o'n yil",
+            "B. asr",
+            "C. ming yillik",
+            "D. yil"
+        ]
+    },
+    {
+        numb: 5,
+        question: "clue",
+        answer: "C. ishora",
+        options: [
+            "A. yechim",
+            "B. savol",
+            "C. ishora",
+            "D. jumboq"
+        ]
+    },
+    {
+        numb: 6,
+        question: "concert",
+        answer: "A. konsert",
+        options: [
+            "A. konsert",
+            "B. kecha",
+            "C. yig'ilish",
+            "D. namoyish"
+        ]
+    },
+    {
+        numb: 7,
+        question: "county",
+        answer: "D. okrug",
+        options: [
+            "A. shahar",
+            "B. qishloq",
+            "C. mamlakat",
+            "D. okrug"
+        ]
+    },
+    {
+        numb: 8,
+        question: "dictionary",
+        answer: "B. lug'at",
+        options: [
+            "A. entsiklopediya",
+            "B. lug'at",
+            "C. darslik",
+            "D. roman"
+        ]
+    },
+    {
+        numb: 9,
+        question: "exist",
+        answer: "A. mavjud bo'lmoq",
+        options: [
+            "A. mavjud bo'lmoq",
+            "B. yo'qolmoq",
+            "C. paydo bo'lmoq",
+            "D. tugatmoq"
+        ]
+    },
+    {
+        numb: 10,
+        question: "flat",
+        answer: "C. tekis",
+        options: [
+            "A. egri",
+            "B. tik",
+            "C. tekis",
+            "D. to'lqinli"
+        ]
+    },
+    {
+        numb: 11,
+        question: "gentleman",
+        answer: "D. oliyjanob inson",
+        options: [
+            "A. ayol",
+            "B. do'st",
+            "C. yigit",
+            "D. oliyjanob inson"
+        ]
+    },
+    {
+        numb: 12,
+        question: "hidden",
+        answer: "B. yashirin",
+        options: [
+            "A. ko'rinadigan",
+            "B. yashirin",
+            "C. aniq",
+            "D. ma'lum"
+        ]
+    },
+    {
+        numb: 13,
+        question: "maybe",
+        answer: "A. balki",
+        options: [
+            "A. balki",
+            "B. aniq",
+            "C. hech qachon",
+            "D. doim"
+        ]
+    },
+    {
+        numb: 14,
+        question: "officer",
+        answer: "B. ofitser",
+        options: [
+            "A. askar",
+            "B. ofitser",
+            "C. general",
+            "D. fuqaro"
+        ]
+    },
+    {
+        numb: 15,
+        question: "original",
+        answer: "A. original",
+        options: [
+            "A. original",
+            "B. nusxa",
+            "C. soxta",
+            "D. dublikat"
+        ]
+    },
+    {
+        numb: 16,
+        question: "pound",
+        answer: "C. urmoq",
+        options: [
+            "A. silamoq",
+            "B. terilmoq",
+            "C. urmoq",
+            "D. otmoq"
+        ]
+    },
+    {
+        numb: 17,
+        question: "process",
+        answer: "A. jarayon",
+        options: [
+            "A. jarayon",
+            "B. natija",
+            "C. holat",
+            "D. qadam"
+        ]
+    },
+    {
+        numb: 18,
+        question: "publish",
+        answer: "B. nashr qilmoq",
+        options: [
+            "A. yozmoq",
+            "B. nashr qilmoq",
+            "C. o'qimoq",
+            "D. bosmoq"
+        ]
+    },
+    {
+        numb: 19,
+        question: "theater",
+        answer: "A. teatr",
+        options: [
+            "A. teatr",
+            "B. kino",
+            "C. opera",
+            "D. muzey"
+        ]
+    },
+    {
+        numb: 20,
+        question: "wealth",
+        answer: "B. boylik",
+        options: [
+            "A. qashshoqlik",
+            "B. boylik",
+            "C. mol-mulk",
+            "D. pul"
+        ]
+    }
+];
