39bf5762dc3a07d495dd87fe414af8f837925c4e
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 17:24:31 +0500

    Create questions23.js

---
+++ questions23.js
@@ -0,0 +1,222 @@
+let questions23 = [
+    {
+        numb: 1,
+        question: "to advance",
+        answer: "A. voranschreiten",
+        options: [
+            "A. voranschreiten",
+            "B. zurückgehen",
+            "C. stoppen",
+            "D. umkehren"
+        ]
+    },
+    {
+        numb: 2,
+        question: "athlete",
+        answer: "B. Sportler",
+        options: [
+            "A. Lehrer",
+            "B. Sportler",
+            "C. Musiker",
+            "D. Künstler"
+        ]
+    },
+    {
+        numb: 3,
+        question: "average",
+        answer: "C. durchschnittlich",
+        options: [
+            "A. arm",
+            "B. reich",
+            "C. durchschnittlich",
+            "D. außergewöhnlich"
+        ]
+    },
+    {
+        numb: 4,
+        question: "behavior",
+        answer: "A. Verhalten",
+        options: [
+            "A. Verhalten",
+            "B. Aussehen",
+            "C. Wissen",
+            "D. Gesundheit"
+        ]
+    },
+    {
+        numb: 5,
+        question: "behind",
+        answer: "D. hinter",
+        options: [
+            "A. vor",
+            "B. neben",
+            "C. über",
+            "D. hinter"
+        ]
+    },
+    {
+        numb: 6,
+        question: "course",
+        answer: "B. Kurs",
+        options: [
+            "A. Prüfung",
+            "B. Kurs",
+            "C. Pause",
+            "D. Jahr"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to lower",
+        answer: "A. senken",
+        options: [
+            "A. senken",
+            "B. erhöhen",
+            "C. behalten",
+            "D. verlieren"
+        ]
+    },
+    {
+        numb: 8,
+        question: "to match",
+        answer: "B. passen",
+        options: [
+            "A. mischen",
+            "B. passen",
+            "C. zerstören",
+            "D. trennen"
+        ]
+    },
+    {
+        numb: 9,
+        question: "member",
+        answer: "C. Mitglied",
+        options: [
+            "A. Chef",
+            "B. Zuschauer",
+            "C. Mitglied",
+            "D. Gegner"
+        ]
+    },
+    {
+        numb: 10,
+        question: "mental",
+        answer: "A. geistig",
+        options: [
+            "A. geistig",
+            "B. körperlich",
+            "C. emotional",
+            "D. physisch"
+        ]
+    },
+    {
+        numb: 11,
+        question: "passenger",
+        answer: "B. Passagier",
+        options: [
+            "A. Fahrer",
+            "B. Passagier",
+            "C. Polizist",
+            "D. Pilot"
+        ]
+    },
+    {
+        numb: 12,
+        question: "personality",
+        answer: "D. Persönlichkeit",
+        options: [
+            "A. Name",
+            "B. Alter",
+            "C. Beruf",
+            "D. Persönlichkeit"
+        ]
+    },
+    {
+        numb: 13,
+        question: "poem",
+        answer: "A. Gedicht",
+        options: [
+            "A. Gedicht",
+            "B. Roman",
+            "C. Artikel",
+            "D. Notiz"
+        ]
+    },
+    {
+        numb: 14,
+        question: "pole",
+        answer: "B. Stange",
+        options: [
+            "A. Seil",
+            "B. Stange",
+            "C. Brett",
+            "D. Kiste"
+        ]
+    },
+    {
+        numb: 15,
+        question: "to remove",
+        answer: "A. entfernen",
+        options: [
+            "A. entfernen",
+            "B. hinzufügen",
+            "C. behalten",
+            "D. ändern"
+        ]
+    },
+    {
+        numb: 16,
+        question: "safety",
+        answer: "C. Sicherheit",
+        options: [
+            "A. Gefahr",
+            "B. Risiko",
+            "C. Sicherheit",
+            "D. Unfälle"
+        ]
+    },
+    {
+        numb: 17,
+        question: "to shoot",
+        answer: "A. schießen",
+        options: [
+            "A. schießen",
+            "B. werfen",
+            "C. fangen",
+            "D. schlagen"
+        ]
+    },
+    {
+        numb: 18,
+        question: "to sound",
+        answer: "B. klingen",
+        options: [
+            "A. riechen",
+            "B. klingen",
+            "C. schmecken",
+            "D. sehen"
+        ]
+    },
+    {
+        numb: 19,
+        question: "to swim",
+        answer: "A. schwimmen",
+        options: [
+            "A. schwimmen",
+            "B. rennen",
+            "C. fliegen",
+            "D. springen"
+        ]
+    },
+    {
+        numb: 20,
+        question: "web",
+        answer: "C. Netz",
+        options: [
+            "A. Straße",
+            "B. Tür",
+            "C. Netz",
+            "D. Fenster"
+        ]
+    }
+];
