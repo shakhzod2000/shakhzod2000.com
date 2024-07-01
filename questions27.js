a39a645a23a36849748cc421e0f1339a9cd19b12
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 17:30:26 +0500

    Create questions27.js

---
+++ questions27.js
@@ -0,0 +1,222 @@
+let questions27 = [
+    {
+        numb: 1,
+        question: "actually",
+        answer: "A. eigentlich",
+        options: [
+            "A. eigentlich",
+            "B. möglicherweise",
+            "C. scheinbar",
+            "D. wahrscheinlich"
+        ]
+    },
+    {
+        numb: 2,
+        question: "bite",
+        answer: "B. Biss",
+        options: [
+            "A. Leckerei",
+            "B. Biss",
+            "C. Kuss",
+            "D. Schluck"
+        ]
+    },
+    {
+        numb: 3,
+        question: "coast",
+        answer: "C. Küste",
+        options: [
+            "A. Berg",
+            "B. Tal",
+            "C. Küste",
+            "D. Fluss"
+        ]
+    },
+    {
+        numb: 4,
+        question: "deal",
+        answer: "D. Abmachung",
+        options: [
+            "A. Streit",
+            "B. Diskussion",
+            "C. Problem",
+            "D. Abmachung"
+        ]
+    },
+    {
+        numb: 5,
+        question: "desert",
+        answer: "A. Wüste",
+        options: [
+            "A. Wüste",
+            "B. Wald",
+            "C. Insel",
+            "D. Fluss"
+        ]
+    },
+    {
+        numb: 6,
+        question: "earthquake",
+        answer: "B. Erdbeben",
+        options: [
+            "A. Überschwemmung",
+            "B. Erdbeben",
+            "C. Sturm",
+            "D. Feuer"
+        ]
+    },
+    {
+        numb: 7,
+        question: "effective",
+        answer: "C. effektiv",
+        options: [
+            "A. ineffektiv",
+            "B. nutzlos",
+            "C. effektiv",
+            "D. unnötig"
+        ]
+    },
+    {
+        numb: 8,
+        question: "examine",
+        answer: "A. untersuchen",
+        options: [
+            "A. untersuchen",
+            "B. ignorieren",
+            "C. zerstören",
+            "D. wegwerfen"
+        ]
+    },
+    {
+        numb: 9,
+        question: "false",
+        answer: "D. falsch",
+        options: [
+            "A. richtig",
+            "B. wahr",
+            "C. echt",
+            "D. falsch"
+        ]
+    },
+    {
+        numb: 10,
+        question: "gift",
+        answer: "B. Geschenk",
+        options: [
+            "A. Einkauf",
+            "B. Geschenk",
+            "C. Bezahlung",
+            "D. Verkauf"
+        ]
+    },
+    {
+        numb: 11,
+        question: "hunger",
+        answer: "C. Hunger",
+        options: [
+            "A. Durst",
+            "B. Müdigkeit",
+            "C. Hunger",
+            "D. Freude"
+        ]
+    },
+    {
+        numb: 12,
+        question: "imagine",
+        answer: "A. vorstellen",
+        options: [
+            "A. vorstellen",
+            "B. vergessen",
+            "C. ignorieren",
+            "D. vernachlässigen"
+        ]
+    },
+    {
+        numb: 13,
+        question: "journey",
+        answer: "B. Reise",
+        options: [
+            "A. Arbeit",
+            "B. Reise",
+            "C. Spiel",
+            "D. Pause"
+        ]
+    },
+    {
+        numb: 14,
+        question: "puzzle",
+        answer: "D. Rätsel",
+        options: [
+            "A. Lösung",
+            "B. Aufgabe",
+            "C. Geschichte",
+            "D. Rätsel"
+        ]
+    },
+    {
+        numb: 15,
+        question: "quite",
+        answer: "A. ziemlich",
+        options: [
+            "A. ziemlich",
+            "B. wenig",
+            "C. selten",
+            "D. niemals"
+        ]
+    },
+    {
+        numb: 16,
+        question: "rather",
+        answer: "B. eher",
+        options: [
+            "A. weniger",
+            "B. eher",
+            "C. manchmal",
+            "D. immer"
+        ]
+    },
+    {
+        numb: 17,
+        question: "specific",
+        answer: "C. spezifisch",
+        options: [
+            "A. allgemein",
+            "B. normal",
+            "C. spezifisch",
+            "D. zufällig"
+        ]
+    },
+    {
+        numb: 18,
+        question: "tour",
+        answer: "A. Rundfahrt",
+        options: [
+            "A. Rundfahrt",
+            "B. Mittagessen",
+            "C. Arbeit",
+            "D. Pause"
+        ]
+    },
+    {
+        numb: 19,
+        question: "trip",
+        answer: "B. Ausflug",
+        options: [
+            "A. Arbeit",
+            "B. Ausflug",
+            "C. Pause",
+            "D. Training"
+        ]
+    },
+    {
+        numb: 20,
+        question: "value",
+        answer: "D. Wert",
+        options: [
+            "A. Kosten",
+            "B. Schulden",
+            "C. Verlust",
+            "D. Wert"
+        ]
+    }
+];
