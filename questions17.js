4d1acc55d81a48e8f2294bcfa7378fc12f2b5554
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 17:17:05 +0500

    Create questions17.js

---
+++ questions17.js
@@ -0,0 +1,222 @@
+let questions17 = [
+    {
+        numb: 1,
+        question: "to appreciate",
+        answer: "A. schätzen",
+        options: [
+            "A. schätzen",
+            "B. verachten",
+            "C. ignorieren",
+            "D. abwerten"
+        ]
+    },
+    {
+        numb: 2,
+        question: "available",
+        answer: "B. verfügbar",
+        options: [
+            "A. besetzt",
+            "B. verfügbar",
+            "C. begrenzt",
+            "D. geschlossen"
+        ]
+    },
+    {
+        numb: 3,
+        question: "to beat",
+        answer: "A. schlagen",
+        options: [
+            "A. schlagen",
+            "B. verlieren",
+            "C. vermeiden",
+            "D. erleiden"
+        ]
+    },
+    {
+        numb: 4,
+        question: "bright",
+        answer: "C. hell",
+        options: [
+            "A. dunkel",
+            "B. trüb",
+            "C. hell",
+            "D. matt"
+        ]
+    },
+    {
+        numb: 5,
+        question: "to celebrate",
+        answer: "D. feiern",
+        options: [
+            "A. trauern",
+            "B. ignorieren",
+            "C. vergessen",
+            "D. feiern"
+        ]
+    },
+    {
+        numb: 6,
+        question: "to determine",
+        answer: "B. bestimmen",
+        options: [
+            "A. zweifeln",
+            "B. bestimmen",
+            "C. zögern",
+            "D. ändern"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to disappear",
+        answer: "A. verschwinden",
+        options: [
+            "A. verschwinden",
+            "B. erscheinen",
+            "C. bleiben",
+            "D. existieren"
+        ]
+    },
+    {
+        numb: 8,
+        question: "else",
+        answer: "C. sonst",
+        options: [
+            "A. vorher",
+            "B. daher",
+            "C. sonst",
+            "D. fast"
+        ]
+    },
+    {
+        numb: 9,
+        question: "fair",
+        answer: "A. fair",
+        options: [
+            "A. fair",
+            "B. unfair",
+            "C. falsch",
+            "D. betrügerisch"
+        ]
+    },
+    {
+        numb: 10,
+        question: "to flow",
+        answer: "D. fließen",
+        options: [
+            "A. stagnieren",
+            "B. stocken",
+            "C. blockieren",
+            "D. fließen"
+        ]
+    },
+    {
+        numb: 11,
+        question: "forward",
+        answer: "B. vorwärts",
+        options: [
+            "A. rückwärts",
+            "B. vorwärts",
+            "C. stillstehen",
+            "D. seitwärts"
+        ]
+    },
+    {
+        numb: 12,
+        question: "hill",
+        answer: "C. Hügel",
+        options: [
+            "A. Tal",
+            "B. Ebene",
+            "C. Hügel",
+            "D. Berg"
+        ]
+    },
+    {
+        numb: 13,
+        question: "level",
+        answer: "A. Niveau",
+        options: [
+            "A. Niveau",
+            "B. Abweichung",
+            "C. Neigung",
+            "D. Schieflage"
+        ]
+    },
+    {
+        numb: 14,
+        question: "lone",
+        answer: "D. allein",
+        options: [
+            "A. gemeinsam",
+            "B. gesellig",
+            "C. begleitet",
+            "D. allein"
+        ]
+    },
+    {
+        numb: 15,
+        question: "puddle",
+        answer: "B. Pfütze",
+        options: [
+            "A. Fluss",
+            "B. Pfütze",
+            "C. See",
+            "D. Ozean"
+        ]
+    },
+    {
+        numb: 16,
+        question: "response",
+        answer: "C. Antwort",
+        options: [
+            "A. Frage",
+            "B. Behauptung",
+            "C. Antwort",
+            "D. Widerspruch"
+        ]
+    },
+    {
+        numb: 17,
+        question: "season",
+        answer: "A. Jahreszeit",
+        options: [
+            "A. Jahreszeit",
+            "B. Monat",
+            "C. Woche",
+            "D. Dekade"
+        ]
+    },
+    {
+        numb: 18,
+        question: "solution",
+        answer: "B. Lösung",
+        options: [
+            "A. Problem",
+            "B. Lösung",
+            "C. Frage",
+            "D. Aufgabe"
+        ]
+    },
+    {
+        numb: 19,
+        question: "to waste",
+        answer: "C. verschwenden",
+        options: [
+            "A. sparen",
+            "B. nutzen",
+            "C. verschwenden",
+            "D. bewahren"
+        ]
+    },
+    {
+        numb: 20,
+        question: "whether",
+        answer: "A. ob",
+        options: [
+            "A. ob",
+            "B. und",
+            "C. oder",
+            "D. weil"
+        ]
+    }
+];
