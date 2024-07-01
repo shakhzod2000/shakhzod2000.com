2e5f697cbc6b219be18367fbed54ceafeca364dd
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 17:54:07 +0500

    Create savollar23.js

---
+++ savollar23.js
@@ -0,0 +1,222 @@
+let savollar23 = [
+    {
+        numb: 1,
+        question: "to advance",
+        answer: "A. oldinga yurmoq",
+        options: [
+            "A. oldinga yurmoq",
+            "B. orqaga qaytmoq",
+            "C. to'xtamoq",
+            "D. qaytmoq"
+        ]
+    },
+    {
+        numb: 2,
+        question: "athlete",
+        answer: "B. sportchi",
+        options: [
+            "A. o'qituvchi",
+            "B. sportchi",
+            "C. musiqachi",
+            "D. san'atkor"
+        ]
+    },
+    {
+        numb: 3,
+        question: "average",
+        answer: "C. o'rtacha",
+        options: [
+            "A. kambag'al",
+            "B. boy",
+            "C. o'rtacha",
+            "D. g'ayrioddiy"
+        ]
+    },
+    {
+        numb: 4,
+        question: "behavior",
+        answer: "A. xulq-atvor",
+        options: [
+            "A. xulq-atvor",
+            "B. ko'rinish",
+            "C. bilim",
+            "D. sog'liq"
+        ]
+    },
+    {
+        numb: 5,
+        question: "behind",
+        answer: "D. orqasida",
+        options: [
+            "A. oldida",
+            "B. yonida",
+            "C. ustida",
+            "D. orqasida"
+        ]
+    },
+    {
+        numb: 6,
+        question: "course",
+        answer: "B. dars",
+        options: [
+            "A. imtihon",
+            "B. dars",
+            "C. tanaffus",
+            "D. yil"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to lower",
+        answer: "A. pasay(tir)moq",
+        options: [
+            "A. pasay(tir)moq",
+            "B. ko'tarmoq",
+            "C. saqlamoq",
+            "D. yo'qotmoq"
+        ]
+    },
+    {
+        numb: 8,
+        question: "to match",
+        answer: "B. mos kelmoq",
+        options: [
+            "A. aralashtirmoq",
+            "B. mos kelmoq",
+            "C. yo'q qilmoq",
+            "D. ajratmoq"
+        ]
+    },
+    {
+        numb: 9,
+        question: "member",
+        answer: "C. a'zo",
+        options: [
+            "A. rahbar",
+            "B. tomoshabin",
+            "C. a'zo",
+            "D. raqib"
+        ]
+    },
+    {
+        numb: 10,
+        question: "mental",
+        answer: "A. aqliy",
+        options: [
+            "A. aqliy",
+            "B. jismoniy",
+            "C. hissiy",
+            "D. fizikalik"
+        ]
+    },
+    {
+        numb: 11,
+        question: "passenger",
+        answer: "B. yo'lovchi",
+        options: [
+            "A. haydovchi",
+            "B. yo'lovchi",
+            "C. politsiyachi",
+            "D. uchuvchi"
+        ]
+    },
+    {
+        numb: 12,
+        question: "personality",
+        answer: "D. shaxsiyat",
+        options: [
+            "A. ism",
+            "B. yosh",
+            "C. kasb",
+            "D. shaxsiyat"
+        ]
+    },
+    {
+        numb: 13,
+        question: "poem",
+        answer: "A. she'r",
+        options: [
+            "A. she'r",
+            "B. roman",
+            "C. maqola",
+            "D. eslatma"
+        ]
+    },
+    {
+        numb: 14,
+        question: "pole",
+        answer: "B. ustun",
+        options: [
+            "A. arqon",
+            "B. ustun",
+            "C. taxta",
+            "D. quti"
+        ]
+    },
+    {
+        numb: 15,
+        question: "to remove",
+        answer: "A. olib tashlamoq",
+        options: [
+            "A. olib tashlamoq",
+            "B. qo'shmoq",
+            "C. saqlamoq",
+            "D. o'zgartirmoq"
+        ]
+    },
+    {
+        numb: 16,
+        question: "safety",
+        answer: "C. xavfsizlik",
+        options: [
+            "A. xavf",
+            "B. risk",
+            "C. xavfsizlik",
+            "D. baxtsiz hodisalar"
+        ]
+    },
+    {
+        numb: 17,
+        question: "to shoot",
+        answer: "A. otmoq",
+        options: [
+            "A. otmoq",
+            "B. uloqtirmoq",
+            "C. ushlamoq",
+            "D. urmoq"
+        ]
+    },
+    {
+        numb: 18,
+        question: "to sound",
+        answer: "B. ovoz chiqarmoq",
+        options: [
+            "A. hidlamoq",
+            "B. ovoz chiqarmoq",
+            "C. tatib ko'rmoq",
+            "D. ko'rmoq"
+        ]
+    },
+    {
+        numb: 19,
+        question: "to swim",
+        answer: "A. suzmoq",
+        options: [
+            "A. suzmoq",
+            "B. yugurmoq",
+            "C. uchmoq",
+            "D. sakramoq"
+        ]
+    },
+    {
+        numb: 20,
+        question: "web",
+        answer: "C. o'rgimchak to'ri",
+        options: [
+            "A. ko'cha",
+            "B. eshik",
+            "C. o'rgimchak to'ri",
+            "D. deraza"
+        ]
+    }
+];
