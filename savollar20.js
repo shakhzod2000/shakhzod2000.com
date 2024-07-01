49557dad102466f46b1f85f5ec37b08cc7042c6d
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 17:50:51 +0500

    Create savollar20.js

---
+++ savollar20.js
@@ -0,0 +1,221 @@
+let savollar20 = [
+    {
+        numb: 1,
+        question: "to achieve",
+        answer: "A. erishmoq",
+        options: [
+            "A. erishmoq",
+            "B. yo'qotmoq",
+            "C. voz kechmoq",
+            "D. unutmoq"        ]
+    },
+    {
+        numb: 2,
+        question: "to advise",
+        answer: "B. maslahat bermoq",
+        options: [
+            "A. tinglamoq",
+            "B. maslahat bermoq",
+            "C. e'tiborsiz qoldirmoq",
+            "D. hukm qilmoq"
+        ]
+    },
+    {
+        numb: 3,
+        question: "already",
+        answer: "C. allaqachon",
+        options: [
+            "A. keyinroq",
+            "B. hech qachon",
+            "C. allaqachon",
+            "D. ba'zida"
+        ]
+    },
+    {
+        numb: 4,
+        question: "basic",
+        answer: "A. oddiy",
+        options: [
+            "A. oddiy",
+            "B. murakkab",
+            "C. ilg'or",
+            "D. ixtisoslashgan"
+        ]
+    },
+    {
+        numb: 5,
+        question: "bit",
+        answer: "B. ozgina",
+        options: [
+            "A. ko'p",
+            "B. ozgina",
+            "C. hammasi",
+            "D. hech narsa"
+        ]
+    },
+    {
+        numb: 6,
+        question: "to consider",
+        answer: "D. o'ylab ko'rmoq",
+        options: [
+            "A. rad etmoq",
+            "B. unutmoq",
+            "C. e'tiborsiz qoldirmoq",
+            "D. o'ylab ko'rmoq"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to destroy",
+        answer: "A. vayron qilmoq",
+        options: [
+            "A. vayron qilmoq",
+            "B. tuzatmoq",
+            "C. qurmoq",
+            "D. saqlamoq"
+        ]
+    },
+    {
+        numb: 8,
+        question: "to entertain",
+        answer: "C. xursand qilmoq",
+        options: [
+            "A. zeriktirmoq",
+            "B. bezovta qilmoq",
+            "C. xursand qilmoq",
+            "D. e'tiborsiz qoldirmoq"
+        ]
+    },
+    {
+        numb: 9,
+        question: "extra",
+        answer: "B. qo'shimcha",
+        options: [
+            "A. zarur",
+            "B. qo'shimcha",
+            "C. minimal",
+            "D. yetarli emas"
+        ]
+    },
+    {
+        numb: 10,
+        question: "goal",
+        answer: "A. maqsad",
+        options: [
+            "A. maqsad",
+            "B. boshlanish",
+            "C. muammo",
+            "D. o'yin"
+        ]
+    },
+    {
+        numb: 11,
+        question: "to lie",
+        answer: "C. yolg'on gapirmoq",
+        options: [
+            "A. yozmoq",
+            "B. o'qimoq",
+            "C. yolg'on gapirmoq",
+            "D. gaplashmoq"
+        ]
+    },
+    {
+        numb: 12,
+        question: "meat",
+        answer: "D. go'sht",
+        options: [
+            "A. sabzavot",
+            "B. meva",
+            "C. don",
+            "D. go'sht"
+        ]
+    },
+    {
+        numb: 13,
+        question: "opinion",
+        answer: "A. fikr",
+        options: [
+            "A. fikr",
+            "B. haqiqat",
+            "C. taxmin",
+            "D. hikoya"
+        ]
+    },
+    {
+        numb: 14,
+        question: "real",
+        answer: "B. haqiqiy",
+        options: [
+            "A. soxta",
+            "B. haqiqiy",
+            "C. noto'g'ri",
+            "D. xayoliy"
+        ]
+    },
+    {
+        numb: 15,
+        question: "to reflect",
+        answer: "D. aks ettirmoq",
+        options: [
+            "A. singdirmoq",
+            "B. yoritmoq",
+            "C. yo'qolmoq",
+            "D. aks ettirmoq"
+        ]
+    },
+    {
+        numb: 16,
+        question: "to regard",
+        answer: "B. deb hisoblamoq",
+        options: [
+            "A. e'tiborsiz qoldirmoq",
+            "B. deb hisoblamoq",
+            "C. mensimaslik",
+            "D. chetlashtirmoq"
+        ]
+    },
+    {
+        numb: 17,
+        question: "to serve",
+        answer: "A. keltirmoq",
+        options: [
+            "A. keltirmoq",
+            "B. olmoq",
+            "C. buyurtma bermoq",
+            "D. pishirmoq"
+        ]
+    },
+    {
+        numb: 18,
+        question: "vegetable",
+        answer: "D. sabzavot",
+        options: [
+            "A. go'sht",
+            "B. meva",
+            "C. don",
+            "D. sabzavot"
+        ]
+    },
+    {
+        numb: 19,
+        question: "war",
+        answer: "B. urush",
+        options: [
+            "A. tinchlik",
+            "B. urush",
+            "C. munozara",
+            "D. kelishuv"
+        ]
+    },
+    {
+        numb: 20,
+        question: "worth",
+        answer: "A. qiymatga ega",
+        options: [
+            "A. qiymatga ega",
+            "B. arzon",
+            "C. keraksiz",
+            "D. qimmat"
+        ]
+    }
+];
