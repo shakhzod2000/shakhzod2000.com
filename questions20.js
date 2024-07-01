bf4d51d0fc14e489fb9fd2abc02f89ecdcfa7625
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 17:20:14 +0500

    Create questions20.js

---
+++ questions20.js
@@ -0,0 +1,222 @@
+let questions20 = [
+    {
+        numb: 1,
+        question: "to achieve",
+        answer: "A. erreichen",
+        options: [
+            "A. erreichen",
+            "B. verlieren",
+            "C. aufgeben",
+            "D. vergessen"
+        ]
+    },
+    {
+        numb: 2,
+        question: "to advise",
+        answer: "B. beraten",
+        options: [
+            "A. zuhören",
+            "B. beraten",
+            "C. ignorieren",
+            "D. verurteilen"
+        ]
+    },
+    {
+        numb: 3,
+        question: "already",
+        answer: "C. bereits",
+        options: [
+            "A. später",
+            "B. niemals",
+            "C. bereits",
+            "D. manchmal"
+        ]
+    },
+    {
+        numb: 4,
+        question: "basic",
+        answer: "A. grundlegend",
+        options: [
+            "A. grundlegend",
+            "B. kompliziert",
+            "C. fortgeschritten",
+            "D. spezialisiert"
+        ]
+    },
+    {
+        numb: 5,
+        question: "bit",
+        answer: "B. ein bisschen",
+        options: [
+            "A. viel",
+            "B. ein bisschen",
+            "C. alles",
+            "D. nichts"
+        ]
+    },
+    {
+        numb: 6,
+        question: "to consider",
+        answer: "D. in Betracht ziehen",
+        options: [
+            "A. ablehnen",
+            "B. vergessen",
+            "C. ignorieren",
+            "D. in Betracht ziehen"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to destroy",
+        answer: "A. zerstören",
+        options: [
+            "A. zerstören",
+            "B. reparieren",
+            "C. aufbauen",
+            "D. bewahren"
+        ]
+    },
+    {
+        numb: 8,
+        question: "to entertain",
+        answer: "C. unterhalten",
+        options: [
+            "A. langweilen",
+            "B. stören",
+            "C. unterhalten",
+            "D. ignorieren"
+        ]
+    },
+    {
+        numb: 9,
+        question: "extra",
+        answer: "B. zusätzlich",
+        options: [
+            "A. notwendig",
+            "B. zusätzlich",
+            "C. minimal",
+            "D. unzureichend"
+        ]
+    },
+    {
+        numb: 10,
+        question: "goal",
+        answer: "A. Ziel",
+        options: [
+            "A. Ziel",
+            "B. Anfang",
+            "C. Problem",
+            "D. Spiel"
+        ]
+    },
+    {
+        numb: 11,
+        question: "to lie",
+        answer: "C. lügen",
+        options: [
+            "A. schreiben",
+            "B. lesen",
+            "C. lügen",
+            "D. sprechen"
+        ]
+    },
+    {
+        numb: 12,
+        question: "meat",
+        answer: "D. Fleisch",
+        options: [
+            "A. Gemüse",
+            "B. Obst",
+            "C. Getreide",
+            "D. Fleisch"
+        ]
+    },
+    {
+        numb: 13,
+        question: "opinion",
+        answer: "A. Meinung",
+        options: [
+            "A. Meinung",
+            "B. Tatsache",
+            "C. Vermutung",
+            "D. Geschichte"
+        ]
+    },
+    {
+        numb: 14,
+        question: "real",
+        answer: "B. echt",
+        options: [
+            "A. gefälscht",
+            "B. echt",
+            "C. falsch",
+            "D. imaginär"
+        ]
+    },
+    {
+        numb: 15,
+        question: "to reflect",
+        answer: "D. reflektieren",
+        options: [
+            "A. absorbieren",
+            "B. leuchten",
+            "C. verschwinden",
+            "D. reflektieren"
+        ]
+    },
+    {
+        numb: 16,
+        question: "to regard",
+        answer: "B. betrachten",
+        options: [
+            "A. ignorieren",
+            "B. betrachten",
+            "C. verachten",
+            "D. meiden"
+        ]
+    },
+    {
+        numb: 17,
+        question: "to serve",
+        answer: "A. servieren",
+        options: [
+            "A. servieren",
+            "B. nehmen",
+            "C. bestellen",
+            "D. kochen"
+        ]
+    },
+    {
+        numb: 18,
+        question: "vegetable",
+        answer: "D. Gemüse",
+        options: [
+            "A. Fleisch",
+            "B. Obst",
+            "C. Getreide",
+            "D. Gemüse"
+        ]
+    },
+    {
+        numb: 19,
+        question: "war",
+        answer: "B. Krieg",
+        options: [
+            "A. Frieden",
+            "B. Krieg",
+            "C. Diskussion",
+            "D. Abkommen"
+        ]
+    },
+    {
+        numb: 20,
+        question: "worth",
+        answer: "A. wert",
+        options: [
+            "A. wert",
+            "B. billig",
+            "C. unnötig",
+            "D. teuer"
+        ]
+    }
+];
