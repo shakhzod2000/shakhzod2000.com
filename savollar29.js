49b2beb4e5f5e5d130b08b9fa755e93ed4bd621a
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 23:36:10 +0500

    Create savollar29.js

---
+++ savollar29.js
@@ -0,0 +1,222 @@
+let savollar29 = [
+    {
+        numb: 1,
+        question: "to advertise",
+        answer: "A. reklama qilmoq",
+        options: [
+            "A. reklama qilmoq",
+            "B. sotib olmoq",
+            "C. rejalashtirmoq",
+            "D. qurmoq"
+        ]
+    },
+    {
+        numb: 2,
+        question: "to assign",
+        answer: "B. topshirmoq",
+        options: [
+            "A. rad etmoq",
+            "B. topshirmoq",
+            "C. tugatmoq",
+            "D. unutmoq"
+        ]
+    },
+    {
+        numb: 3,
+        question: "audience",
+        answer: "C. auditoriya",
+        options: [
+            "A. aktyor",
+            "B. musobaqa",
+            "C. auditoriya",
+            "D. murabbiy"
+        ]
+    },
+    {
+        numb: 4,
+        question: "breakfast",
+        answer: "A. nonushta",
+        options: [
+            "A. nonushta",
+            "B. tushlik",
+            "C. kechki ovqat",
+            "D. gazak"
+        ]
+    },
+    {
+        numb: 5,
+        question: "competition",
+        answer: "D. musobaqa",
+        options: [
+            "A. mashg'ulot",
+            "B. tomoshabin",
+            "C. qiyinchilik",
+            "D. musobaqa"
+        ]
+    },
+    {
+        numb: 6,
+        question: "cool",
+        answer: "B. salqin",
+        options: [
+            "A. issiq",
+            "B. salqin",
+            "C. iliq",
+            "D. quruq"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to gain",
+        answer: "A. olmoq",
+        options: [
+            "A. olmoq",
+            "B. yo'qotmoq",
+            "C. saqlab qolmoq",
+            "D. e'tiborsiz qoldirmoq"
+        ]
+    },
+    {
+        numb: 8,
+        question: "importance",
+        answer: "C. ahamiyat",
+        options: [
+            "A. boylik",
+            "B. zavq",
+            "C. ahamiyat",
+            "D. quvonch"
+        ]
+    },
+    {
+        numb: 9,
+        question: "knowledge",
+        answer: "A. bilim",
+        options: [
+            "A. bilim",
+            "B. kuch",
+            "C. zaiflik",
+            "D. xotira"
+        ]
+    },
+    {
+        numb: 10,
+        question: "major",
+        answer: "D. katta",
+        options: [
+            "A. kichik",
+            "B. ahamiyatsiz",
+            "C. oson",
+            "D. katta"
+        ]
+    },
+    {
+        numb: 11,
+        question: "mean",
+        answer: "C. yovuz",
+        options: [
+            "A. do'stona",
+            "B. baxtli",
+            "C. yovuz",
+            "D. qayg'uli"
+        ]
+    },
+    {
+        numb: 12,
+        question: "to prefer",
+        answer: "A. afzal ko'rmoq",
+        options: [
+            "A. afzal ko'rmoq",
+            "B. rad etmoq",
+            "C. unutmoq",
+            "D. e'tiborsiz qoldirmoq"
+        ]
+    },
+    {
+        numb: 13,
+        question: "president",
+        answer: "B. prezident",
+        options: [
+            "A. talaba",
+            "B. prezident",
+            "C. o'qituvchi",
+            "D. do'st"
+        ]
+    },
+    {
+        numb: 14,
+        question: "progress",
+        answer: "C. rivojlanish",
+        options: [
+            "A. orqaga qaytish",
+            "B. muvaffaqiyatsizlik",
+            "C. rivojlanish",
+            "D. turg'unlik"
+        ]
+    },
+    {
+        numb: 15,
+        question: "respect",
+        answer: "A. hurmat",
+        options: [
+            "A. hurmat",
+            "B. nafrat",
+            "C. befarqlik",
+            "D. yoqtirmaslik"
+        ]
+    },
+    {
+        numb: 16,
+        question: "rich",
+        answer: "B. boy",
+        options: [
+            "A. kambag'al",
+            "B. boy",
+            "C. tejamkor",
+            "D. saxiy"
+        ]
+    },
+    {
+        numb: 17,
+        question: "skill",
+        answer: "A. mahorat",
+        options: [
+            "A. mahorat",
+            "B. bilmaslik",
+            "C. tasodif",
+            "D. baxtsizlik"
+        ]
+    },
+    {
+        numb: 18,
+        question: "somehow",
+        answer: "C. qandaydir",
+        options: [
+            "A. aniq",
+            "B. shubhasiz",
+            "C. qandaydir",
+            "D. aniq"
+        ]
+    },
+    {
+        numb: 19,
+        question: "strength",
+        answer: "B. kuch",
+        options: [
+            "A. zaiflik",
+            "B. kuch",
+            "C. mo'rtlik",
+            "D. yumshoqlik"
+        ]
+    },
+    {
+        numb: 20,
+        question: "to vote",
+        answer: "A. ovoz bermoq",
+        options: [
+            "A. ovoz bermoq",
+            "B. e'tiborsiz qoldirmoq",
+            "C. rad etmoq",
+            "D. tark etmoq"
+        ]
+    }
+];
