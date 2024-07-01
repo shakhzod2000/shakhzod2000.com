ff196ff9b0dd748f18f07f47f8061fbdc7a91bfc
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 17:16:35 +0500

    Create questions16.js

---
+++ questions16.js
@@ -0,0 +1,222 @@
+let questions16 = [
+    {
+        numb: 1,
+        question: "academy",
+        answer: "A. Akademie",
+        options: [
+            "A. Akademie",
+            "B. Schule",
+            "C. Universität",
+            "D. Bibliothek"
+        ]
+    },
+    {
+        numb: 2,
+        question: "ancient",
+        answer: "B. antik",
+        options: [
+            "A. modern",
+            "B. antik",
+            "C. jung",
+            "D. neu"
+        ]
+    },
+    {
+        numb: 3,
+        question: "board",
+        answer: "A. Brett",
+        options: [
+            "A. Brett",
+            "B. Tafel",
+            "C. Wand",
+            "D. Boden"
+        ]
+    },
+    {
+        numb: 4,
+        question: "century",
+        answer: "B. Jahrhundert",
+        options: [
+            "A. Jahrzehnt",
+            "B. Jahrhundert",
+            "C. Jahrtausend",
+            "D. Jahr"
+        ]
+    },
+    {
+        numb: 5,
+        question: "clue",
+        answer: "C. Hinweis",
+        options: [
+            "A. Lösung",
+            "B. Frage",
+            "C. Hinweis",
+            "D. Rätsel"
+        ]
+    },
+    {
+        numb: 6,
+        question: "concert",
+        answer: "A. Konzert",
+        options: [
+            "A. Konzert",
+            "B. Party",
+            "C. Versammlung",
+            "D. Aufführung"
+        ]
+    },
+    {
+        numb: 7,
+        question: "county",
+        answer: "D. Grafschaft",
+        options: [
+            "A. Stadt",
+            "B. Dorf",
+            "C. Land",
+            "D. Grafschaft"
+        ]
+    },
+    {
+        numb: 8,
+        question: "dictionary",
+        answer: "B. Wörterbuch",
+        options: [
+            "A. Enzyklopädie",
+            "B. Wörterbuch",
+            "C. Lehrbuch",
+            "D. Roman"
+        ]
+    },
+    {
+        numb: 9,
+        question: "to exist",
+        answer: "A. existieren",
+        options: [
+            "A. existieren",
+            "B. verschwinden",
+            "C. erscheinen",
+            "D. beenden"
+        ]
+    },
+    {
+        numb: 10,
+        question: "flat",
+        answer: "C. flach",
+        options: [
+            "A. kurvig",
+            "B. steil",
+            "C. flach",
+            "D. gewellt"
+        ]
+    },
+    {
+        numb: 11,
+        question: "gentleman",
+        answer: "D. Gentleman",
+        options: [
+            "A. Dame",
+            "B. Freund",
+            "C. Junge",
+            "D. Gentleman"
+        ]
+    },
+    {
+        numb: 12,
+        question: "hidden",
+        answer: "B. versteckt",
+        options: [
+            "A. sichtbar",
+            "B. versteckt",
+            "C. offensichtlich",
+            "D. bekannt"
+        ]
+    },
+    {
+        numb: 13,
+        question: "maybe",
+        answer: "A. vielleicht",
+        options: [
+            "A. vielleicht",
+            "B. sicher",
+            "C. niemals",
+            "D. immer"
+        ]
+    },
+    {
+        numb: 14,
+        question: "officer",
+        answer: "B. Offizier",
+        options: [
+            "A. Soldat",
+            "B. Offizier",
+            "C. General",
+            "D. Zivilist"
+        ]
+    },
+    {
+        numb: 15,
+        question: "original",
+        answer: "A. original",
+        options: [
+            "A. original",
+            "B. Kopie",
+            "C. Fälschung",
+            "D. Duplikat"
+        ]
+    },
+    {
+        numb: 16,
+        question: "to pound",
+        answer: "C. hämmern",
+        options: [
+            "A. streicheln",
+            "B. tippen",
+            "C. hämmern",
+            "D. werfen"
+        ]
+    },
+    {
+        numb: 17,
+        question: "process",
+        answer: "A. Prozess",
+        options: [
+            "A. Prozess",
+            "B. Ergebnis",
+            "C. Zustand",
+            "D. Schritt"
+        ]
+    },
+    {
+        numb: 18,
+        question: "to publish",
+        answer: "B. veröffentlichen",
+        options: [
+            "A. schreiben",
+            "B. veröffentlichen",
+            "C. lesen",
+            "D. drucken"
+        ]
+    },
+    {
+        numb: 19,
+        question: "theater",
+        answer: "A. Theater",
+        options: [
+            "A. Theater",
+            "B. Kino",
+            "C. Oper",
+            "D. Museum"
+        ]
+    },
+    {
+        numb: 20,
+        question: "wealth",
+        answer: "B. Reichtum",
+        options: [
+            "A. Armut",
+            "B. Reichtum",
+            "C. Besitz",
+            "D. Geld"
+        ]
+    }
+];
