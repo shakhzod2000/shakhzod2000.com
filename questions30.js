e43de7023d0f90bfc3ef824f708cea0886569708
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 23:41:26 +0500

    Create questions30.js

---
+++ questions30.js
@@ -0,0 +1,222 @@
+let questions30 = [
+    {
+        numb: 1,
+        question: "above",
+        answer: "A. über",
+        options: [
+            "A. über",
+            "B. unter",
+            "C. neben",
+            "D. hinter"
+        ]
+    },
+    {
+        numb: 2,
+        question: "ahead",
+        answer: "B. voraus",
+        options: [
+            "A. hinter",
+            "B. voraus",
+            "C. neben",
+            "D. zwischen"
+        ]
+    },
+    {
+        numb: 3,
+        question: "amount",
+        answer: "C. Betrag",
+        options: [
+            "A. Gewicht",
+            "B. Länge",
+            "C. Betrag",
+            "D. Fläche"
+        ]
+    },
+    {
+        numb: 4,
+        question: "belief",
+        answer: "D. Glaube",
+        options: [
+            "A. Zweifel",
+            "B. Angst",
+            "C. Hoffnung",
+            "D. Glaube"
+        ]
+    },
+    {
+        numb: 5,
+        question: "center",
+        answer: "A. Zentrum",
+        options: [
+            "A. Zentrum",
+            "B. Rand",
+            "C. Ecke",
+            "D. Seite"
+        ]
+    },
+    {
+        numb: 6,
+        question: "common",
+        answer: "B. häufig",
+        options: [
+            "A. selten",
+            "B. häufig",
+            "C. einzigartig",
+            "D. eigenartig"
+        ]
+    },
+    {
+        numb: 7,
+        question: "cost",
+        answer: "D. kosten",
+        options: [
+            "A. sparen",
+            "B. verdienen",
+            "C. sammeln",
+            "D. kosten"
+        ]
+    },
+    {
+        numb: 8,
+        question: "demonstrate",
+        answer: "A. demonstrieren",
+        options: [
+            "A. demonstrieren",
+            "B. verstecken",
+            "C. ignorieren",
+            "D. vergessen"
+        ]
+    },
+    {
+        numb: 9,
+        question: "different",
+        answer: "C. anders",
+        options: [
+            "A. gleich",
+            "B. ähnlich",
+            "C. anders",
+            "D. gewöhnlich"
+        ]
+    },
+    {
+        numb: 10,
+        question: "evidence",
+        answer: "D. Beweis",
+        options: [
+            "A. Vermutung",
+            "B. Theorie",
+            "C. Spekulation",
+            "D. Beweis"
+        ]
+    },
+    {
+        numb: 11,
+        question: "honesty",
+        answer: "A. Ehrlichkeit",
+        options: [
+            "A. Ehrlichkeit",
+            "B. Lüge",
+            "C. Betrug",
+            "D. Täuschung"
+        ]
+    },
+    {
+        numb: 12,
+        question: "idiom",
+        answer: "C. Redewendung",
+        options: [
+            "A. Wort",
+            "B. Satz",
+            "C. Redewendung",
+            "D. Buch"
+        ]
+    },
+    {
+        numb: 13,
+        question: "independent",
+        answer: "B. unabhängig",
+        options: [
+            "A. abhängig",
+            "B. unabhängig",
+            "C. kontrolliert",
+            "D. gefangen"
+        ]
+    },
+    {
+        numb: 14,
+        question: "inside",
+        answer: "D. innen",
+        options: [
+            "A. außen",
+            "B. oberhalb",
+            "C. unterhalb",
+            "D. innen"
+        ]
+    },
+    {
+        numb: 15,
+        question: "master",
+        answer: "A. Meister",
+        options: [
+            "A. Meister",
+            "B. Lehrling",
+            "C. Anfänger",
+            "D. Schüler"
+        ]
+    },
+    {
+        numb: 16,
+        question: "memory",
+        answer: "B. Erinnerung",
+        options: [
+            "A. Vergessen",
+            "B. Erinnerung",
+            "C. Gegenwart",
+            "D. Zukunft"
+        ]
+    },
+    {
+        numb: 17,
+        question: "proper",
+        answer: "C. angemessen",
+        options: [
+            "A. falsch",
+            "B. unangemessen",
+            "C. angemessen",
+            "D. gewöhnlich"
+        ]
+    },
+    {
+        numb: 18,
+        question: "scan",
+        answer: "A. scannen",
+        options: [
+            "A. scannen",
+            "B. übersehen",
+            "C. ignorieren",
+            "D. löschen"
+        ]
+    },
+    {
+        numb: 19,
+        question: "section",
+        answer: "B. Abschnitt",
+        options: [
+            "A. Gesamtheit",
+            "B. Abschnitt",
+            "C. Einzelteil",
+            "D. Detail"
+        ]
+    },
+    {
+        numb: 20,
+        question: "surface",
+        answer: "D. Oberfläche",
+        options: [
+            "A. Tiefe",
+            "B. Inneres",
+            "C. Rand",
+            "D. Oberfläche"
+        ]
+    }
+];
