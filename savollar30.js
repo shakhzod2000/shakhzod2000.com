d33a1534d5ad395629a5943c389338f2b5393a07
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 23:52:27 +0500

    Create savollar30.js

---
+++ savollar30.js
@@ -0,0 +1,222 @@
+let savollar30 = [
+    {
+        numb: 1,
+        question: "above",
+        answer: "A. ustida",
+        options: [
+            "A. ustida",
+            "B. ostida",
+            "C. yonida",
+            "D. orqasida"
+        ]
+    },
+    {
+        numb: 2,
+        question: "ahead",
+        answer: "B. oldinda",
+        options: [
+            "A. orqasida",
+            "B. oldinda",
+            "C. yonida",
+            "D. o'rtasida"
+        ]
+    },
+    {
+        numb: 3,
+        question: "amount",
+        answer: "C. miqdor",
+        options: [
+            "A. vazn",
+            "B. uzunlik",
+            "C. miqdor",
+            "D. maydon"
+        ]
+    },
+    {
+        numb: 4,
+        question: "belief",
+        answer: "D. ishonch",
+        options: [
+            "A. shubha",
+            "B. qo'rquv",
+            "C. umid",
+            "D. ishonch"
+        ]
+    },
+    {
+        numb: 5,
+        question: "center",
+        answer: "A. markaz",
+        options: [
+            "A. markaz",
+            "B. chet",
+            "C. burchak",
+            "D. tomon"
+        ]
+    },
+    {
+        numb: 6,
+        question: "common",
+        answer: "B. odatiy",
+        options: [
+            "A. kamdan-kam",
+            "B. odatiy",
+            "C. noyob",
+            "D. g'alati"
+        ]
+    },
+    {
+        numb: 7,
+        question: "cost",
+        answer: "D. narx turmoq",
+        options: [
+            "A. tejash",
+            "B. topish",
+            "C. yig'ish",
+            "D. narx turmoq"
+        ]
+    },
+    {
+        numb: 8,
+        question: "demonstrate",
+        answer: "A. namoyish qilmoq",
+        options: [
+            "A. namoyish qilmoq",
+            "B. yashirmoq",
+            "C. e'tiborsiz qoldirmoq",
+            "D. unutmoq"
+        ]
+    },
+    {
+        numb: 9,
+        question: "different",
+        answer: "C. turli xil",
+        options: [
+            "A. bir xil",
+            "B. o'xshash",
+            "C. turli xil",
+            "D. odatiy"
+        ]
+    },
+    {
+        numb: 10,
+        question: "evidence",
+        answer: "D. dalil",
+        options: [
+            "A. taxmin",
+            "B. nazariya",
+            "C. spekulyatsiya",
+            "D. dalil"
+        ]
+    },
+    {
+        numb: 11,
+        question: "honesty",
+        answer: "A. to'g'riso'zlik",
+        options: [
+            "A. to'g'riso'zlik",
+            "B. yolg'on",
+            "C. firibgarlik",
+            "D. aldov"
+        ]
+    },
+    {
+        numb: 12,
+        question: "idiom",
+        answer: "C. idioma",
+        options: [
+            "A. so'z",
+            "B. gap",
+            "C. idioma",
+            "D. kitob"
+        ]
+    },
+    {
+        numb: 13,
+        question: "independent",
+        answer: "B. mustaqil",
+        options: [
+            "A. qaram",
+            "B. mustaqil",
+            "C. nazorat qilinadigan",
+            "D. tutqun"
+        ]
+    },
+    {
+        numb: 14,
+        question: "inside",
+        answer: "D. ichi",
+        options: [
+            "A. tashqarisida",
+            "B. yuqorisida",
+            "C. pastida",
+            "D. ichi"
+        ]
+    },
+    {
+        numb: 15,
+        question: "master",
+        answer: "A. usta",
+        options: [
+            "A. usta",
+            "B. shogird",
+            "C. boshlovchi",
+            "D. talaba"
+        ]
+    },
+    {
+        numb: 16,
+        question: "memory",
+        answer: "B. xotira",
+        options: [
+            "A. unutish",
+            "B. xotira",
+            "C. hozirgi",
+            "D. kelajak"
+        ]
+    },
+    {
+        numb: 17,
+        question: "proper",
+        answer: "C. o'rinli",
+        options: [
+            "A. noo'rinli",
+            "B. noo'rin",
+            "C. to'g'ri",
+            "D. odatiy"
+        ]
+    },
+    {
+        numb: 18,
+        question: "scan",
+        answer: "A. qarab chiqmoq",
+        options: [
+            "A. qarab chiqmoq",
+            "B. ko'rmaslik",
+            "C. e'tiborsiz qoldirish",
+            "D. o'chirish"
+        ]
+    },
+    {
+        numb: 19,
+        question: "section",
+        answer: "B. bo'lim",
+        options: [
+            "A. butunlik",
+            "B. bo'lim",
+            "C. qism",
+            "D. detal"
+        ]
+    },
+    {
+        numb: 20,
+        question: "surface",
+        answer: "D. sirt",
+        options: [
+            "A. chuqurlik",
+            "B. ichki qism",
+            "C. chet",
+            "D. sirt"
+        ]
+    }
+];
