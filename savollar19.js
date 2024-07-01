71db636f19a1b72174823d753f2f2314ac79ef8a
Shakhzod Shermatov <sshermatov50@gmail.com>
2024-07-01 17:50:00 +0500

    Create savollar19.js

---
+++ savollar19.js
@@ -0,0 +1,222 @@
+let savollar19 = [
+    {
+        numb: 1,
+        question: "alive",
+        answer: "B. tirik",
+        options: [
+            "A. o'lik",
+            "B. tirik",
+            "C. kasal",
+            "D. charchagan"
+        ]
+    },
+    {
+        numb: 2,
+        question: "bone",
+        answer: "C. suyak",
+        options: [
+            "A. teri",
+            "B. go'sht",
+            "C. suyak",
+            "D. muskul"
+        ]
+    },
+    {
+        numb: 3,
+        question: "to bother",
+        answer: "A. harakat qilmoq",
+        options: [
+            "A. harakat qilmoq",
+            "B. e'tiborsiz qoldirmoq",
+            "C. asabiylashtirmoq",
+            "D. bezovta qilmoq"
+        ]
+    },
+    {
+        numb: 4,
+        question: "captain",
+        answer: "D. kapitan",
+        options: [
+            "A. askar",
+            "B. uchuvchi",
+            "C. ofitser",
+            "D. kapitan"
+        ]
+    },
+    {
+        numb: 5,
+        question: "conclusion",
+        answer: "B. yakun",
+        options: [
+            "A. boshlanish",
+            "B. yakun",
+            "C. o'rtasi",
+            "D. natija"
+        ]
+    },
+    {
+        numb: 6,
+        question: "doubt",
+        answer: "C. shubha",
+        options: [
+            "A. ishonch",
+            "B. ishonmoq",
+            "C. shubha",
+            "D. e'tiqod"
+        ]
+    },
+    {
+        numb: 7,
+        question: "to explore",
+        answer: "A. kashf qilmoq",
+        options: [
+            "A. kashf qilmoq",
+            "B. oldini olmoq",
+            "C. e'tiborsiz qoldirmoq",
+            "D. himoya qilmoq"
+        ]
+    },
+    {
+        numb: 8,
+        question: "foreign",
+        answer: "D. chet el",
+        options: [
+            "A. mahalliy",
+            "B. tanish",
+            "C. mintaqaviy",
+            "D. chet el"
+        ]
+    },
+    {
+        numb: 9,
+        question: "glad",
+        answer: "B. xursand",
+        options: [
+            "A. qayg'uli",
+            "B. xursand",
+            "C. g'azablangan",
+            "D. xavotirlangan"
+        ]
+    },
+    {
+        numb: 10,
+        question: "however",
+        answer: "A. shunga qaramasdan",
+        options: [
+            "A. shunga qaramasdan",
+            "B. shuning uchun",
+            "C. bundan tashqari",
+            "D. shunday bo'lsa ham"
+        ]
+    },
+    {
+        numb: 11,
+        question: "injustice",
+        answer: "C. adolatsizlik",
+        options: [
+            "A. adolat",
+            "B. adolatli",
+            "C. adolatsizlik",
+            "D. tartib"
+        ]
+    },
+    {
+        numb: 12,
+        question: "international",
+        answer: "D. xalqaro",
+        options: [
+            "A. milliy",
+            "B. mahalliy",
+            "C. mintaqaviy",
+            "D. xalqaro"
+        ]
+    },
+    {
+        numb: 13,
+        question: "lawyer",
+        answer: "A. advokat",
+        options: [
+            "A. advokat",
+            "B. sudya",
+            "C. politsiyachi",
+            "D. guvoh"
+        ]
+    },
+    {
+        numb: 14,
+        question: "to mention",
+        answer: "B. aytib o'tmoq",
+        options: [
+            "A. sukut saqlamoq",
+            "B. aytib o'tmoq",
+            "C. e'tiborsiz qoldirmoq",
+            "D. yashirmoq"
+        ]
+    },
+    {
+        numb: 15,
+        question: "policy",
+        answer: "C. qonun-qoida",
+        options: [
+            "A. fikr",
+            "B. taklif",
+            "C. qonun-qoida",
+            "D. qonun"
+        ]
+    },
+    {
+        numb: 16,
+        question: "social",
+        answer: "A. ijtimoiy",
+        options: [
+            "A. ijtimoiy",
+            "B. yolg'iz",
+            "C. ajralgan",
+            "D. individual"
+        ]
+    },
+    {
+        numb: 17,
+        question: "speech",
+        answer: "D. nutq",
+        options: [
+            "A. suhbat",
+            "B. munozara",
+            "C. intervyu",
+            "D. nutq"
+        ]
+    },
+    {
+        numb: 18,
+        question: "staff",
+        answer: "B. xodimlar",
+        options: [
+            "A. tashrif buyuruvchilar",
+            "B. xodimlar",
+            "C. mehmonlar",
+            "D. mijozlar"
+        ]
+    },
+    {
+        numb: 19,
+        question: "toward",
+        answer: "C. tomon",
+        options: [
+            "A. uzoqlashmoq",
+            "B. atrofida",
+            "C. tomon",
+            "D. uzoqda"
+        ]
+    },
+    {
+        numb: 20,
+        question: "wood",
+        answer: "A. yog'och",
+        options: [
+            "A. yog'och",
+            "B. metall",
+            "C. tosh",
+            "D. shisha"
+        ]
+    }
+];
